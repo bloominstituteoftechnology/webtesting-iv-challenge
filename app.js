87b3b588ee26b2ea764c7f4fbbcdcf48a486ef6d
mixelpixel <mixelpix@mac.com>
2017-09-05 18:57:15 -0400

    idem

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose');
 const server = require('./server');
+
+mongoose.connect('mongodb://localhost/food', {}, (err) => {
+  if (err) return console.log(err);
+  console.log('You are connected to the food DataBase');
+});
+
+server.listen(8080, () => {
+  console.log("Ye ol' server is listening on port 8080, me boy-o!");
+});
