f12b7a13b71b5208def6785f4c070cab166d660e
mixelpixel <mixelpix@mac.com>
2017-09-05 19:15:54 -0400

    linter not working?

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const server = require('./server');
 
+/* eslint no-console: 0 */
 mongoose.connect('mongodb://localhost/food', {}, (err) => {
   if (err) return console.log('\nWTF??????\n', err);
   console.log('DUDE! You are like totally connected to the food DataBase, man!\
