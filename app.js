0b707d07dadb2b8945e1862ea002810aba96b9df
mixelpixel <mixelpix@mac.com>
2017-09-05 19:27:08 -0400

    app √

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = require('./server');
 /* eslint no-console: 0 */
 /* eslint no-multi-str: 0 */
 mongoose.connect('mongodb://localhost/food', {}, (err) => {
-  if (err) return console.log('\nWTF??????\n', err);
+  if (err) return console.log('\nyoYOyo-yo!!! WTF, yo??????\n', err);
   console.log('DUDE! You are like totally connected to the food DataBase, man!\
   \nLike, all your chakras are, like, totally... cool...\
   \ny\'know, like: lined up and in balance, man... Whoa.\n');
