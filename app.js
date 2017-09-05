05a7c7798bb8bb22e5f4a78460eca3f0b6ccc68d
mixelpixel <mixelpix@mac.com>
2017-09-05 19:12:08 -0400

    server up and running with mongoDB

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const server = require('./server');
 
 mongoose.connect('mongodb://localhost/food', {}, (err) => {
-  if (err) return console.log(err);
-  console.log('You are connected to the food DataBase');
+  if (err) return console.log('\nWTF??????\n', err);
+  console.log('DUDE! You are like totally connected to the food DataBase, man!\
+  \nLike, all your chakras are, like, totally... cool...\
+  \ny\'know, like: lined up and in balance, man... Whoa.\n');
 });
 
 server.listen(8080, () => {
-  console.log("Ye ol' server is listening on port 8080, me boy-o!");
+  console.log("\nYe ol' server is listening on port 8080, me boy-o!\n");
 });
