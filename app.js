df362025013524638fcc9dfbb91a39a271645178
mixelpixel <mixelpix@mac.com>
2017-09-05 19:26:20 -0400

    multi-str ok √

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const server = require('./server');
 
 /* eslint no-console: 0 */
+/* eslint no-multi-str: 0 */
 mongoose.connect('mongodb://localhost/food', {}, (err) => {
   if (err) return console.log('\nWTF??????\n', err);
   console.log('DUDE! You are like totally connected to the food DataBase, man!\
