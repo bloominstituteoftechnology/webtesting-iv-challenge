3dca9ee6ae83941e56ea8741cc84c1d13a7ed00a
atiffany <abby.tiffany@gmail.com>
2018-01-31 16:11:18 -0800

    skeleton of routes written

---
+++ routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+route.post('/', (req, res) => {});
+
+route.put('/', (req, res) => {});
+
+route.get('/', (req, res) => {});
+
+route.delete('/:zipCode', (req, res) => {});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const routes = require('./routes');
 
 
 const server = express();
 server.use(morgan('combined'));
 server.use(bodyParser.json());
+server.use(routes)
+
 
 module.exports = server;
