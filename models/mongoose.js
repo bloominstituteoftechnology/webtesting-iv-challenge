754ca54f31942d1d7b7a861536e997dbeb2e9e89
Johnathan Justinn <jjustinn85@gmail.com>
2018-03-14 16:29:55 -0400

    Marsupial schema written

---
+++ models/mongoose.js
@@ -0,0 +1,33 @@
+import { Mongoose } from 'mongoose';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.schema;
+
+const MarsupialSchema = new Schema({
+    name: {
+        type: String, 
+        required: true
+    },
+    latinName: {
+        type: String, 
+        required: true
+    },
+    region: {
+        type: String, 
+        required: true
+    }
+});
+
+MarsupialSchema.methods.getName = () => {
+    return this.name
+};
+
+MarsupialSchema.statics.getAllMarsupials = (cb) => {
+    Mongoose.find({}, (err, marsupials) => {
+        if (err) return cb(err);
+        cb(marsupials)
+    });
+};
+
+const Marsupial = mongoose.model('Marsupial', MarsupialSchema);
+module.exports = Marsupial;
