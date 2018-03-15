f1993c5d93e923ab4fc4a6d00080be8c5a49da7e
Clara Andrew <candrewwani@gmail.com>
2018-03-15 02:45:59 -0400

    tests passing

--- server.js
+++ server.js
@@ -8,48 +8,54 @@ server.use(express.json());
 const User = require("./users");
 
 server.get("/users", (req, res) => {
-	User.find()
-		.then(users => {
-			res.json(users);
-		})
-		.catch(err => {
-			res.error(err);
-		});
+  User.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 server.post("/users", (req, res) => {
-	const { name, location } = req.body;
-	const newUser = new User({ name, location });
-	newUser
-		.save()
-		.then(user => {
-			res.status(201).json(user);
-		})
-		.catch(err => {
-			res.error(err);
-		});
+  const { name, location } = req.body;
+  const newUser = new User({ name, location });
+  newUser
+    .save()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 server.put("/users", (req, res) => {
-	const { name, location } = req.body;
-	User.update({ name }, req.body)
-		.then(user => {
-			res.status(201).json(user);
-		})
-		.catch(err => {
-			res.error(err);
-		});
+  const { name, location } = req.body;
+  User.update({ name }, req.body)
+    .then(user => {
+      User.find({ name })
+        .then(user => {
+          res.status(201).json(user);
+        })
+        .catch(err => {
+          res.json(err);
+        });
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 server.delete("/users", (req, res) => {
-	const { name, location } = req.body;
-	User.find({ name })
-		.remove()
-		.then(res => {
-			res.status(202).json(`Successfully deleted ${name}.`);
-		})
-		.catch(err => {
-			res.error(err);
-		});
+  const { name, location } = req.body;
+  User.find({ name })
+    .remove()
+    .then(res => {
+      res.status(200).json({ message: `Successfully deleted ${name}.` });
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 module.exports = server;
