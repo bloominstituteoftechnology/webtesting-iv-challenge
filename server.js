6be7f8ca146244c6ebf36c242f54d56764c1209c
U-Honolulu\Troy <me@bitcointroy.co>
2018-03-14 21:09:26 -0500

    ERRORS EVERYWHERE!

--- server.js
+++ server.js
@@ -1,21 +1,55 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
 const server = express();
-server.use(morgan('combined'));
+server.use(morgan("combined"));
 server.use(express.json());
 
-//server.get
+const User = require("./users");
 
-//server.post
-// req.body === newUser
-//server.put
-// req.body === update
-// {name: 'Romeo', location: 'Hungary'}
-// User.find(req.body.name).update(req.body)
+server.get("/users", (req, res) => {
+	User.find()
+		.then(users => {
+			res.json(users);
+		})
+		.catch(err => {
+			res.error(err);
+		});
+});
 
-//server.delete
-// req.body = {name: 'Romeo'}
-// User.find(req.body).del()
+server.post("/users", (req, res) => {
+	const { name, location } = req.body;
+	const newUser = new User({ name, location });
+	newUser
+		.save()
+		.then(user => {
+			res.status(201).json(user);
+		})
+		.catch(err => {
+			res.error(err);
+		});
+});
+server.put("/users", (req, res) => {
+	const { name, location } = req.body;
+	User.update({ name }, req.body)
+		.then(user => {
+			res.status(201).json(user);
+		})
+		.catch(err => {
+			res.error(err);
+		});
+});
+
+server.delete("/users", (req, res) => {
+	const { name, location } = req.body;
+	User.find({ name })
+		.remove()
+		.then(res => {
+			res.status(202).json(`Successfully deleted ${name}.`);
+		})
+		.catch(err => {
+			res.error(err);
+		});
+});
 
 module.exports = server;
