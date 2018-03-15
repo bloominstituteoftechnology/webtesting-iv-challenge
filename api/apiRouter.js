a559fd1de84e34f8649e8fa527f46522c135f59d
Samuel Cha <sunchipssam@gmail.com>
2018-03-15 06:52:46 -0700

    Added api router

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -4,4 +4,8 @@ const amaRouter = require('../mvc/controllers/ama/amaRouter');
 
 router.use('/ama', amaRouter);
 
+router.route('/').get((req, res) => {
+  res.json({ api: 'route GET' });
+});
+
 module.exports = router;
