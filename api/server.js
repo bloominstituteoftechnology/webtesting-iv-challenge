ee45f78b8c79c421a7eb101929c95e37256e5d4d
Christopher Beards <beardscm@gmail.com>
2018-03-28 15:34:51 -0400

    Updated Package.json

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const server = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+server.use(morgan('combined'));
+server.use(express.json());
+
+//===============================
+//          ROUTES
+//===============================
+
+server.get('/', function(req, res) {
+  res.status(200).json({ status: 'API Running' });
+});
+
+module.exports = server;
