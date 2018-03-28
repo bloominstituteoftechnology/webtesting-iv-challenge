cab2bfed620ed9e8fc00c36595224142743dbb63
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-03-28 18:05:03 -0400

    Think I updated Routes for Mongo... Not sure if Correct Yet.
    server.js --- Hopefully wrote proper routing for GET/POST/PUT/DELETE.

--- server.js
+++ server.js
@@ -1,21 +1,52 @@
 const express = require('express');
-// const morgan = require('morgan');
 
 const server = express();
-// Didn't say to import this, not sure if we should use or not
-// server.use(morgan('combined'));
 server.use(express.json());
 
-server.post('/band', (req, res) => {
-  res.status(200).send(req.body);
+const Band = require('/models');
+
+server.get('/bands', (req, res) => {
+  Band.find().then((bands) => res.status(200).json(bands));
 });
 
-server.get('/bands', (req,res) => {
-    res.status(200).send(req.body);
-})
+server.post('/band', (req, res) => {
+  const newBand = new Band(req.body);
 
-server.put('/band/:name', (req,res) => {
-  res.status(200).send(req.body);
-})
+  newBand
+    .save()
+    .then(savedBand => {
+      res.status(201).json(savedBand);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+});
+
+server.put('/band/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedBand = req.body;
+
+  Band.findByIdAndUpdate(id, updatedBand)
+    .then(band => {
+      if (band) res.status(200).json(band);
+      else res.status(404).json("Band ID Not Found")
+    })
+    .catch(error => {
+      res.status(500).json("Error Updating Band")
+    })
+});
+
+server.delete('/band/:id', (req, res) => {
+  const { id } = req.params;
+
+  Band.findByIdAndRemove(id)
+    .then(removedBand => {
+      if (removedBand) res.status(200).json(removedBand)
+      else res.status(404).json('Band ID Not Found')
+    })
+    .catch(error => {
+      res.status(500).json('Error Deleting Band')
+    })
+});
 
 module.exports = server;
