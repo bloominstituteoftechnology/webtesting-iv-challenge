f01be32dbc927cda415e9edcb8bc209fcdf5d36b
Smithface <kylewibblesmith@gmail.com>
2018-03-28 16:09:58 -0400

    added basic outline for CRUD requests

--- server.js
+++ server.js
@@ -6,4 +6,20 @@ const server = express();
 server.use(morgan('combined')); //described by morgan as "Standard Apache combined log output."
 server.use(express.json());
 
+server.post('/anime', (req, res) => {
+  res.json(req.body);
+});
+
+server.get('/anime', (req, res) => {
+  res.json('here\'s all the anime I know about');
+});
+
+server.put('/anime', (req, res) => {
+  res.json('you added a new anime');
+});
+
+server.delete('/anime', (req, res) => {
+  res.json('you deleted an anime');
+})
+
 module.exports = server;
