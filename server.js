6b507f76a9ffd072964ce96bccc09d4c1fec8126
Maximo Delarosa <mjd809@gmail.com>
2018-03-28 16:08:05 -0400

    Completed two routes

--- server.js
+++ server.js
@@ -5,5 +5,18 @@ const server = express();
 server.use(morgan('Combined'));
 server.use(express.json());
 
+const User = require('./Users');
+
+server.get('/users', (req, res) => {
+  User.find()
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(422).json({ err: err }));
+});
+
+server.post('users', (req, res) => {
+    User.Create(req.body)
+        .then(user => res.status(200).json({ success: 'User created'}))
+        .catch(err => res.json({ msg:'Could not create User', err}))
+});
 
 module.exports = server;
