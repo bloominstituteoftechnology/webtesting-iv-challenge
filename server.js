98b560084ebd967a38be68efe3e17a0ebffe8093
Dixie Korley <dixiekorley@gmail.com>
2018-03-28 16:55:26 -0400

    Working on server.test.js

--- server.js
+++ server.js
@@ -6,4 +6,23 @@ const server = express();
 server.use(morgan('combined'));
 server.use(express.json());
 
+const Jobs = require('./models');
+
+server.post('/jobs', (req, res) => {
+    const job = req.body;
+    const newJob = new Jobs(job);
+
+    newJob.save((err, jobs) => {
+        if (err) return res.send(err);
+        res.send(jobs);
+    });
+});
+
+server.get('/jobs', (req, res) => {
+    Jobs.find({}, (err, jobs) => {
+        if (err) return res.send(err);
+        res.send(jobs);
+    });
+});
+
 module.exports = server;
--- server.test.js
+++ server.test.js
@@ -1,5 +1,5 @@
-
 const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/jobs');
 
 const chai = require('chai');
 const chaihttp = require('chai-http');
@@ -9,5 +9,36 @@ const sinon = require('sinon');
 const server = require('./server');
 chai.use(chaihttp);
 
-const List = require('./models');
+const Job = require('./models');
 
+describe('Server', () => {
+    describe('[POST] /jobs', () => {
+        it('should add a new job', () => {
+            const job = {
+                name: 'John Smith',
+                occupation: 'Author'
+            };
+            chai.request(server)
+            .post('/jobs')
+            .send(job)
+            .end((err, res) => {
+                console.log(res.body);
+                if (err) console.error(err);
+                expect(res.status).to.equal(200);
+                expect(res.body.name).to.equal('John Smith');
+            });
+        });
+    });
+
+    describe('[GET] /jobs', () => { 
+        it('should return all the jobs', () => {
+            chai.request(server)
+            .get('/jobs')
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.status).to.equal(200);
+                expect(res.body).to.be.a('array');
+            });
+        });
+    });
+});
