cf55edbc530a26eca44a5ac93db4ae96a529f8ad
Peter Gray <peter@petergraycreative.com>
2018-03-28 15:01:19 -0500

    Add test for getRecordByName and getAllRecords

--- models.test.js
+++ models.test.js
@@ -7,11 +7,26 @@ const sinon = require('sinon');
 const Record = require('./models');
 
 describe('Records', () => {
-  it('should return the record with the given name', () => {
-    const record = new Record({
-      name: "The White Album",
-      recordType: "LP",
+  describe('getRecordByName', () => {
+    it('should return the record with the given name', () => {
+      const record = new Record({
+        name: 'The White Album',
+        recordType: 'LP',
+      });
+      expect(record.getRecordByName()).to.equal('The White Album');
     });
-    expect(record.getRecordByName()).to.equal('The White Album');
   });
-})
+  describe('getAllRecords', () => {
+    it('should return all the bands', () => {
+      sinon.stub(Record, 'find');
+      Record.find.yields(null, [
+        { name: 'The White Album', recordType: 'LP' },
+        { name: 'Abbey Road', recordType: 'LP' }
+      ]);
+      Record.getAllRecords((records) => {
+        expect(records.length).to.equal(2);
+        expect(records[0].name).to.equal('The White Album');
+      });
+    });
+  });
+});
---
+++ server.test.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const chai = require('chai');
+const chaihttp = require('chai-http');
+const { expect } = chai;
+const sinon = require('sinon');
+
+const server = require('./server');
+chai.use(chaihttp);
+
+const Record = require('./models');
+
