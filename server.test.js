778a2bb59773f06318f8965180fd279bd927848a
Smithface <kylewibblesmith@gmail.com>
2018-03-28 16:29:59 -0400

    added first test for POST route
    also added outline for other routes

--- server.test.js
+++ server.test.js
@@ -11,15 +11,28 @@ const Anime = require('./model');
 
 describe('Server', () => {
   describe('[POST] /anime', () => {
-  
+    it('should add a new anime', () => {
+      const newAnime = {
+        name: 'One Piece',
+        genre: 'Adventure Fantasy'
+      };
+      chai.request(server)
+        .post('/anime')
+        .send(newAnime)
+        .end((err, res) => {
+          if (err) console.error(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.name).to.equal('One Piece');
+        });
+    });
   });
-  describe('[GET] /anime', () => {
+  // describe('[GET] /anime', () => {
   
-  });
-  describe('[PUT] /anime', () => {
+  // });
+  // describe('[PUT] /anime', () => {
   
-  });
-  describe('[DELETE] /anime', () => {
+  // });
+  // describe('[DELETE] /anime', () => {
   
-  });
+  // });
 });
