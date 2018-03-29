fc53bb95de2af648f1a8fef4e2f3938aa545dfe4
german gonzalez <gcomlnk@gmail.com>
2018-03-29 14:54:16 -0400

    adding mongoose and connecting to DB users

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const server = express();
 server.use(morgan('combined'));
@@ -9,3 +10,10 @@ server.use(express.json());
 server.get('/users', (req, res) => {
     res.json('Hello World');
 });
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://germancin:secure123@159.65.170.21/users')
+    .then(conn => console.log('Connected to MongoDB - Server:159.65.170.21 DB:users'))
+    .catch(err => console.log('error :::: ' + err));
+
+module.exports =  server;
