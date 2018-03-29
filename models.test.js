62d1f1f7e37d0cee5e46ca4bc219c26c404c5130
Mephestys <mephestys@gmail.com>
2018-03-28 19:26:06 -0600

    Wrote new model tests to have functional full suite.

--- models.test.js
+++ models.test.js
@@ -1,57 +1,39 @@
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/test');
-// const Anime = require('./model');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+const Anime = require('./model');
 
-// const chai = require('chai');
-// const { expect } = chai;
-// const sinon = require('sinon');
+const chai = require('chai');
+const { expect } = chai;
+const sinon = require('sinon');
 
-// describe('Anime', () => {
-//   before((done) => {
-//     mongoose.connect('mongodb://localhost/test');
-//     const db = mongoose.connection;
-//     db.on('error', () => {
-//       console.error('connection error');
-//     });
-//     db.once('open', () => {
-//       console.log('we are connected');
-//       done();
-//     });
-//   });
+describe('Anime', () => {
+  describe('#getName()', () => {
+    it('should return the name of the anime', () => {
+      const anime = new Anime({
+        name: 'DragonballZ',
+        genre: 'Action'
+      });
+      expect(anime.getName()).to.equal('DragonballZ');
+    });
 
-//   after((done) => {
-//     mongoose.connection.db.dropDatabase(() => {
-//       mongoose.connection.close(done);
-//     });
-//   });
+    it('should return a string', () => {
+      const anime = new Anime({
+        name: 'JoJo\'s Bizarre Adventure',
+        genre: 'I have no idea'
+      });
+      expect(typeof anime.getName()).to.equal('string');
+    });
+  });
 
-//   const neo = new Anime({ name : 'Neon Genesis Evangelion' });
-//   const trigun = new Anime({ name : 'Trigun' });
-//   const hxh = new Anime({ name : 'Hunter x Hunter' });
-
-//   describe('#getName()', () => {
-//     it('should return the name of the anime', () => {
-//       const anime = new Anime({
-//         name: 'Neon Genesis Evangelion',
-//         genre: 'Super Robot',
-//       });
-//       expect(anime.getName()).to.equal('Neon Genesis Evangelion');
-//     });
-//     it('should return a string', () => {
-//       const anime = new Anime({
-//         name: 'Bokurano',
-//       });
-//       expect(typeof anime.getName()).to.equal('string');
-//     });
-//   });
-
-//   describe('#getAllAnimes()', () => {
-//     it('should return all the animes', async function(done) {
-//       sinon.stub(Anime, 'find');
-//       Anime.find.resolves([hxh, trigun, neo]);
-//       const allAnimes = await Anime.getAllAnimes();
-//       expect(allAnimes.length).to.equal(3);
-//       done();
-//     });
-//   });
-// });
+  describe('#getAllAnimes()', () => {
+    it('should return all the anime in the database', () => {
+      sinon.stub(Anime, 'find');      
+      Anime.find.yields(null, [{ name: 'Akira'}]);
+      Anime.getAllAnimes((anime) => {
+        expect(anime.length).to.equal(1);
+        expect(anime[0].name).to.equal('Akira');
+        Anime.find.restore();
+      });
+    });
+  });
+});
