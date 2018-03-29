1f97adeb4e32383d8a2cd2b851d008cccff5fcf6
Ronnie Miksch <fakepreferences@gmail.com>
2018-03-28 17:21:33 -0700

    Trying all manner of things for the after to work

--- index.js
+++ index.js
@@ -18,16 +18,20 @@ server.post('/painter', (req,res) => {
     style: req.body.style
   })
   .save()
-  .then(sG => res.send(sG))
+  .then(sG => res.json(sG))
   .catch(err => res.status(404).json({error: err}));
 });
 
 
 server.get('/allPainters', (req,res) => {
-  const send = (sg) => {
-    res.send(sg);
-  };
-  Painter.getAllPainters(send);
+  // const send = (sg) => {
+  //   res.json(sg);
+  // };
+  // Painter.getAllPainters(send);
+  Painter.find({}, (err, ptr) => {
+    if(err) res.status(422).json({error: err});
+    res.json(ptr);
+  })
   // .then(sG => res.send(sG))
   // .catch(err => res.status(422).json({error: err}));
   // res.json("test");
--- index.test.js
+++ index.test.js
@@ -8,28 +8,26 @@ chai.use(chaiHttp);
 const server = require('./index');
 
 describe('Index', () => {
-  const dbase = mongoose.connection;
   before(done => {
-    console.log('before');
     mongoose.connect('mongodb://localhost/testPainters');
+    const dbase = mongoose.connection;
     dbase.on('error', () => {
       console.error('connection error');
+      done();
     });
     dbase.once('open', () => {
-      console.log('we are connected');
       done();
     });
   });
 
   after(done => {
-    // console.log('after');
     mongoose.connection.db.dropDatabase(() => {
       mongoose.connection.close(done);
     });
   });
 
   describe('[POST] /painter', () => {
-    it('should add a new painter', () => {
+    it('should add a new painter', (done) => {
       const newPainter = {
         name: 'van Gogh',
         style: 'Post-Impressionism'
@@ -39,23 +37,31 @@ describe('Index', () => {
       .send(newPainter)
       .end((err, res) => {
         //console.log("first log", res.body);
-        if (err) console.error(err);
+        if (err) {
+          console.error(err);
+          done();
+        };
         expect(res.status).to.equal(200);
         expect(res.body.name).to.equal('van Gogh');
       });
+      done();
     });
   });
 
   describe('[GET] /allPainters', () => {
-    it("should return all the painters", () => {
+    it("should return all the painters", (done) => {
       chai.request(server)
       .get("/allPainters")
       .end((err, res) => {
         // console.log("log", res.body);
-        if (err) console.error(err);
+        if (err) {
+          console.error(err);
+          done();
+        }
         expect(res.status).to.equal(200);
         expect(res.body[0].name).to.equal("Franz Kline");
       });
+      done();
     });
   });
 });
