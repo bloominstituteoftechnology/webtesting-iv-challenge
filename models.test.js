dd4433a6b802f117a34ddbb12da312643940eaed
cliffkang <cliffgkang@gmail.com>
2018-03-28 18:16:58 -0700

    wrote a few tests in models, need to code up models.js now

---
+++ models.test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const chai = require('chai');
+const { expect } = chai;
+const sinon = require('sinon');
+
+const Show = require('./models');
+
+describe('Shows', () => {
+   describe('getShowName', () => {
+      it('should return the expected show name', () => {
+         const show = new Show({
+            name: 'Black Mirror',
+            year: 2015
+         });
+         expect(show.getShowName()).to.equal.apply('Black Mirror');
+      });
+   });
+
+   describe('getAllShows', () => {
+      it('should return all the shows', () => {
+         sinon.stub(Show, 'find');
+         Show.find.yields(null, [
+            { name: 'Black Mirror', year: 2015 },
+            { name: 'Brooklyn Nine-Nine', year: 2013 }
+         ]);
+         Show.getAllShows(shows => {
+            expect(shows.length).to.equal(2);
+            expect(shows[1].name).to.equal('Brooklyn Nine-Nine');
+         });
+      });
+   });
+});
--- server.js
+++ server.js
@@ -3,5 +3,8 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+server.get('/band', (req, res) => {
+   const { name } = req.body;
+})
 
 module.exports = server;
