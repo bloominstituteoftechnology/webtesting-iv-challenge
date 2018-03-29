58ad97b42dbc96d7643101cacfbe0f9e9af2ab0b
Mephestys <mephestys@gmail.com>
2018-03-28 19:12:45 -0600

    Pretty beyond done with this entire project. - Removed 'unique' requirement from model, was messing up tests - Added server routes GET, POST, PUT, DELETE - Added tests for all routes

--- model.js
+++ model.js
@@ -5,7 +5,6 @@ const AnimeSchema = new Schema({
   name: {
     type: String,
     required: true,
-    unique: true,
   },
   genre: {
     type: String,
--- server.test.js
+++ server.test.js
@@ -1,76 +1,121 @@
 const mongoose = require('mongoose');
-const chai = require('chai');
-const chaihttp = require('chai-http');
-const { expect } = chai;
-const sinon = require('sinon');
-
-const server = require('./server');
-chai.use(chaihttp);
+mongoose.connect('mongodb://localhost/test');
 
 const Anime = require('./model');
+const server = require('./server');
 
-describe('Server', () => {
-  before((done) => {
-    mongoose.connect('mongodb://localhost/test');
-    const db = mongoose.connection;
-    db.on('error', () => {
-      console.error('connection error');
-    });
-    db.once('open', () => {
-      console.log('we are connected');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHTTP = require('chai-http');
+
+chai.use(chaiHTTP);
+
+describe('/anime', () => {
+  let animeId;
+
+  beforeEach((done) => {
+    new Anime({
+      name: 'Appleseed',
+      genre: 'Sci-fi Action'
+    }).save((err, savedAnime) => {
+      if (err) {
+        console.log(err);
+        return done();
+      }
+      animeId = savedAnime.id;
       done();
     });
   });
 
-  after((done) => {
-    mongoose.connection.db.dropDatabase(() => {
-      mongoose.connection.close(done);
+  afterEach((done) => {
+    Anime.remove({}, (err) => {
+      if (err) {
+        console.log(err);
+        return done();
+      };
+      done();
+    });
+  });
+
+  describe('[GET] /anime', () => {
+    it('should return all anime titles in the database', (done) => {
+      chai.request(server)
+        .get('/anime')
+        .end((err, res) => {
+          if (err) {
+            console.log(err);
+            return done();
+          }
+          expect(res.status).to.equal(200);
+          expect(Array.isArray(res.body)).to.equal(true);
+          expect(res.body.length).to.equal(1);
+          done();
+        });
+    });
+  });
+
+  describe('[PUT] /anime', () => {
+    it('should update the anime document', (done) => {
+      const update = {
+        id: animeId,
+        name: 'Sailor Moon',
+        genre: 'Magical Girl'
+      };
+      chai.request(server)
+        .put('/anime')
+        .send(update)
+        .end((err, res) => {
+          if (err) {
+            console.log(err);
+            return done();
+          }
+          expect(res.body.name).to.equal('Sailor Moon');
+          done();
+        });
+    });
+  });
+
+  describe('[DELETE] /anime/:id', () => {
+    it('should remove the specified document', (done) => {
+      chai.request(server)
+        .delete(`/anime/${animeId}`)
+        .end((err, res) => {
+          if (err) {
+            console.log(err);
+            return done();
+          }
+          expect(res.text).to.equal('successfully deleted');
+          Anime.findById(animeId, (err, deletedAnime) => {
+            if (err) {
+              console.log(err);
+              return done();
+            }
+            expect(deletedAnime).to.equal(null);
+            done();
+          });
+        });
     });
   });
 
   describe('[POST] /anime', () => {
     it('should add a new anime', (done) => {
-      const newAnime = {
-        name: 'One Piece',
-        genre: 'Adventure Fantasy'
+      const anime = {
+        name: 'Ghost in the Shell',
+        genre: 'Awesomeness Incarnate'
       };
+
       chai.request(server)
         .post('/anime')
-        .send(newAnime)
+        .send(anime)
         .end((err, res) => {
           if (err) {
-            console.error(err);
-            done();
+            console.log(err);
+            return done();
           }
           expect(res.status).to.equal(200);
-          expect(res.body.name).to.equal('One Piece');
+          expect(res.body.name).to.equal('Ghost in the Shell');
+          done();
         });
-      done();
     });
   });
-  
-  describe('[GET] /anime', () => {
-    it('should return a list of all anime in the database', (done) => {
-      // const getRequest = await chai.request(server).get('/anime');
-      chai.request(server)
-        .get('/anime')
-        .end((err, res) => {
-          if (err) {
-            console.error(err);
-            done();
-          }
-          expect(res.status).to.equal(200);
-          //expect(res.body).to.exist();
-        });
-      done();
-    });
-  });
-  
-  // });
-  // describe('[PUT] /anime', () => {
-  
-  // });
-  // describe('[DELETE] /anime', () => {
-  
-  // });
 });
