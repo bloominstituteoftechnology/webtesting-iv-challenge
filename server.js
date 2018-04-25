1c4f416448fb9aaa45c0f2284fbc522626047667
William <wtpwinberg@gmail.com>
2018-04-25 15:53:28 -0700

    GET function properly

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const Band = require('./band');
+const server = express();
+server.use(express.json());
+server.use(morgan('dev'));
+
+// ROUTES
+server.get('/api/bands', (req, res) => {
+  Band.find({}, (err, bands) => {
+    if (err) {
+      res.status(500).json({ error: 'Cannot find your bands' });
+    }
+    res.json(bands);
+  });
+});
+
+module.exports = server;
