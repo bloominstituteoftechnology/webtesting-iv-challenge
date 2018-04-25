df445aae53b0bbcfcc7d1da401b321ce77d1a3dd
AJ Genung <DragonRydrz@me.com>
2018-04-25 17:32:39 -0400

    working on post testing

--- server.js
+++ server.js
@@ -6,8 +6,30 @@ server.use(express.json());
 server.use(morgan('dev'));
 
 // Routes
-server.get('/', (req, res) => {
-    res.send('Hello, I am a working route')
-})
+server.get('/api/bands', (req, res) => {
+  Band.find()
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.post('/api/bands', (req, res) => {
+  const band = new Band(req.body);
+  console.log('before');
+  band
+    .save()
+    .then(response => {
+      console.log(response);
+      res.status(201).json(response);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  console.log('after');
+});
 
 module.exports = server;
--- server.tests.js
+++ server.tests.js
@@ -3,8 +3,8 @@ const chai = require('chai');
 const chaiHTTP = require('chai-http');
 
 mongoose.connect('mongodb://localhost/test', {}, err => {
-    if (err) return console.log(err);
-    console.log('TEST for DB Connection Achieved');
+  if (err) return console.log(err);
+  console.log('TEST for DB Connection Achieved');
 });
 
 const expect = chai.expect;
@@ -14,60 +14,86 @@ const Band = require('./Band');
 chai.use(chaiHTTP);
 
 describe('Bands', () => {
-    let bandId;
-    beforeEach(done => {
-        const newBand = new Band({
-            name: 'Metallica',
-            genre: 'Metal',
-            recentAlbum: 'Ride the Lightning',
-        })
-        newBand.save((err, saveBand) => {
-            if (err) {
-                console.log(err);
-                return done();
-            }
-            bandId = savedBand._id;
-            return done();
-        })
-        // creates new band
-        const newBand2 = new Band({
-            name: 'Skillet',
-            genre: 'Rock',
-            recentAlbum: 'Unleashed'
-        })
-        // saves the above band to the database
-        newBand2.save((err, saveBand) => {
-            if (err) {
-                console.log(err);
-                return done();
-            }
-            bandId = savedBand._id;
-            return done();
-        })
-    })
+  let bandId;
+  beforeEach(done => {
+    const newBand = new Band({
+      name: 'Metallica',
+      genre: 'Metal',
+      recentAlbum: 'Ride the Lightning',
+    });
+    newBand.save((err, savedBand) => {
+      if (err) {
+        console.log(err);
+      }
+      bandId = savedBand._id;
+    });
+    // creates new band
+    const newBand2 = new Band({
+      name: 'Skillet',
+      genre: 'Rock',
+      recentAlbum: 'Unleashed',
+    });
+    // saves the above band to the database
+    newBand2.save((err, savedBand) => {
+      if (err) {
+        console.log(err);
+      }
+      bandId = savedBand._id;
+    });
+    return done();
+  });
 
-    afterEach(done => {
-        Band.remove({}, err => {
-            if (err) console.log(err);
+  afterEach(done => {
+    Band.remove({}, err => {
+      if (err) console.log(err);
+      return done();
+    });
+  });
+
+  // Tests
+  describe(`[GET] /api/bands`, () => {
+    it('should get a list of all the bands in the db', done => {
+      chai
+        .request(server)
+        .get('/api/bands')
+        .end((err, response) => {
+          if (err) {
+            console.log(err);
             return done();
+          }
+          expect(response.status).to.equal(200);
+          expect(response.body).to.be.lengthOf(2);
+          return done();
         });
     });
-    
-    // Tests
-    describe(`[GET] /api/bands`, () => {
-        it('should get a list of all the bands in the db', done => {
-            chai
-            .request(server)
-            .get('/api/bands')
-            .end((err, response) => {
-                if(err) {
-                    console.log(err);
-                    return done();
-                }
-                expect(response.status).to.equal(200);
-                expect(response.data).to.be.lengthOf(2);
-                return done();
-            })
+  });
+
+  describe('[POST] /api/bands', () => {
+    it('should add a new band object to the database', done => {
+      const createBand = {
+        name: 'BonJovi',
+        genre: 'Classic Rock',
+        recentAlbum: 'Bounce',
+      };
+      chai
+        .request(server)
+        .post('/api/bands', {
+          name: 'BonJovi',
+          genre: 'Classic Rock',
+          recentAlbum: 'Bounce',
         })
-    })
-})
+        .end((err, response) => {
+          if (err) {
+            console.log(err);
+            return done();
+          }
+          console.log(response);
+          //   expect(response.status).to.equal(201);
+          expect(response.body).to.haveOwnProperty('_id');
+          expect(response.body).to.haveOwnProperty('name');
+          expect(response.body).to.haveOwnProperty('genre');
+          expect(response.body).to.haveOwnProperty('recentAlbum');
+        });
+    });
+  });
+});
