d3c709e1bc599e599b4a5685859237228281abce
Kenny <khpham24@gmail.com>
2018-04-26 11:16:05 -0600

    cleaned up route tests

--- test/Routes.test.js
+++ test/Routes.test.js
@@ -1,12 +1,6 @@
 const mongoose = require('mongoose');
 const chai = require('chai');
 const chaiHTTP = require('chai-http');
-
-mongoose.connect('mongodb://localhost/test', {}, err => {
-  if (err) return console.log(err);
-  console.log('TEST DB Connectioned accomplished');
-});
-
 const expect = chai.expect;
 const server = require('../server');
 const Band = require('../Model');
@@ -14,6 +8,21 @@ const Band = require('../Model');
 chai.use(chaiHTTP);
 
 describe('Band', () => {
+  let bandId;
+
+  before(done => {
+    mongoose.connect('mongodb://localhost/test', {}, err => {
+      if (err) return console.log(err);
+      console.log('TEST DB Connection Achieved');
+    });
+    done();
+  });
+
+  after(done => {
+    mongoose.connection.close();
+    done();
+  });
+
   describe(`[GET] /api/bands`, () => {
     it('should get a list of all the bands in db', done => {
       chai
@@ -29,37 +38,10 @@ describe('Band', () => {
         });
     });
   });
-});
-
-describe('Bands', () => {
-  let bandId;
-  beforeEach(done => {
-    const newBand = new Band({
-      name: 'Radiohead',
-      genre: 'Alt-rock',
-      numberOfMembers: '2',
-      yearFounded: '1990'
-    });
-    newBand.save((err, savedBand) => {
-      if (err) {
-        console.log(err);
-        done();
-      }
-      bandId = savedBand._id;
-      done();
-    });
-  });
-
-  afterEach(done => {
-    Band.remove({}, err => {
-      if (err) console.log(err);
-      return done();
-    });
-  });
 
   describe(`[POST] /api/bands`, () => {
     it('should post a band in the database', done => {
-      const newBand = new Band({
+      const band = new Band({
         name: 'Radiohead',
         genre: 'Alt-rock',
         numberOfMembers: '2',
@@ -68,7 +50,7 @@ describe('Bands', () => {
       chai
         .request(server)
         .post('/api/bands')
-        .send(newBand)
+        .send(band)
         .end((err, response) => {
           if (err) {
             console.log(err);
@@ -77,14 +59,6 @@ describe('Bands', () => {
           expect(response.status).to.equal(201);
           return done();
         });
-    });
-  });
-});
-
-describe('Band', () => {
-  beforeEach(done => {
-    Band.remove({}, err => {
-      done();
     });
   });
 
