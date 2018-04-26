f598e23981a19829ef0cf05e9326b71426d28d61
Johnathan Huggett <johnathanhuggett@Johnathans-MacBook-Pro.local>
2018-04-26 16:51:23 -0400

    Johnathan Huggett, id problems

--- server.js
+++ server.js
@@ -4,17 +4,31 @@ const server = express();
 
 server.use(express.json());
 server.use(morgan("dev"));
-const Band = require("./models/bands");
+const Bands = require("./models/bands");
 // ROUTES
 
 // test
 server.get("/api/bands", (req, res) => {
-    Band.find({}, (err, bands) => {
+    Bands.find({}, (err, bands) => {
         if (err) {
             res.status(500).json({ errorMessage: "Cannot find bands" });
         }
+
         res.status(200).json(bands);
     });
+});
+
+server.get("/api/bands/:id", (req, res) => {
+    const id = req.params.id.toString();
+    // console.log("----", id);
+    Bands.findById(id)
+        .then(band => {
+            console.log("----", band);
+            res.status(200).json(band);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
 module.exports = server;
--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,21 +2,30 @@ const mongoose = require("mongoose");
 const chai = require("chai");
 const chaiHTTP = require("chai-http");
 
-mongoose.connect("mongodb://localhost/test", {}, err => {
-    if (err) console.log(err);
-    console.log(`\n=== Connected to mongo test ===\n`);
-});
-
 const expect = chai.expect;
 const assert = chai.assert;
 const server = require("../server");
-
-chai.use(chaiHTTP);
-
 //schema
 const Band = require("../models/bands");
 
+chai.use(chaiHTTP);
+
+// MAKE SURE TO SET UP MG IN BEFORE AND AFTER
 describe("Bands", () => {
+    let bandId = [];
+    before(done => {
+        mongoose.connect("mongodb://localhost/test", {}, err => {
+            if (err) console.log(err);
+            console.log(`\n=== Connected to mongo test ===\n`);
+        });
+        done();
+    });
+
+    after(done => {
+        mongoose.connection.close();
+        done();
+    });
+
     beforeEach(done => {
         const bandOne = new Band({
             name: "Bill Withers",
@@ -27,6 +36,7 @@ describe("Bands", () => {
             if (err) {
                 done(err);
             }
+            bandId.push(savedBand._id.toString());
         });
         const bandTwo = new Band({
             name: "Fleet Foxes",
@@ -37,6 +47,7 @@ describe("Bands", () => {
             if (err) {
                 return done(err);
             }
+            bandId.push(savedBand._id.toString());
             done();
         });
     });
@@ -61,36 +72,7 @@ describe("Bands", () => {
                         return done(err);
                     }
                     expect(response.status).to.equal(200);
-                    done();
-                });
-        });
-    });
-
-    describe(`[GET] /api/bands`, () => {
-        it("should return an array of bands", done => {
-            chai
-                .request(server)
-                .get("/api/bands")
-                .end((err, response) => {
-                    if (err) {
-                        return done(err);
-                    }
                     assert.isArray(response.body);
-                    done();
-                });
-        });
-    });
-
-    describe(`[GET] /api/bands`, () => {
-        it("should return properties _id, name, genre, tourStatus", done => {
-            chai
-                .request(server)
-                .get("/api/bands")
-                .end((err, response) => {
-                    // console.log("+++", response.body[0]._id);
-                    if (err) {
-                        return done(err);
-                    }
                     assert.exists(response.body[0].name, "name");
                     assert.exists(response.body[0].genre, "genre");
                     assert.exists(response.body[0].tourStatus, "tourStatus");
@@ -99,4 +81,44 @@ describe("Bands", () => {
                 });
         });
     });
+
+    describe(`[GET] /api/bands/:id`, () => {
+        it("should get the band with matching ID", done => {
+            const id = bandId[0];
+            chai
+                .request(server)
+                .get(`/api/bands/${id}`)
+                // .query({ params: id })
+                .then(response => {
+                    console.log("+++", response.body);
+                    // expect(bandId[0]).to.equal(response.body._id);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+            done();
+        });
+    });
 });
+
+// GET by ID
+// PUT
+// POST
+/*
+.request(server)
+.post(/api/bands)
+.send({//object that you want to create})
+.ebd((err, response) => {console.log(response)})
+*/
+
+// TEST ERR after completed with above
+/*
+it('should fail if bad name, tour status, or genre), done => {
+    chai
+    .request(server)
+    .post(/api/)
+    .send({})
+    .then
+    .catch 
+}
+*/
