b2a0e9f9c1dac1e0efeb62dec906a473a237cd8b
Robert Rak <robertrak@hotmail.com>
2018-04-26 16:38:48 -0400

    post

--- routes.test.js
+++ routes.test.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 const chai = require('chai');
 const chaiHTTP = require('chai-http');
 
-mongoose.connect('mongodb://localhost/test', {}, err => {
-  if (err) return console.log(err);
-  console.log('Test DB power level is over 9000!!!');
-});
-
 const expect = chai.expect;
 const server = require('./server');
 const Char = require('./charModel');
@@ -15,6 +10,19 @@ chai.use(chaiHTTP);
 
 describe('Characters', () => {
   let charId;
+  
+  before(done => {
+    mongoose.connect('mongodb://localhost/test', {}, err => {
+      if (err) return console.log(err);
+      console.log('Test DB power level is over 9000!!!');
+    });
+    done();
+  });
+
+  after(done => {
+    mongoose.connection.close();
+    done();
+  });
 
   beforeEach((done) => {
     const newChar = new Char({
@@ -41,23 +49,29 @@ describe('Characters', () => {
   });
 
   describe(`[GET] /api/dbz-chars`, () => {
-    it('should get a list of characters in the db', done => {
-      chai
-        .request(server)
-        .get('/api/dbz-chars')
-        .end((err, response) => {
-          if (err) {
-            // assert that err should be type status etc.
-            console.log(err);
-            return done();
-          }
-          expect(response.status).to.equal(200);
-          return done();
-        });
+    it('should get a list of characters in the db', () => {
+      return chai
+              .request(server)
+              .get('/api/dbz-chars')
+              .then((res) => {
+                expect(res.status).to.equal(200);
+              });
         // check if its an array
         // check if 200
         // check body
         // check id
+    });
+  });
+
+  describe(`[POST] /api/dbz-chars/add`, () => {
+    it('should add a character to the list in the db', () => {
+      return chai
+              .request(server)
+              .post('/api/dbz-chars/add')
+              .send({ name: 'Goku', race: 'Saiyan', planet: 'Vegeta' })
+              .then((res) => {
+                expect(res.status).to.equal(200);
+              });
     });
   });
 });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
+const Char = require('./charModel');
 
 server.use(express.json());
 server.use(morgan('dev'));
@@ -8,6 +9,20 @@ server.use(morgan('dev'));
 
 server.get('/api/dbz-chars', (req, res) => {
     res.status(200).json({ running: 'The api is good to go' });
+});
+
+server.post('/api/dbz-chars/add', (req, res) => {
+  const { name, race, planet } = req.body;
+  const newChar = new Char({ name: name, race: race, planet: planet });
+
+  newChar
+    .save()
+    .then(savedChar => {
+      res.status(200).json(savedChar);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = server;
