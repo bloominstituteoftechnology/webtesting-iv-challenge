28f7c833fddd8fd7f745deae806af27b75de9ef8
totsuya <supertatortots@gmail.com>
2018-05-24 18:50:28 -0400

    added some more requests

--- server.spec.js
+++ server.spec.js
@@ -11,4 +11,17 @@ describe('server here', () => {
     expect(response.type).toEqual('application/json');
     expect(response.body).toEqual({api: 'API is totally running'});
   })
+
+  it('Should bring a request for api/food with a list of foodstuffs', async () => {
+    const response = await request(server).get('/api/food')
+    expect(response.status).toBe(200)
+    expect(response.type).toBe('application/json')
+    expect(response.body.foods).toHaveLength(5)
+  })
+
+  // it('Should implement a new food item via POST', async () => {
+  //   const response = await request(server).post('/api/food')
+  //   expect
+  // })
 })
+
