f05491eab7c1e694b92afad1e7b21543d80f896a
totsuya <supertatortots@gmail.com>
2018-05-24 18:50:14 -0400

    set up a get and post

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
-
+const Food = require('./foodSchema/foodSchema.js');
 const server = express();
 
 server.get('/', (req, res) => {
   res.status(201).json({api: 'API is totally running'});
 })
 
+server.get('/api/food', (req, res) => {
+  Food.find()
+  .then(foods => res.status(200).send({ foods }))
+  .catch(err => res.status(500).send(err))
+})
+
+server.post('/api/food', (req, res) => {
+  Food.create(req.body)
+  .then(food => res.status(201).send({ food }))
+  .catch(err => res.status(500).send(err))
+})
+
 if (process.env.NODE_ENV !== 'test') {
   server.listen(9000);
 }
+
+
 
 module.exports = server;
