a6cd12a0ac123646d8a8dbbb7c303c645ad0776d
Paul Fang <paulfang@pauls-mbp.home>
2018-05-24 16:50:07 -0700

    working on server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'running!' });
+});
+
+if (process.env.NODE_ENV !== 'test') {
+  server.listen(9000);
+}
+
+module.exports = server;
