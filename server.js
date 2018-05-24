150ab2484fb00db68668f163038bb8a53306977a
Yasin Shuman <email@yasin.io>
2018-05-24 13:03:33 -0700

    sharing with david

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const server = express();
+const User = require("./users/User");
 
+server.use(express.json);
 server.get("/", (req, res) => {
   res.status(200).json({ api: "running!" });
+});
+
+server.post("/create", (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then(newUser => {
+      res.status(200).json(newUser);
+    })
+    .catch(err => {
+      console.log(user);
+      res.status(500).json({ message: "Error creating user", err });
+    });
 });
 
 if (process.env.NODE_ENV !== "test") {
--- server.spec.js
+++ server.spec.js
@@ -4,11 +4,19 @@ const server = require("./server");
 
 describe("server", () => {
   it("should return Ok and a json object from the index route", async () => {
-    const expectedBody = { api: "running!" };
+    let expectedBody = { api: "running!" };
 
     const response = await request(server).get("/");
     expect(response.status).toEqual(200);
     expect(response.type).toEqual("application/json");
+    expect(response.body).toEqual(expectedBody);
+  });
+
+  it("should should return Ok and return a json object of the new user created", async () => {
+    let expectedBody = {}; //user object needs to be put here
+
+    const response = await request(server).get("/");
+    expect(response.status).toEqual(200);
     expect(response.body).toEqual(expectedBody);
   });
 });
