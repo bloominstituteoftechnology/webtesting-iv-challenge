c621e4cc2a5f3c941a69329575d79a2123ec2a49
Nedu Robert <nedumrobert@gmail.com>
2018-05-24 22:23:24 +0100

    implemented index route using TDD

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const server = express()
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'running!' })
+});
+
+if(process.env.NODE_ENV !== 'test') {
+    server.listen(5000);
+}
+
+
+module.exports = server;
---
+++ server.spec.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const server = require('./server');
+
+describe('server', () => {
+    it('should return 200 status code and a json object from the index route', async () => {
+        const expectedBody = { api: 'running!' };
+
+        const response = await request(server).get('/');
+        expect(response.status).toEqual(200);
+        expect(response.type).toEqual('application/json');
+        expect(response.body).toEqual(expectedBody);
+    })
+})
