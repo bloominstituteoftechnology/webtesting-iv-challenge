6375d595d69d3c18c0d7ec1cdaa0497b7ceaf43f
Cruise Brantley <cruise.brantley@gmail.com>
2018-05-24 18:10:56 -0500

    delete id test functioning

--- server.spec.js
+++ server.spec.js
@@ -4,6 +4,21 @@ const server = require("./server");
 const User = require("./users/Users");
 
 describe("server", () => {
+  beforeAll(() => {
+    return mongoose
+      .connect("mongodb://localhost/testingdb")
+      .then(console.log("connected to test db"));
+  });
+
+  beforeEach(() => {});
+  afterEach(() => {
+    return User.remove();
+  });
+
+  afterAll(() => {
+    return mongoose.disconnect();
+  });
+
   it("should return Ok and a json object from the index route", async () => {
     const expectedBody = { api: "running!" };
 
@@ -14,16 +29,23 @@ describe("server", () => {
     expect(response.body).toEqual(expectedBody);
   });
 
-  test("It should response the GET method", () => {
+  test("It should response the DEL method", () => {
     return request(server)
       .delete("/")
       .then(response => {
         expect(response.statusCode).toBe(200);
       });
   });
-  test("It should response the GET method", async () => {
+  
+  test("It should response the DEL user id", async () => {
     const user = { username: "frodo", password: "irrelevant" };
 
     const savedUser = await User.create(user); // new + save
+
+    return request(server)
+      .delete(`/${savedUser._id}`)
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+      });
   });
 });
