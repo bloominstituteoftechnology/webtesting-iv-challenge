24c59b887b48a08816b69a3c577548e35284d9df
Justin Hong <jhong2009@gmail.com>
2018-05-24 16:37:22 -0700

    remmoved mongoose options

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-const { server } = require("./server/server");
+const server = require("./server/server");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 9000;
 
-mongoose.connect("mongodb://localhost/testing-users", {
-	useMongoClient: true
-});
+mongoose.connect("mongodb://localhost/testing-users");
 
 server.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
