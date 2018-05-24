aa5010303d51991be087b697bb169d253fb42939
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-24 15:36:37 -0700

    GET for Dog.spec

--- dogs/Dog.spec.js
+++ dogs/Dog.spec.js
@@ -1,9 +1,69 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 const Dog = require('./Dog')
 
-describe('POST /dogs', () => {
+// describe('Dogs', () => {
+//     let dogId;
+//     beforeEach(done => {
+//         const newDog = new Dog({
+//             name: "Kenzie",
+//             breed: "English Settler"
+//         });
+//     newDog
+//         .save((err, savedDog) => {
+//             if(err) {
+//                 console.log(err);
+//                 done();
+//             }
+//         dogId = savedDog._id;
+//         done();
+//         })
+//     });
+
+//     afterEach(done => {
+//         Dog.remove({}, err => {
+//             if (err) console.log(err);
+//             return done();
+//         });
+//     });
+
+describe('GET to /api/dogs', () => {
+    it('should get a list of dog breeds', async() => {
+        const savedDog = await Dog.create
+            .request(server)
+            .get('/api/dogs')
+            .end((err, response) => {
+                if (err) {
+                    console.log(err);
+                    return done();
+                }
+                expect(response.status).to.equal(200);
+                return done();
+            })
+    });
+});
+
+
+describe('POST to /api/dogPost', () => {
+    it('should add a new dog to DB', done => {
+        Dog.find({})
+        chai
+            .request(server)
+            .post('/api/dogPost')
+            .end((err, response) => {
+                if(err) {
+                    console.log(err);
+                    return done();
+                }
+                // console.log(response);
+                expect(response.status).to.equal(201);
+                return done();
+            })
+    })
+})
+
+    
     beforeAll(() => {
         return mongoose
             .connect('mongodb://localhost/testingdb')
