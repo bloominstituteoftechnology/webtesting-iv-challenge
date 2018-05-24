8ec5c51669820fbbfa4de5b109a5c9bb7ac497b7
cgoez <goez2001@gmail.com>
2018-05-24 14:05:15 -0700

    Add userSchema, test case for username, pw

--- Users/User.spec.js
+++ Users/User.spec.js
@@ -10,23 +10,24 @@ describe("A new user", () => {
       .then(console.log("connected to test db"));
   });
 
-  beforeEach(async () => {
-    const user = { username: "frodo", password: "irrelevant" };
-    const savedUser = await User.create(user);
-  })
-
   afterEach(() => {
     return User.remove();
   });
 
-//   it("should have a username and password", () => {
-//     expect(typeof username).toBe("string");
-//     expect(username.length).toBeGreaterThan(0)
-//     expect(typeof password).toBe("string");
-//     expect(password.length).toBeGreaterThan(0)
-//   });
+  it("should have a username and password", async () => {
+    const user = { username: "frodo", password: "mordor" };
+    const savedUser = await User.create(user);
+
+    expect(typeof savedUser.username).toBe("string");
+    expect(savedUser.username.length).toBeGreaterThan(0);
+    expect(typeof user.password).toBe("string");
+    expect(user.password.length).toBeGreaterThan(0);
+  });
 
   it("should hash the password before saving the user", async () => {
+    const user = { username: "frodo", password: "irrelevant" };
+    const savedUser = await User.create(user);
+
     expect(savedUser.password).not.toEqual(user.password);
     expect(savedUser.password).toHaveLength(60);
   });
