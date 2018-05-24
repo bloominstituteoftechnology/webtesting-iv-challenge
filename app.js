739c5fda217933a4dbba71247791dc4278c8f5bb
Justin Hong <jhong2009@gmail.com>
2018-05-24 13:11:49 -0700

    set up server and mongo db

---
+++ app.js
@@ -0,0 +1,11 @@
+const { server } = require("./server/server");
+const mongoose = require("mongoose");
+const port = process.env.PORT || 9000;
+
+mongoose.connect("mongodb://localhost/testing-users", {
+	useMongoClient: true
+});
+
+server.listen(port, () => {
+	console.log("Server listening on port ${port}");
+});
