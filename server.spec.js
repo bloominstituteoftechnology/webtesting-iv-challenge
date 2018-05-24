f21284ba4b8943a3070a4f82adaecb890a86c4fa
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-24 14:34:10 -0700

    added user file and test file

--- server.spec.js
+++ server.spec.js
@@ -2,13 +2,11 @@ const request = require('supertest');
 const server = require('./server');
 
 describe('server', () => {
-  it('should return Ok and a json object from the index route', async () => {
+  it('should return http status code', async () => {
     const expectedBody = ({ api: 'running' });
 
     const response = await request(server).get('/');
 
     expect(response.status).toEqual(200);
-    expect(response.type).toEqual('application/json');
-    expect(response.body).toEqual(expectedBody);
   });
 });
