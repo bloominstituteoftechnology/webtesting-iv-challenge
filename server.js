5a2a4dc9dd0646a4c92e84940b12790acd2d0853
Nedu Robert <nedumrobert@gmail.com>
2018-05-25 01:23:18 +0100

    implemented routers using TDD

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const express = require('express');
-const server = express()
+
+const User = require('./models/User');
+
+const server = express();
+
+server.use(express.json())
 
 server.get('/', (req, res) => {
     res.status(200).json({ api: 'running!' })
+});
+
+server.get('/api/users', (req, res) => {
+    User.find().then(users => res.status(200).json({ users})).catch(err => res.status(500).json({ error: 'error fetching users'}));
+});
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id).then(user => res.status(200).json({ user })).catch(err => res.status(500).json({ err: 'error finding user with that id'}));
+});
+
+server.post('/api/users', (req, res) => {
+    User.create(req.body).then(user => res.status(201).json({ user })).catch(err => res.status(500).json({ err : 'error creating user'}));
+});
+
+server.put('/api/users/:id', (req, res) => {
+    User.findIdByAndUpdate(req.params.id, req.body).then(user => res.status(200).json({ user })).catch(err => res.status(500).json({ err : 'error updating user'}));
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id).then(msg => res.status(200).json({ msg: 'successfully deleted user' })).catch(err => res.status(500).json({ err : 'error deleting user'}));
 });
 
 if(process.env.NODE_ENV !== 'test') {
--- server.spec.js
+++ server.spec.js
@@ -1,7 +1,31 @@
 const request = require('supertest');
 const server = require('./server');
+const faker = require('faker');
+const mongoose = require('mongoose');
 
-describe('server', () => {
+const User = require('./models/User');
+
+describe('users API', () => {
+    beforeAll(() => {
+        const { userName, password } = faker.internet;
+        const usersData = [];
+        for(let i = 0; i < 10; i++){
+            usersData.push({ username: userName(), password: password() })
+        }
+        return mongoose
+            .connect('mongodb://localhost/testingdb')
+            .then(() => User.insertMany(usersData));
+    });
+
+    afterAll(() => {
+        return User.remove().then(() => mongoose.disconnect());
+    });
+
+    // beforeEach(() => {
+    //     const { userName, password } = faker.internet;
+    //     user = { username: userName(), password: password() }
+    // });
+
     it('should return 200 status code and a json object from the index route', async () => {
         const expectedBody = { api: 'running!' };
 
@@ -9,5 +33,49 @@ describe('server', () => {
         expect(response.status).toEqual(200);
         expect(response.type).toEqual('application/json');
         expect(response.body).toEqual(expectedBody);
+    })
+
+    it('should get all users', async () => {
+        const response = await request(server).get('/api/users');
+        expect(response.status).toEqual(200);
+        expect(response.type).toEqual('application/json');
+        expect(response.body).not.toBeUndefined();
+        expect(response.body.users).toHaveLength(10);
+    })
+
+    it('should get an individual user', async () => {
+        const { userName, password } = faker.internet;
+        const user = { username: userName(), password: password() }
+        const newUser = await User.create(user);
+        const response = await request(server).get(`/api/users/${newUser._id}`);
+
+        expect(response.status).toEqual(200);
+        expect(response.type).toEqual('application/json');
+        expect(response.body).not.toBeUndefined();
+        expect(response.body.user).toMatchObject({username: user.username});
+    })
+
+    it('should create a new user', async () => {
+        const { userName, password } = faker.internet;
+        const user = { username: userName(), password: password() }
+        const response = await request(server).post(`/api/users`).send(user);
+
+        expect(response.status).toEqual(201);
+        expect(response.type).toEqual('application/json');
+        expect(response.body.user).toMatchObject({ username: user.username });
+        expect(response.body.user).toHaveProperty('_id');
+        expect(response.body.user).toHaveProperty('username');
+        expect(response.body.user).toHaveProperty('password');
+    })
+
+    it('should delete a user', async () => {
+        const { userName, password } = faker.internet;
+        const user = { username: userName(), password: password() };
+
+        const newUser = await User.create(user);
+        const response = await request(server).delete(`/api/users/${newUser._id}`);
+        const users = await User.find();
+        expect(response.status).toEqual(200);
+        expect(response.body.msg).toEqual('successfully deleted user');        
     })
 })
