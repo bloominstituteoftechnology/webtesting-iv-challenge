3d5d6fb50a30aa1f581397f6e1168199ddf5f62e
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-21 14:50:29 -0700

    get users route added to server

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ server.get('/', (req, res) => {
     res.status(200).json({ api: 'Server running!' });
 });
 
+server.get("/users", (req, res) => {
+    User.find()
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
 if (process.env.NODE_ENV !== 'test') {
     const port = 5000;
     server.listen(port, () => {
