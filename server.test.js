9b879ad6415814fcbe0112370a4b8e57caf0a570
jesuarva <jesuarva.developer@gmail.com>
2018-06-21 22:03:38 +0200

    DEFINE base asserts for GET

---
+++ server.test.js
@@ -0,0 +1,6 @@
+describe('GET', () => {
+  test('Return a 200 code', () => {});
+  test('Response`s body is of type JSON', () => {});
+});
+describe('POST', () => {});
+describe('DELETE', () => {});
