79a93b3547801938621589736677966e8a572db0
jesuarva <jesuarva.developer@gmail.com>
2018-06-21 22:14:45 +0200

    DEFINE base assertions for POST

--- server.test.js
+++ server.test.js
@@ -2,5 +2,9 @@ describe('GET', () => {
   test('Return a 200 code', () => {});
   test('Response`s body is of type JSON', () => {});
 });
-describe('POST', () => {});
+describe('POST', () => {
+  test('Request`s body must be of type JSON', () => {});
+  test('Collection.create( newDocument ) -> must return the newy created Document', () => {});
+  test('If Document to create contains a password -> Encrypt password', () => {});
+});
 describe('DELETE', () => {});
