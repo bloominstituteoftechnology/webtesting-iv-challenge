999adbd5ce58e67f37d0c8c6373dd7df8ed125dc
Lavell Burton <taylormade800@gmail.com>
2018-06-21 14:01:16 -0700

    initial commit updating files

---
+++ users/userModel.spec.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const user = require('./User');
+
+
+describe('user model', () => {
+    beforeAll(() => {
+        return mongoose.connect('mongodb://localhost/testdb');
+    })
+    afterEach(() => {
+        return user.remove();
+    })
+    afterAll(() => {
+        return mongoose.disconnect();
+    })
+    it('should hash the password before saving the user', async () => {
+        const bilbo = { username: 'bilbo', password: 'baggins' }
+
+        const savedUser = await User.create(bilbo);
+        
+        expect(savedUser.username).toEqual(bilbo.username);
+        expect(savedUser.password).not.toEqual(bilbo.password);
+        expect(savedUser.password).toHaveLength(60);
+    })
+})
