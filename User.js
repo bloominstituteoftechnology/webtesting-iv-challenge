4ae773213934bf55fd71bd40f67518ced94c2481
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-21 14:44:53 -0700

    server and user tests passed

--- User.js
+++ User.js
@@ -26,4 +26,4 @@ userSchema.pre("save", function (next) {
         });
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema, "users");
--- server.js
+++ server.js
@@ -1,50 +1,18 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 const server = express();
-const User = require("./User");
 
 server.use(express.json());
 
-mongoose.connect("mongodb://localhost/serverTesting", {}, err => {
-    if (err) return console.log(err);
-    console.log("DB Connected");
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'Server running!' });
 });
 
-server.get("/", (req, res) => {
-    res.status(200).json({ api: "running!" });
-});
-
-server.get("/users", (req, res) => {
-    User.find()
-        .then(response => {
-            res.status(200).json(response);
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        });
-});
-
-server.post("/users", (req, res) => {
-    const user = new User(req.body);
-    user
-        .save()
-        .then(response => {
-            res.status(201).json(response);
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        });
-});
-
-
-server.delete("/users/:id", (req, res) => {
-    User.findByIdAndRemove(req.params.id)
-        .then(response => res.json({ status: "success, user deleted" }))
-        .catch(err => res.status(500).json(err));
-});
-
-if (process.env.NODE_ENV !== "test") {
-    server.listen(5000);
-}
+if (process.env.NODE_ENV !== 'test') {
+    const port = 5000;
+    server.listen(port, () => {
+        console.log(`Server running port ${port}`)
+    });
+};
 
 module.exports = server;
