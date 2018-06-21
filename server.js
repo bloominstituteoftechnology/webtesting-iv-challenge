3d70d7994098d807d5dc3ca6bb325947f0013bb5
Chris Honda <christopher.honda92@gmail.com>
2018-06-21 15:20:57 -0700

    Added some endpoints; tests still catch on .catch handlers

--- server.js
+++ server.js
@@ -10,6 +10,59 @@ server.get('/', (req, res) => {
     res.status(200).json({ api: 'running' });
 });
 
+server.get('/users', (req, res) => {
+    User
+        .find()
+        .then(users => {
+            res.status(200).json(users)
+        });
+        .catch(error => {
+            res.status(500).json({ 
+                errorMessage: error.message; 
+            });
+        });
+});
+
+server.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    User
+        .findById(id)
+        .then(user => {
+            res.status(200).json(users)
+        });
+        .catch(error => {
+            res.status(500).json({
+                errorMessage: error.message;
+            });
+        });
+})
+
+server.post('/users', (req, res) => {
+    const user = req.body;
+    User
+        .create(user)
+        .then(user => res.status(201).json(user))
+        .catch(error => {
+            res.status(500).json({
+            errorMessage: error.message
+            });
+        });
+});
+
+server.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    User
+        .findByIdAndRemove(id)
+        .then(response => res.status(200).json({
+            response: 'user deleted'
+        }));
+        .catch(error => {
+            res.status(500).json({
+                errorMessage: error.message
+            });
+        });
+});
+
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
