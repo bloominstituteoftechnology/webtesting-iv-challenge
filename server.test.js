caccc63c0c1ffe73fc1569c4db476a72c9c0a62a
jesuarva <jesuarva.developer@gmail.com>
2018-06-21 22:28:26 +0200

    DEFINE base asserts for DELETE

--- server.test.js
+++ server.test.js
@@ -7,4 +7,8 @@ describe('POST', () => {
   test('Collection.create( newDocument ) -> must return the newy created Document', () => {});
   test('If Document to create contains a password -> Encrypt password', () => {});
 });
-describe('DELETE', () => {});
+describe('DELETE', () => {
+  test('Does the Document to delete exist?', () => {});
+  test('MongoDB must confirm the deletion form the database.', () => {});
+  test('The Response must confirm the deletion form the database.', () => {});
+});
