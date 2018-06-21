43085bab5c553c2a0be4523d94ebb9727ca8f7b1
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-06-21 16:48:00 -0400

    Test create a Todo task with default completed value

--- Todo.spec.js
+++ Todo.spec.js
@@ -3,7 +3,25 @@ const mongoose = require('mongoose');
 const Todo = require('./Todo');
 
 describe('Todo model', () => {
-  it('should return a todo', async () => {
-    
+  beforeAll(() => {
+    return mongoose.connect('mongodb://localhost/todoTest');
+  });
+
+  afterEach(() => {
+    return Todo.remove();
+  });
+
+  afterAll(() => {
+    return mongoose.disconnect();
+  });
+
+  it('should create a todo and return a completed task with false value ', async () => {
+    const todo = {task: 'Testing my todo app'};
+
+    const savedTodo = await Todo.create(todo);
+
+    expect(savedTodo.task).toEqual(todo.task);
+    expect(savedTodo.completed).toEqual(false);
+
   })
 })
