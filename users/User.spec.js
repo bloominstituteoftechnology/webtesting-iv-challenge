9d2ec50548f90b0614dc61b1c2bc10aca18a0ba8
udykas <udykas@gmail.com>
2018-06-21 16:34:27 -0400

    Sever tests are set up and passing, working on User tests now

---
+++ users/User.spec.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const User = require('./User');
+
+describe('User model', () => {
+
+    beforeAll(() => {
+        return mongoose.connect('mongodb://localhost/testdb')
+    })
+
+    it('should hash users password before saving', async() => {
+        const user = { username: 'gollum', password: 'precious' };
+
+        const savedUser = await User.create(user);
+
+        expect(savedUser.username).toEqual(user.username);
+        expect(savedUser.password).not.toEqual(user.password);
+        expect(savedUser.password).toHaveLength(60);
+    })
+})
