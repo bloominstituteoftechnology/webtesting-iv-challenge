da2414774408fe63e33aa39510beb30aaec8e027
Chris Honda <christopher.honda92@gmail.com>
2018-06-21 14:15:07 -0700

    Added port and server.listen to server file for testing

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const server = express();
 
+const port = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost/servertestDB');
+
 server.get('/', (req, res) => {
     res.status(200).json({ api: 'running' });
+});
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 });
 
 module.exports = server;
