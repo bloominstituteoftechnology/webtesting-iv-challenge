79dda50d20db56256deb8cdb47771391f882b516
Bee C <b.choeung@gmail.com>
2018-06-28 18:28:43 -0400

    created two tests for POST and 2 tests for DELETE

--- users/User.spec.js
+++ users/User.spec.js
@@ -24,16 +24,55 @@ describe('user model', () => {
         expect(savedUser.password).not.toEqual(medusa.password);
         expect(savedUser.password).toHaveLength(60);
     });
-
+    //--testing stuff--
     const userName = {
         string: true,
         number: false,
     };
     describe('username should be a string', () => {
-        test('is a string named medusa', () => {
+        it('is a string named medusa', () => {
             expect(userName.string).toBeTruthy();
         });
     });
+ 
 
+    //1st create test
+    describe('/api/post', () => {
+        it('should add a user with status code 201 , created', () => {
+            const statusCode = 201;
+            const addUser = {username:'medusa', password: 'password'};
+
+            expect(addUser).toEqual(addUser);
+        });
+    });
+
+     //2nd create test
+     describe('/api/post', () => {
+        it('should add a user that contains a password status code 201, created', () => {
+            const statusCode = 201;
+            const userDetails = {password: 'password'};
+
+            expect(addUser).toEqual(addUser);
+        });
+    });
+
+        //1st delete test
+        describe('/api/delete', () => {
+            it('should delete a user with status code 204 , no content', () => {
+                const statusCode = 204;
+                const deleteUser = {username:'medusa', password: 'password'};
+    
+                expect(deleteUser).toBeNull();
+            });
+        });
+
+        //2nd delete test
+        describe('/api/delete', () => {
+            it('should render a button', () => {
+                const button = header.find('.toggle');
+        
+                expect(button.length).toEqual(1);
+            });
+        });
 
 });
