15d4acf36f0b90ebd1ea5f73f0d94fd8fe78509d
april <martinezapril56@yahoo.com>
2018-06-28 13:45:35 -0400

    still watching video

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const express = require( 'express' );
-const mongoose = require('mongoose')
+
+
+const server = express();
+
+server.get( '/', ( req, res ) =>
+{
+    res.status( 200 ).json( { api:'running'} );
+} );
+
+module.exports = server;
