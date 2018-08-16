dabbdf0a9e1af05626d17bebc5692d378f34d93a
emaquino44 <prexy44@yahoo.com>
2018-08-16 12:36:21 -0700

    added tests for post and created Post endpoints

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require('express');
 
+const server = express();
+
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+  res.status(200).send('API is running');
+});
+
+server.post('/greet/:name', (req, res) => {
+	const { lastName } = req.body;
+	const { name } = req.params;
+
+	res.status(200).json({ hello: `${name} ${lastName}`});
+});
+
+
+module.exports=server;
--- server.spec.js
+++ server.spec.js
@@ -12,4 +12,28 @@ describe('server.js', ()=> {
 				expect(response.status).toEqual(expected);
 				});
 			});
+
+
+describe('POST /greet/:name', () => {
+	it('should return { hello: name } when name is provided inside body ', async () => {
+
+	//arrange
+		const expected = { hello: 'Fred Flinstones' };
+
+	//act
+		const response = await request(server)
+			.post('/greet/Fred')
+			.send({ lastName: 'Flinstones' });
+
+			//assert
+			expect(response.body).toEqual(expected);
+		});
+	it('should return status code 200 Ok', async ()=> {
+				const expected = 200;
+
+				const response = await request(server).post('/greet/:name');
+
+				expect(response.status).toEqual(expected);
+			});
+	});
 	});
