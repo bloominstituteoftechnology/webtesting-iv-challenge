5388210cca2197d75160c31c387bec6ecc8f26ad
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2018-08-16 13:30:07 -0700

    initial commit

---
+++ server.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest')
+
+const server = require('./server.js')
+
+describe('server.js', () => {
+  describe('POST /users', () => {})
+
+  describe('DELETE /users', () => {})
+})
