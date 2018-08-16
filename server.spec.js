1a95e0d367f306f65a84417e28c4c174c9809b0c
Kevin <brackkevin@gmail.com>
2018-08-16 16:41:45 -0500

    POST: smurfs endpoint tests written

--- server.spec.js
+++ server.spec.js
@@ -28,12 +28,84 @@ describe("server.js", () => {
     });
   });
 
-  //   describe("POST: smurf endpoint (/smurfs)", () => {
-  //       it("should return status code 200 ok", async () => {
-  //           const expected = 200;
+  describe("POST: smurf endpoint (/smurfs)", () => {
+    it("should return status code 200 ok when correct data is provided", async () => {
+      const expected = 200;
 
-  //           const res = await request(server).post("/smurfs");
-  //           expect(res.status).toEqual(expected);
-  //       })
-  //   })
+      const res = await request(server)
+        .post("/smurfs")
+        .send({
+          name: "Smurfette",
+          age: "200",
+          height: "5cm"
+        });
+      expect(res.status).toEqual(expected);
+    });
+
+    it("should return the new smurfs array when correct data is provided", async () => {
+      const expected =
+        '[{"name":"Brainey","age":200,"height":"5cm"},{"name":"Smurfette","age":"200","height":"5cm","id":0},{"name":"Smurferino","age":"200","height":"5cm","id":1}]';
+
+      const res = await request(server)
+        .post("/smurfs")
+        .send({
+          name: "Smurferino",
+          age: "200",
+          height: "5cm"
+        });
+      expect(res.text).toEqual(expected);
+    });
+
+    it("should return error code 422 when incomplete data is passed", async () => {
+      const expected = 422;
+
+      const res = await request(server)
+        .post("/smurfs")
+        .send({
+          age: "200",
+          height: "5cm"
+        });
+      expect(res.status).toEqual(expected);
+    });
+
+    it("should return appropriate error message when incomplete data is passed", async () => {
+      const expected =
+        '{"Error":"Ya gone did smurfed! Name/Age/Height are all required to create a smurf in the smurf DB."}';
+
+      const res = await request(server)
+        .post("/smurfs")
+        .send({
+          age: "200",
+          height: "5cm"
+        });
+      expect(res.text).toEqual(expected);
+    });
+
+    it("should return error code 422 when there is a smurf already with that name", async () => {
+      const expected = 422;
+
+      const res = await request(server)
+        .post("/smurfs")
+        .send({
+          name: "Brainey",
+          age: "200",
+          height: "5cm"
+        });
+      expect(res.status).toEqual(expected);
+    });
+
+    it("should return appropriate error text when there is a smurf already with that name", async () => {
+      const expected =
+        '{"Error":"Ya gone did smurfed! Brainey already exists in the smurf DB."}';
+
+      const res = await request(server)
+        .post("/smurfs")
+        .send({
+          name: "Brainey",
+          age: "200",
+          height: "5cm"
+        });
+      expect(res.text).toEqual(expected);
+    });
+  });
 });
