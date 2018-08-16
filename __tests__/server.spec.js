050e9e92e6244a4371aef85c48a4c82882a9aef1
cmok4290 <cmok4290@gmail.com>
2018-08-16 16:47:58 -0400

    added post tests

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -3,38 +3,69 @@ const server = require('../server.js');
 
 describe('server.js', () => {
     describe('index route', () => {
-        it('should return an OK status code from the index route', async () => {
-            // option config
-            // 0 = async/await
-            // 1 = promises
-            let option = 0;
-            const statusCode = 200;
+        describe('get', () => {
+            it('should return an OK status code from the get route', async () => {
+                // option config
+                // 0 = async/await
+                // 1 = promises
+                let option = 0;
+                const statusCode = 200;
 
-            if(option) {
-                let response;
-                return request(server).get('/').then(res => {
-                    response = res;
+                if(option === 1) {
+                    let response;
+                    return request(server).get('/').then(res => {
+                        response = res;
+                        
+                        expect(response.status).toEqual(statusCode);
+                    });
+                } else {
+                    const response = await request(server).get('/');
 
                     expect(response.status).toEqual(statusCode);
-                });
-            } else {
+                }
+            });
+
+            it('should return a response body from the index route', async () => {
+                const expectedBody = { api: 'running' };
                 const response = await request(server).get('/');
 
+                expect(response.body).toEqual(expectedBody);
+            });
+
+            it('should return a response type from the index route', async () => {
+                const response = await request(server).get('/');
+
+                expect(response.type).toEqual('application/json');
+            });
+        });
+
+        describe('post', () => {
+            it('should return an OK status code from the post route', async () => {
+                const statusCode = 200;
+                const response = await request(server)
+                    .post('/')
+                    .send({ first: 'frodo', last: 'baggins' });
+
                 expect(response.status).toEqual(statusCode);
-            }
+            });
+
+            it('should return object when request body provided', async () => {
+                const expected = { hello: 'frodo baggins' };
+
+                const response = await request(server)
+                    .post('/greet/frodo')
+                    .send({ lastName: 'baggins' });
+            
+                expect(response.body).toEqual(expected);
+            });
         });
 
-        it('should return a JSON object from the index route', async () => {
-            const expectedBody = { api: 'running' };
-            const response = await request(server).get('/');
+        describe('put', () => {
 
-            expect(response.body).toEqual(expectedBody);
         });
 
-        it('should return a JSON object from the index route', async () => {
-            const response = await request(server).get('/');
+        describe('delete', () => {
 
-            expect(response.type).toEqual('application/json');
         });
     });
 });
--- server.js
+++ server.js
@@ -7,4 +7,17 @@ server.get('/', (req, res) => {
     res.status(200).json({ api: 'running' });
 });
 
+server.post('/', (req, res) => {
+    const { first, last } = req.body;
+
+    res.status(200).json({ hello: `${first} ${last}` });
+});
+
+server.post('/greet/:name', (req, res) => {
+    const { lastName } = req.body;
+    const { name } = req.params;
+
+    res.status(200).json({ hello: `${name} ${lastName}` });
+});
+
 module.exports = server;
