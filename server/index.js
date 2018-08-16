ee427c426837961365020d4fa542ced967c0d9de
Mariana Cerda <raymond_grc@yahoo.com>
2018-08-16 16:34:44 -0500

    Add helmet js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const helmet = require('helmet')
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet())
 
 server.get('/', (req, res) => {
   res.status(200).json({ success: true, data: { api: 'running' } });
