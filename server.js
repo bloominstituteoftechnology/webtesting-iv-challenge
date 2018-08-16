e8b83d94f76f47321845df7f0cde8ac8dc9b45aa
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2018-08-16 14:00:26 -0700

    implement POST test and POST server logic

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require('express')
 const server = express()
 server.use(express.json())
 
-server.get('/', (req, res) => {
-  res.status(200).json({ api: 'Server Online' })
+server.post('/api/users', (req, res) => {
+  const { username, password } = req.body
+  if (!username || !password) {
+    return res.status(400).json({ msg: 'username and password are required.' })
+  }
+  res.status(201).json({ msg: `user account (${username}) created.` })
 })
 
 module.exports = server
--- server.test.js
+++ server.test.js
@@ -3,7 +3,37 @@ const request = require('supertest')
 const server = require('./server.js')
 
 describe('server.js', () => {
-  describe('POST /users', () => {})
+  describe('POST /users', () => {
+    it('should return status code 400 if username or password is not provided', async () => {
+      const user1 = { username: 'Cesar' }
+      const expected = 400
+
+      const res1 = await request(server).post('/api/users').send(user1)
+      expect(res1.status).toEqual(expected)
+
+      const user2 = { password: 'letmein' }
+      const res2 = await request(server).post('/api/users').send(user2)
+      expect(res2.status).toEqual(expected)
+    })
+
+    it('should return status code 201 upon user creation', async () => {
+      const user = { username: 'Cesar', password: 'letmein' }
+      const expected = 201
+
+      const res = await request(server).post('/api/users').send(user)
+
+      expect(res.status).toEqual(expected)
+    })
+
+    it('should return {msg: user account ([username]) created.} upon user creation', async () => {
+      const user = { username: 'Cesar', password: 'letmein' }
+      const expected = { msg: `user account (${user.username}) created.` }
+
+      const res = await request(server).post('/api/users').send(user)
+
+      expect(res.body).toEqual(expected)
+    })
+  })
 
   describe('DELETE /users', () => {})
 })
