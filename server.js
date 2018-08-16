6e33fca81448a7bb296439c000b48dc32c7587b1
DirupT <iamcooled@gmail.com>
2018-08-16 14:14:55 -0500

    added tests for PUT / cleaned up code

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ server.post('/greet', (req, res) => {
     const { name } = req.body;
 
     res.status(200).json({ hello: name });
+});
+
+server.put('/greet/:name', (req, res) => {
+    const { name } = req.body;
+
+    res.status(200).json({ hello: `Changed name from ${req.params.name} to ${name}` });
+});
+
+server.delete('/greet/:name', (req, res) => {
+    res.status(200).json({ removed: req.params.name })
 })
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -3,15 +3,13 @@ const request = require('supertest');
 const server = require('./server');
 
 describe('server.js', () => {
-    describe('GET', () => {
+    describe('GET /', () => {
         it('should return status code 200', async () => {
-            const res = await request(server).get('/');
-            expect(res.status).toEqual(200);
+            await request(server).get('/').expect(200);
         });
 
         it('should return JSON', async () => {
-            const res = await request(server).get('/');
-            expect(res.type).toEqual('application/json');
+            await request(server).get('/').expect('Content-Type', /json/);
         });
 
         it(`should return an object containing { api: 'running' } `, async () => {
@@ -22,19 +20,11 @@ describe('server.js', () => {
 
     describe('POST /greet', () => {
         it('should return status code 200', async () => {
-            const res = await request(server)
-                .post('/greet')
-                .send({ name: 'Tanner' });
-
-            expect(res.status).toEqual(200);
+            await request(server).post('/greet').send({ name: 'Tanner' }).expect(200);
         });
 
         it('should return JSON', async () => {
-            const res = await request(server)
-                .post('/greet')
-                .send({ name: 'Tanner' });
-
-            expect(res.type).toEqual('application/json');
+            await request(server).post('/greet').send({ name: 'Tanner' }).expect('Content-Type', /json/);
         });
 
         it(`should return { hello: name } when name is provided inside body`, async () => {
@@ -44,6 +34,33 @@ describe('server.js', () => {
                 .send({ name: 'Tanner' });
 
             expect(res.body).toEqual(expected);
+        });
+    });
+
+    describe('PUT /greet/:id', () => {
+        it('should return status code 200', async () => {
+            await request(server).put('/greet/tanner').send({ name: 'Dan' }).expect(200);
+        });
+
+        it('should return JSON', async () => {
+            await request(server).put('/greet/tanner').send({ name: 'Dan' }).expect('Content-Type', /json/);
+        });
+
+        it(`should return { hello: name } when name is provided inside body`, async () => {
+            const expected = { hello: 'Changed name from tanner to Dan' };
+            const res = await request(server)
+                .put('/greet/tanner')
+                .send({ name: 'Dan' });
+
+            expect(res.body).toEqual(expected);
+        });
+    });
+
+    describe('DELETE /greet/:id', () => {
+        it('should return status code 200', async () => {
+            const res = await request(server)
+                .delete('/greet/tanner')
+                .expect(200);
         });
     });
 });
