3cf996a4ab4cec6383e36d86d8a5855a12757454
Wdifulvio523 <w.difulvio523@gmail.com>
2018-08-16 16:44:12 -0400

    Created more tests for deleting a country

--- server.js
+++ server.js
@@ -7,11 +7,21 @@ server.get('/', (req, res) => {
     res.status(200).json({ api: 'running' });
   });
 
+  
+  server.get('/countries', (req, res) => {
+    res.status(200).json({countries: "here are some countries"});
+  });
 
   server.post('/countries', (req, res) => {
     const {country} = req.body;
 
       res.status(201).json({country});
+  });
+
+  server.delete('/countries/:country', (req, res) => {
+      const { country } = req.params;
+
+      res.status(200).json({message: `Success in deleting ${country}`});
   })
   
   module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -2,46 +2,90 @@ const request = require("supertest");
 
 const server = require("./server.js");
 
-describe('server.js', () => {
-    describe('root endpoint (/)', () => {
-        it('should return status code 200 OK', async () => {
-            const expected = 200;
-            const res = await request(server).get('/');
+describe("server.js", () => {
+  describe("root endpoint (/)", () => {
 
-            expect (res.status).toEqual(expected);
-        });
-        it("should return JSON", async () => {
-            const res = await request(server).get("/");
-      
-            expect(res.type).toEqual("application/json");
-          });
-
-          it("should return object that looks like {api:running} ", async () => {
-            const expected = { api: "running" };
-      
-            const res = await request(server).get("/");
-      
-            expect(res.body).toEqual(expected);
-          });
+    it("should return status code 200 OK", async () => {
+      const expected = 200;
+      const res = await request(server).get("/");
+      expect(res.status).toEqual(expected);
     });
 
-    describe('POST endpoint (/countries)', () => {
-        it('should return status code 201 created', async () => {
-            const expected = 201;
-
-            const res = await request(server)
-            .post('/countries')
-            .send({country: 'USA'});
-            expect(res.status).toEqual(expected)
-        });
-
-        it('should return created country', async () => {
-            const expected = {country: "USA"};
-
-            const res = await request(server)
-            .post('/countries')
-            .send({country: 'USA'});
-            expect(res.body).toEqual(expected)
-        });
+    it("should return JSON", async () => {
+      const res = await request(server).get("/");
+      expect(res.type).toEqual("application/json");
     });
+
+    it("should return object that looks like {api:running} ", async () => {
+      const expected = { api: "running" };
+      const res = await request(server).get("/");
+      expect(res.body).toEqual(expected);
+    });
+  });
+
+  describe("GET endpoint(/countries)", () => {
+
+    it("should return status code 200 OK", async () => {
+      const expected = 200;
+      const res = await request(server).get("/countries");
+      expect(res.status).toEqual(expected);
+    });
+
+    it("should return JSON", async () => {
+      const res = await request(server).get("/countries");
+      expect(res.type).toEqual("application/json");
+    });
+
+    it("should return object that looks like expected ", async () => {
+      const expected = { countries: "here are some countries" };
+      const res = await request(server).get("/countries");
+      expect(res.body).toEqual(expected);
+    });
+  });
+
+  describe("POST endpoint (/countries)", () => {
+
+    it("should return status code 201 created", async () => {
+      const expected = 201;
+      const res = await request(server)
+        .post("/countries")
+        .send({ country: "USA" });
+      expect(res.status).toEqual(expected);
+    });
+
+    it("should return JSON", async () => {
+        const res = await request(server).get("/countries");
+        expect(res.type).toEqual("application/json");
+      });
+
+    it("should return created country", async () => {
+      const expected = { country: "USA" };
+      const res = await request(server)
+        .post("/countries")
+        .send({ country: "USA" });
+      expect(res.body).toEqual(expected);
+    });
+  });
+
+  describe("DELETE endpoint (/countries/:country)", () => {
+    
+    it("should return status code 200 OK", async () => {
+      const expected = 200;
+      const res = await request(server).delete("/countries/:country");
+      expect(res.status).toEqual(expected);
+    });
+
+    it("should return JSON", async () => {
+        const res = await request(server).delete("/countries/USA");
+        expect(res.type).toEqual("application/json");
+      });
+
+    it("should return message that says `Success in deleting {country}`", async () => {
+      const expected = { message: `Success in deleting USA` };
+
+      const res = await request(server).delete("/countries/USA");
+
+      expect(res.body).toEqual(expected);
+    });
+  });
 });
