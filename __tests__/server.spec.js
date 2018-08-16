fc92ed5833aba01a47b8396061c052e3d17bc502
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-08-16 19:27:31 -0400

    Initial commit

---
+++ __tests__/server.spec.js
@@ -0,0 +1,3 @@
+const request = require('supertest');
+const server = require('../server');
+
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Up and running...')
+})
+
+const port = 8000;
+
+server.listen(port, function() {
+    console.log(`\n--- Web API Listening on http://localhost:${port} ---\n`);
+})
+
+module.exports = server;
