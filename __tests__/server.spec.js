c4b83d3ad040f7672471599ec2686290d4cc3713
Austin Merando <ajmerando1@yahoo.com>
2018-08-16 15:50:02 -0700

    post test

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -7,11 +7,26 @@ describe('server.js', () => {
         it('should return status code 200 OK', async () => {
             const response = await request(server).get('/');
 
-            expect (response.status).toEqual(200);
+            expect(response.status).toEqual(200);
         });
 
         it('should return JSON', async () => {
+            const response = await request(server).get('/');
 
+            expect(response.type).toEqual('application/json')
+        })
+    })
+
+    describe('post endpoint', () => {
+        it('should return name', async () => {
+            const expected = { hello: 'flint bean'};
+
+            const response = await request(server)
+            .post('/greet/flint')
+            .send({lastName: 'bean'})
+
+            expect(response.body).toEqual(expected);
         })
     })
 })
+
