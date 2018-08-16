93f673ec9c959eb2b5597e72d6c5ce258cf62f31
nchen0 <nicky.chen0@gmail.com>
2018-08-16 14:44:17 -0700

    add

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
-server.get("/", () => {
+server.get("/", (req, res) => {
   res.status(200).json({ api: "running" });
 });
 
+server.post("/greet", (req, res) => {
+  const { name } = req.body;
+  res.status(201).json({ hello: name });
+});
+
+server.post("/greet/:name", (req, res) => {
+  const { name } = req.params;
+  res.status(201).json({ hello: name });
+});
+
+server.delete("/greet/:name", (req, res) => {
+  const { name } = req.params;
+  res.status(400).json("Frodo Deleted");
+});
 module.exports = server;
--- server.specs.test.js
+++ server.specs.test.js
@@ -1,2 +1,48 @@
 const request = require("supertest");
 const server = require("./server");
+
+describe("server.js", () => {
+  describe("GET routes", () => {
+    it("should return status code 200 OK", async () => {
+      const expected = 200;
+      const response = await request(server).get("/");
+      expect(response.status).toEqual(expected);
+    });
+
+    it("should return JSON", async () => {
+      const response = await request(server).get("/");
+      expect(response.type).toEqual("application/json");
+    });
+
+    it("should return body", async () => {
+      const expected = { api: "running" };
+      const response = await request(server).get("/");
+
+      expect(response.body).toEqual(expected);
+    });
+  });
+
+  describe("POST routes", () => {
+    it('should return {hello: "name"} when provided a name inside the body.', async () => {
+      const expected = { hello: "Bilbo" };
+      const response = await request(server)
+        .post("/greet")
+        .send({ name: "Bilbo" });
+      expect(response.body).toEqual(expected);
+    });
+
+    it("should return {hello: name} when provided a name as a parameter", async () => {
+      const expected = { hello: "Frodo" };
+      const response = await request(server).post("/greet/Frodo");
+
+      expect(response.body).toEqual(expected);
+    });
+  });
+
+  describe("DELETE routes", () => {
+    it("should delete a name when provided the right id", async () => {
+      const response = await request(server).delete("/greet/Frodo");
+      expect(response.body).toEqual("Frodo Deleted");
+    });
+  });
+});
