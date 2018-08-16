f2f6288c8ef903c140db086d3199bd150310198c
markdavidwilliams <markwilliams.aus@gmail.com>
2018-08-16 15:45:34 -0500

    completed POST and DELETE endpoint testing

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const server = require('../server');
 
 describe('server', () => {
-  describe('root endpoint (/)', () => {
+  describe('ROOT endpoint (/)', () => {
     it('should ask if the server is a teapot', async () => {
       const response = await request(server).get('/');
       expect(response.status).toEqual(418);
@@ -13,4 +13,20 @@ describe('server', () => {
       expect(response.type).toEqual('application/json');
     });
   });
+
+  describe('POST endpoint (/)', () => {
+    it('should return drinkable tea when tea type is provided in the body', async () => {
+      const expected = { tea: 'green tea is ready' };
+      const response = await request(server).post('/').send({ type: 'green' });
+      expect(response.body).toEqual(expected);
+    });
+  });
+
+  describe('DELETE endpoint (/:tea)', () => {
+    it('should drink the tea requested provided in the parameters', async () => {
+      const expected = { tea: 'the green tea was delicious' };
+      const response = await request(server).delete('/green');
+      expect(response.body).toEqual(expected);
+    })
+  })
 })
--- server.js
+++ server.js
@@ -4,10 +4,28 @@ const server = express();
 
 server.use(express.json());
 
+let teas = [];
+
 server.get('/', (req, res) => {
   res
     .status(418)
     .json();
-})
+});
+
+server.post('/', (req, res) => {
+  const teaType = req.body;
+  teas = [ ...teas, teaType ];
+  res
+    .status(418)
+    .json({ tea: `${teaType.type} tea is ready`});
+});
+
+server.delete('/:tea', (req, res) => {
+const teaToDrink = req.params.tea;
+teas = teas.filter(tea => tea.teaType != teaToDrink);
+res
+  .status(418)
+  .json({ tea: `the ${teaToDrink} tea was delicious` });
+});
 
 module.exports = server;
