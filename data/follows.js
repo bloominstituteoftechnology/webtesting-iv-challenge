650e7be82293628a9ee79ab0237c920f5eb7e2af
Kevin <brackkevin@gmail.com>
2018-08-16 15:37:18 -0500

    Init, and import Smurf DB

---
+++ data/follows.js
@@ -0,0 +1,37 @@
+const LNFollowing = {
+  total: 6,
+  data: [
+    {
+      from_id: "73098479",
+      to_id: "31557216",
+      followed_at: "2018-07-20T18:19:57Z"
+    },
+    {
+      from_id: "73098479",
+      to_id: "26610234",
+      followed_at: "2018-07-15T19:13:06Z"
+    },
+    {
+      from_id: "73098479",
+      to_id: "128489946",
+      followed_at: "2018-07-12T08:33:00Z"
+    },
+    {
+      from_id: "73098479",
+      to_id: "124510977",
+      followed_at: "2018-07-09T23:19:01Z"
+    },
+    {
+      from_id: "73098479",
+      to_id: "106125347",
+      followed_at: "2018-07-09T22:47:20Z"
+    },
+    {
+      from_id: "73098479",
+      to_id: "142965689",
+      followed_at: "2018-07-08T16:08:02Z"
+    }
+  ]
+};
+
+export default LNFollowing;
