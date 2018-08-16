1a3f37d8c96333f759371d14cb7e0653354cfbd0
Randy Calderon <randycweb@gmail.com>
2018-08-16 15:14:59 -0700

    Added tests 200 status code species

--- server.js
+++ server.js
@@ -6,16 +6,24 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
     if (res) {
-    res.status(200).json({ path: 'exists' });
+        res.status(200).json({ path: 'exists' });
     } else {
-        res.status(400).json({ path: "doesn't exist"})
+        res.status(400).json({ path: "doesn't exist" })
+    }
+})
+
+server.get('/species', (req,res) => {
+    if (res) {
+        res.status(200).json({ path: 'exists' });
+    } else {
+        res.status(400).json({ path: "doesn't exist" })
     }
 })
 
 server.post('/species', (req, res) => {
     const { animal } = req.body;
 
-    res.status(200).json({ species: `${animal}`});
+    res.status(200).json({ species: `${animal}` });
 })
 
 server.delete('/species/:id', (req, res) => {
--- server.spec.js
+++ server.spec.js
@@ -11,25 +11,38 @@ describe('server get requests', () => {
         expect(response.status).toEqual(OK);
     })
 
-    it("returns status code 400", async() => {
+    it("returns status code 400", async () => {
         const NOT_FOUND = 404;
 
-        const response = await request(server).get('/lime')
-        expect(response.status).toEqual(NOT_FOUND)
+        const response = await request(server).get('/lime');
+        expect(response.status).toEqual(NOT_FOUND);
     })
 
-    it('creates a species', async () => {
-        const species = { species: 'Jaguar' };
+    describe('species endpoints and functionality', () => {
+        it('returns a status code of 200', async () => {
+            const OK = 200;
 
-        const response = await request(server).post('/species').send( {animal: 'Jaguar'} )
-        expect(response.body).toEqual(species);
+            const response = await request(server).get('/species');
+            expect(response.status).toEqual(OK);
+        })
+
+        it('returns  a status code for creation', async () => {
+            const response = await request(server).post('/species').send({ animal: 'liger'})
+            expect(response.status).toEqual(200);
+        })
+
+        it('creates a species', async () => {
+            const species = { species: 'Jaguar' };
+
+            const response = await request(server).post('/species').send({ animal: 'Jaguar' });
+            expect(response.body).toEqual(species);
+        })
+
+        it('deletes a species', async () => {
+            const species = {};
+
+            const response = await request(server).delete('/species/1');
+            expect(response.body).toEqual(species);
+        })
     })
-
-    it('deletes a species', async () => {
-        const species = {};
-
-        const response = await request(server).delete('/species/1') 
-        expect(response.body).toEqual(species)
-    })
-
 })
