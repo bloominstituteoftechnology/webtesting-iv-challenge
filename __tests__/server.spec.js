2a30fc411eabde95d74cb9c4bad48face96d4c4f
markdavidwilliams <markwilliams.aus@gmail.com>
2018-08-16 15:54:47 -0500

    MVP completed: wrote 2 tests per endpoint

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -15,6 +15,11 @@ describe('server', () => {
   });
 
   describe('POST endpoint (/)', () => {
+    it('should reassure the client that it is indeed a teapot', async () => {
+      const response = await request(server).post('/').send({ type: 'chamomile' });
+      expect(response.status).toEqual(418);
+    })
+
     it('should return drinkable tea when tea type is provided in the body', async () => {
       const expected = { tea: 'green tea is ready' };
       const response = await request(server).post('/').send({ type: 'green' });
@@ -23,10 +28,15 @@ describe('server', () => {
   });
 
   describe('DELETE endpoint (/:tea)', () => {
+    it('should continue to be a teapot', async () => {
+      const response = await request(server).delete('/chamomile');
+      expect(response.status).toEqual(418);
+    })
+
     it('should drink the tea requested provided in the parameters', async () => {
       const expected = { tea: 'the green tea was delicious' };
       const response = await request(server).delete('/green');
       expect(response.body).toEqual(expected);
-    })
-  })
+    });
+  });
 })
