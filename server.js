2575dec5feed94e58844800d1c618e8590efee2e
nchen0 <nicky.chen0@gmail.com>
2018-08-16 13:58:37 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const server = express();
+server.use(express.json());
+
+server.get("/", () => {
+  res.status(200).json({ api: "running" });
+});
+
+module.exports = server;
