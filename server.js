ebf8f27e8c6a7e8b0777ef14545f6f4344adca5c
Ari Mercado <ari@Apples-Mac-3.local>
2018-08-16 15:54:42 -0700

    added delete endpoint and two tests

--- server.js
+++ server.js
@@ -9,10 +9,24 @@ server.get('/', (req, res) => {
 
 });
 
-server.post('/greet', (req, res) => {
+server.post('/names', (req, res) => {
   const { name } = req.body;
 
   res.status(200).json( { hello: name });
+})
+
+server.post('/names/:name', (req, res) => {
+  const { lastName } = req.body;
+  const { name } = req.params;
+
+  res.status(200).json({ hello: `${name} ${lastName}` });
+});
+
+server.delete('/names/:name', (req, res) => {
+  let { name } = req.body
+  name ? res.status(404).json({ error: 'Name not found' }) : null
+
+  res.status(204)
 })
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -15,19 +15,67 @@ describe('server.js', () => {
       expect(response.body).toEqual(expected);
     })
   }); 
+
+  describe('POST/ name', () => {
+    it ("should return { hello: name } when name provided inside body", async () => {
+      //arrange
+      const expected = { hello: 'bilbo' };
+
+      // act
+      const response = await request(server)
+        .post('/names')
+        .send({ name: 'bilbo'})
+
+      // assert
+      expect(response.body).toEqual(expected);
+    })
+    it('should respond with status code 200', async () => {
+      const response = await request(server)
+        .post('/names/')
+        .send({ lastName: 'baggins' });
+      expect(response.status).toBe(200)
+    })
+  })
+
+  describe('POST /names/:name', () => {
+    it('should return { hello: name } when name provided inside body', async () => {
+      // arrange
+      const expected = { hello: 'frodo baggins' };
+
+      // act
+      const response = await request(server)
+        .post('/names/frodo')
+        .send({ lastName: 'baggins' });
+
+      // assert
+      expect(response.body).toEqual(expected);
+
+    });
+
+    it('should respond with status code 200', async () => {
+      const response = await request(server)
+        .post('/names/frodo')
+        .send({ lastName: 'baggins' });
+      expect(response.status).toBe(200)
+    })
+  });
+
+
+
+  describe('DELETE /delete', () => {
+    it('should delete a user with status code 204', async () => {
+        const name = { name: 'Freddy' }
+
+        const reponse = await request(server)
+          .delete('/names/:name')
+          .send( name )
+
+        expect(response.status).toBe(204)
+    })
+    it('should return 404 if name is not found', async () => {
+        const response = await request(server).delete('/names/:name').send({ name })
+        expect(response.status).toBe(404)
+    })
+  })
 });
 
-describe('POST/ greet', () => {
-  it ("should return { hello: name } when name provided inside body", async () => {
-    //arrange
-    const expected = { hello: 'bilbo' };
-
-    // act
-    const response = await request(server)
-      .post('/greet')
-      .send({ name: 'bilbo'})
-
-    // assert
-    expect(response.body).toEqual(expected);
-  })
-})
