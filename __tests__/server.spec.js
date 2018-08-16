33a7d60c8231b346e3b98a624606dcdbdb6a35bb
TangledTessellations <mcevoya@oregonstate.edu>
2018-08-16 15:30:47 -0700

    Add tests and endpoint for DELETE /beatles

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -39,18 +39,22 @@ describe('server.js', () => {
         it('returns a list from /beatles', async () => {
             const expected = [
                 {
+                    id: 1,
                     name: "Paul",
                     instrument: "Bass"
                 },
                 {
+                    id: 2,
                     name: "John",
                     instrument: "Guitar"
                 },
                 {
+                    id: 3,
                     name: "Ringo",
                     instrument: "Drums"
                 },
                 {
+                    id: 4,
                     name: "George",
                     instrument: "Guitar"
                 }
@@ -62,26 +66,78 @@ describe('server.js', () => {
     })
 
     describe('POST /beatles', () => {
-        it('should return status code 200', async () => {
-            const expected = 200
-            const response = await request(server).post('/beatles')
-            expect(response.status).toEqual(expected)
-        })
-
         it('should return JSON', async () => {
             const expected = 'application/json'
             const response = await request(server).post('/beatles')
             expect(response.type).toEqual(expected)
         })
 
+        it('should return status code 422 when no info sent', async () => {
+            const expected = 422
+            const response = await request(server).post('/beatles')
+            expect(response.status).toEqual(expected)
+        })
+
+        it('should return status code 422 when incomplete info sent', async () => {
+            const expected = 422
+            const fifthBeatle = {
+                id: 5,
+                instrument: "Spoons"
+            }
+            const response = await request(server).post('/beatles').send(fifthBeatle)
+            expect(response.status).toEqual(expected)
+        })
+
         it('adds new musician to the Beatles and returns the new group', async () => {
             const fifthBeatle = {
+                id: 5,
                 name: "Alex",
                 instrument: "Spoons"
             }
 
             const response = await request(server).post('/beatles').send(fifthBeatle)
             expect(response.body).toContainEqual(fifthBeatle)
+        })
+
+        it('should tell the bad news when it throws an error ', async () => {
+            const error = { error: true }
+            const response = await request(server).post('/beatles').send(error)
+            expect(response.body.error).toMatch('The beatles really don\'t need another member')
+        })
+    })
+
+    describe('DELETE /beatles', () => {
+        it('should return JSON', async () => {
+            const expected = 'application/json'
+            const response = await request(server).delete('/beatles')
+            expect(response.type).toEqual(expected)
+        })
+
+        it('should return 422 when no info passed', async () => {
+            const expected = 422
+            const response = await request(server).delete('/beatles')
+            expect(response.status).toEqual(expected)
+        })
+
+        it('should delete a beatle when name is given', async () => {
+            const killThisBeatle = { name: 'Ringo' }
+            const response = await request(server).delete('/beatles').send(killThisBeatle)
+
+            expect(response.body.map(beatle => beatle.name)).not.toContain(killThisBeatle.name)
+        })
+
+        it('should delete a beatle when id is given', async () => {
+            const killThisBeatle = { id: 1 }
+            const response = await request(server).delete('/beatles').send(killThisBeatle)
+
+            expect(response.body.map(beatle => beatle.id)).not.toContain(killThisBeatle.id)
+        })
+
+        it('should delete a beatle when instrument is given', async () => {
+            const killThisBeatle = { instrument: 'Guitar' }
+            const response = await request(server).delete('/beatles').send(killThisBeatle)
+
+            expect(response.body.map(beatle => beatle.instrument)).not.toContain(killThisBeatle.instrument)
         })
 
     })
--- server.js
+++ server.js
@@ -8,40 +8,72 @@ server.get('/', (req, res) => {
     res.status(200).json({api : "running"})
 })
 
-let dummyMusicos = [
+let theBeatles = [
     {
+        id: 1,
         name: "Paul",
         instrument: "Bass"
     },
     {
+        id: 2,
         name: "John",
         instrument: "Guitar"
     },
     {
+        id: 3,
         name: "Ringo",
         instrument: "Drums"
     },
     {
+        id: 4,
         name: "George",
         instrument: "Guitar"
     }
 ]
 
 server.get('/beatles', (req,res) => {
-    res.status(200).json(dummyMusicos)
+    res.status(200).json(theBeatles)
 })
 
 server.post('/beatles', (req, res) => {
-    const { name, instrument} = req.body
-
-    const musician = {
-        name: name,
-        instrument: instrument
+    try{
+        const { id, name, instrument, error } = req.body
+        if(error){
+            throw Error()
+        }
+        if(!id || !name || !instrument){
+            res.status(422).json({error: "Missing id, name, or instrument"})
+        }
+        const musician = {
+            id: id,
+            name: name,
+            instrument: instrument
+        }
+        theBeatles.push(musician)
+        res.status(200).json(theBeatles)
+    }catch(err){
+        res.status(500).json({error: "The beatles really don't need another member"})
     }
+   
+})
 
-    dummyMusicos.push(musician)
+server.delete('/beatles', (req,res) => {
+    try{
+        const { id, name, instrument} = req.body
+        if(!id && !name && !instrument){
+            res.status(422).json({error: "Missing id, name, or instrument"})
+        }
 
-    res.status(200).json(dummyMusicos)
+        theBeatles = theBeatles.filter(beatle => {
+            if(beatle.name !== name && beatle.id !== id && beatle.instrument !== instrument){
+                return beatle
+            }
+        })
+
+        res.status(200).json(theBeatles)
+    }catch(err){
+        res.status(500).json({id: id, name: name, instrument: instrument, error: "Hey, you can't get rid of a BEATLE!! (you're a monster)"})
+    }
 })
 
 module.exports = server
