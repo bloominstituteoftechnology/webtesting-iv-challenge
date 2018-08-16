b62622513478d10fee37b044be4e568f5be50a69
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-08-16 17:32:08 -0500

    POST /resources

--- server.js
+++ server.js
@@ -12,4 +12,9 @@ server.get("/sing", (req, res) => {
   res.status(200).send("I believe I can fly, I believe I can test an A-P-I!");
 });
 
+server.post("/resources", (req, res) => {
+  const transaction = req.body;
+  res.status(201).json(transaction);
+});
+
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -1,6 +1,9 @@
 const request = require("supertest");
+const faker = require("faker");
 
 const server = require("./server.js");
+
+let randomTransaction = faker.helpers.createTransaction();
 
 describe("server.js", () => {
   describe("GET /", () => {
@@ -30,5 +33,31 @@ describe("server.js", () => {
       const expected = "I believe I can fly, I believe I can test an A-P-I!";
       expect(actual).toBe(expected);
     });
+  });
+  describe("POST /resources", () => {
+    it("should return HTTP status code 201 Created", async () => {
+      const response = await request(server)
+        .post("/resources")
+        .send(randomTransaction);
+      const actual = response.status;
+      const expected = 201;
+      expect(actual).toEqual(expected);
+    });
+    it("should return in JSON format", async () => {
+      const response = await request(server)
+        .post("/resources")
+        .send(randomTransaction);
+      const actual = response.type;
+      const expected = "application/json";
+      expect(actual).toEqual(expected);
+    });
+    // it("should return expected data", async () => {
+    //   const response = await request(server)
+    //     .post("/resources")
+    //     .send(randomTransaction);
+    //   const actual = response.body;
+    //   const expected = randomTransaction;
+    //   expect(actual).toEqual(expected);
+    //});
   });
 });
