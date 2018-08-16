5167a7adc99b77ada23c354bc3fa840f963af1d5
Mariana Cerda <raymond_grc@yahoo.com>
2018-08-16 16:24:30 -0500

    Post, delete, get method/tests functional

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ success: true, data: { api: 'running' } });
+});
+
+server.post('/instrument', (req, res) => {
+  const { instrument } = req.body;
+
+  res.status(200).json({ "instrument": `${instrument}` });
+});
+
+server.delete('/instrument', (req, res) => {
+  res.status(200).json({"deleted": "Cello"})
+})
+
+
+
+
+module.exports = server;
---
+++ server/tests/test.js
@@ -0,0 +1,56 @@
+const request = require('supertest');
+
+const server = require('../index.js');
+
+describe('index.js', () => {
+  describe('root', () => {
+    it('should return status code 200', async () => {
+      const response = await request(server).get('/');
+
+      expect(response.status).toEqual(200);
+    });
+
+    it('should return type JSON', async () => {
+      const response = await request(server).get('/');
+
+      expect(response.type).toEqual('application/json');
+    });
+
+    it('should return accurate JSON', async () => {
+      const expected = {
+        success: true,
+        data: {
+          api: 'running',
+        },
+      };
+      const response = await request(server).get('/');
+
+      expect(response.body).toEqual(expected);
+    });
+  });
+});
+
+describe('POST /instrument', () => {
+  it('returns instrument: instrumentName when invoked', async () => {
+    // arrange
+    const expected = { instrument: 'Cello' };
+
+    // act
+    const response = await request(server)
+      .post('/instrument')
+      .send({ instrument: 'Cello' });
+
+    // assert
+    expect(response.body).toEqual(expected);
+  });
+});
+
+describe('delete /instrument', () => {
+  it ('deletes something', async () => {
+    const response = await request(server)
+      .delete('/instrument')
+      .send('Cello');
+
+      expect(response.body).toEqual({"deleted": "Cello"});
+  })
+})
