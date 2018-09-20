c0a57ac161d4c33e2ddd402b4c2563a85992ff1b
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-09-20 16:17:13 -0400

    Joseph Stossmeister: WIP

---
+++ server.spec.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const server = require('./server');
+
+describe('server.js', () => {
+  it('runs the tests', () => {
+    expect(true).not.toBeFalsy();
+  });//end run test
+});//end server.js
