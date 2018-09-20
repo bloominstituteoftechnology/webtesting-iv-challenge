440e010f5865e1a7fb50fafff253fed92030f8a8
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-20 16:18:44 -0500

    wrote test for GET / and corresponding server code

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const server = express();
 
 server.use(express.json());
 
-
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'running' });
+});
 
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -6,4 +6,12 @@ describe('server.js', () => {
     it('should run the tests', () => {
         expect(true).toBeTruthy();
     })
-})
+
+    describe('GET /', () => {
+        it('returns a 200 status code (OK)', async () => {
+            const response = await request(server).get('/');
+
+            expect(response.status).toBe(200);
+        });
+    });
+});
