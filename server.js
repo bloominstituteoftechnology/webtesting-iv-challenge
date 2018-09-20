0327800e413277b0b06ed43d31165efbec0d7b93
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-20 17:15:49 -0500

    added and successfully passed the POST tests

--- server.js
+++ server.js
@@ -8,5 +8,15 @@ server.get('/', (req, res) => {
     res.status(200).json({ api: 'running' });
 });
 
+server.post('/roster', (req, res) => {
+    const name = req.body.player;
+    const position = req.body.position;
+    res.status(201).json({ 
+        id: 1,
+        name: `${name}`,
+        position: `${position}`
+     });
+});
+
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -14,4 +14,25 @@ describe('server.js', () => {
             expect(response.status).toBe(200);
         });
     });
+
+    describe('POST /roster', () => {
+        it('should add a player to roster', async () => {
+            let player = 'Alvin Kamara';
+            let position = 'RB';
+            let response = await request(server)
+                .post(`/roster`)
+                .send({ player, position })
+                // .send({ name: 'Alvin Kamara', position: 'RB' });
+            expect(response.body).toEqual({ id: 1, name: 'Alvin Kamara', position: 'RB' });
+        });
+
+        it('should return JSON', async () => {
+            let player = 'Alvin Kamara';
+            let position = 'RB';
+            const response = await request(server)
+                .post('/roster')
+                .send({ player, position })
+            expect(response.type).toEqual('application/json');
+        });
+    });
 });
