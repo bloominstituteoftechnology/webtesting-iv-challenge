5326ee7f89b943a546ec04c225f9c3ccf6f51b8f
dasgma <dasgrigoma@gmail.com>
2018-09-20 15:23:44 -0700

    MVP done

--- server.js
+++ server.js
@@ -9,7 +9,23 @@ server.get('/', (req, res) => {
     res.status(200).json({ api: 'running' })
 });
 
+server.get('/students', (req, res) => {
+    res.status(200).json({students: 'List of students'});
+});
 
+server.post('/students', (req, res) => {
+    const { student } = req.body;
+    
+    res.status(201).json({student});
+    
+});
+
+
+server.delete("/students/:student", (req, res) => {
+    const { student } = req.params;
+        
+    res.status(200).json({Message: `Deleted ${student}`});
+  });
 
 module.exports = server;
 
--- server.spec.js
+++ server.spec.js
@@ -2,28 +2,89 @@ const server = require('./server');
 const request = require('supertest');
 
 describe('server.js', () => {
-    it('should be running', () => {
-        expect(true).toBeTruthy();
+    describe('root endpoint (/)', () => {
+  
+      it('should return status code 200 OK', async () => {
+        const expected = 200;
+        const response = await request(server).get('/');
+        expect(response.status).toEqual(expected);
+      });
+  
+      it('should return JSON', async () => {
+        const response = await request(server).get('/');
+        expect(response.type).toEqual('application/json');
+      });
+  
+      it('should return object  {api:running} ', async () => {
+        const expected = { api: 'running' };
+        const response = await request(server).get('/');
+        expect(response.body).toEqual(expected);
+      });
     });
-
-    describe('/', () => {
-        it('returns a 200 (Ok) status code', async() => {
-            const response = await request(server).get('/');
-
-            expect(response.status).toEqual(200);
-        });
-
-        it('returns a api: running', async() => {
-            const expectedBody = {api: 'running'};
-            const response = await request(server).get('/');
-
-            expect(response.body).toEqual(expectedBody);
-        });
-
-        it('returns a JSON', async() => {
-            const response = await request(server).get('/');
-            
-            expect(response.type).toEqual('application/json');
-        });
+  
+    describe('GET endpoint(/students)', () => {
+  
+      it('should return status code 200 OK', async () => {
+        const expected = 200;
+        const response = await request(server).get('/students');
+        expect(response.status).toEqual(expected);
+      });
+  
+      it('should return JSON', async () => {
+        const response = await request(server).get('/students');
+        expect(response.type).toEqual('application/json');
+      });
+  
+      it('should return object that looks like expected ', async () => {
+        const expected = { students: 'List of students' };
+        const response = await request(server).get('/students');
+        expect(response.body).toEqual(expected);
+      });
     });
-});
+  
+    describe('POST endpoint (/students)', () => {
+  
+      it('should return status code 201 created', async () => {
+        const expected = 201;
+        const response = await request(server)
+          .post('/students')
+          .send({ student: 'Das' });
+        expect(response.status).toEqual(expected);
+      });
+  
+      it('should return JSON', async () => {
+          const response = await request(server).get('/students');
+          expect(response.type).toEqual('application/json');
+        });
+  
+      it('should return created student', async () => {
+        const expected = { student: 'Das' };
+        const response = await request(server)
+          .post('/students')
+          .send({ student: 'Das' });
+        expect(response.body).toEqual(expected);
+      });
+    });
+  
+    describe('DELETE (/students/:student)', () => {
+      
+      it('should return status code 200 OK', async () => {
+        const expected = 200;
+        const response = await request(server).delete('/students/:student');
+        expect(response.status).toEqual(expected);
+      });
+  
+      it('should return JSON', async () => {
+          const response = await request(server).delete('/students/Das');
+          expect(response.type).toEqual('application/json');
+        });
+  
+      it('should return message that says `Success in deleting {student}`', async () => {
+        const expected = { Message: `Deleted Das` };
+  
+        const response = await request(server).delete('/students/Das');
+  
+        expect(response.body).toEqual(expected);
+      });
+    });
+  });
