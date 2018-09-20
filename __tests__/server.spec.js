a714fea79e91bd11fd83aeac183a510cea1604ba
LaikaFusion <newrad4@gmail.com>
2018-09-20 16:42:46 -0400

    create tests done

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -15,4 +15,30 @@ describe('server.js', () => {
       }));
     });
   });
+
+
+  describe('create route', () => {
+    it('should 400', async () => {
+      const expectedStatusCode = 400;
+      const response = await request(server).post('/create');
+      expect(response.status).toEqual(expectedStatusCode);
+    });
+    it('should respond with one object in an array', async () => {
+      const response = await request(server).post('/create').send({ name: 'Manny', species: 'cat' });
+      expect(response.body).toHaveLength(1);
+    });
+    it('should 200', async () => {
+      const expectedStatusCode = 200;
+      const response = await request(server).post('/create').send({ name: 'Manny', species: 'cat' });
+      expect(response.status).toEqual(expectedStatusCode);
+    });
+    
+    it('should respond with updated pet list', async () => {
+      const response = await request(server).post('/create').send({ name: 'Manny', species: 'cat' });
+      expect(response.body).toEqual(expect.arrayContaining([{
+        name: expect.stringMatching('Manny'),
+        species: expect.stringMatching('cat')
+      }]));
+    });
+  });
 });
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+let storage = [];
 
+app.use(express.json());
 
-
-
+app.post("/create", (req, res) => {
+  if(!req.body.name || !req.body.species){
+    res
+    .status(400)
+    .json({ errorMessage: "Invalid body" })
+    return;
+  }
+  storage.push({... req.body})
+  res
+    .status(200)
+    .json(storage)
+    return;
+});
 app.use("/", (req, res) =>
   res
     .status(404)
