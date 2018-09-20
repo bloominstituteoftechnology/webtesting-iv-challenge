43ddac7add651ee0428a0b9d69f2bd8ed1b77a84
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-20 13:57:25 -0600

    Added pseudocode

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express'); 
+
+const server = express.json(); 
+
+module.exports = server;
---
+++ server.spec.js
@@ -0,0 +1,23 @@
+const server = require('./server'); 
+
+// GET Request Tests 
+// should return a status code of 200
+// check for the response body? 
+// the type of the response should be JSON
+
+//POST Request Tests 
+//should return a status code of 201
+//should return posted data in the response body
+// it should  return a JSON  response 
+
+//DELETE Request Tests 
+//should return a status code of 200 when the user is deleted
+//should return the id of the deleted item 
+//should make sure it is a JSON response 
+
+//PUT Request Tests
+//should return a status code of 201 when a user is updated
+//should return the id of the updated item
+//should make sure it is a JSON response 
+
+
