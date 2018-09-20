5fb1531107177d8aa56e5fb07268e08b4069b044
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-09-20 15:52:54 -0500

    POST and DELETE
    My POST and DELETE are not testing correctly, says that there is something wrong with the .toBe, its equalling 404 but should be doing a 201.

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-const express = require('express'); 
-const server = express(); 
-server.use(express.json())
+const express = require("express");
+const server = express();
+server.use(express.json());
 
-server.get('/', (req, res)=>{
-    
-    res.status(200).json({api: "running"});  
+let movies = [
+  { id: 0, film: "Romy and Michele Highschool Reunion" },
+  { id: 1, film: "Halloween" },
+  { id: 2, film: "Eighth Grade" }
+];
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "running" });
 });
-server.get('/hello', (req, res)=>{
-    
-    res.status(200).json({hello: "FSW12"});  
+server.get("/hello", (req, res) => {
+  res.status(200).json({ hello: "FSW12" });
 });
-server.post('/movies/:title', (req, res)=>{
-    res.status(200).json({movie:"hereditary horror"}); 
-})
-module.exports = server; 
+server.post("/movies/:id", (req, res) => {
+  const { film } = req.body;
+  const id = movies.length;
+  movies.push({id, film});
+  res.status(201).json(movies);
+});
+server.delete("/movies/:title", (req, res) => {
+    const {id} = req.params; 
+    if(movies.find(movie => Number(movie.id) === Number(id))){
+        movies = movies.filter(movie => {
+            return Number(movie.id) !== Number(id); 
+        });
+        res.status(200).json(movies);
+    }else{
+        res.status(404).json({message: "id does not exist"})
+    }
+});
+module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -1,31 +1,51 @@
-const server = require('./server.js');
-const request = require('supertest'); 
+const server = require("./server.js");
+const request = require("supertest");
 
-describe ('server.js', () =>{
-    it('runs the test', () => {
-        expect(true).toBeTruthy(); 
-    }); 
-    describe("GET /", () => {
-        it('returns a 200 (OK) status code', async ()=> {
-            const response = await request(server).get('/'); 
-            expect(response.status).toEqual(200); 
-        }); 
-        it('should return {api: "running"} ', async () => {
-            const expectedBody = {api: 'running'}; 
-            const response = await request(server).get('/'); 
-            expect(response.body).toEqual(expectedBody);   
-        }); 
-        it('should return JSON ', async () => {
-            const response = await request(server).get('/hello'); 
-            expect(response.type).toEqual('application/json');   
-        })
-    })
-    describe("/movies/:title", () => {
-        it("should return a greeting", async () => {
-            const response = await request(server)
-            .post('/movies/hereditary')
-            .send({genre: "horror"}); 
-            expect(response.body).toEqual({movie:"hereditary horror"}); 
-        })
-    })
+describe("server.js", () => {
+  it("runs the test", () => {
+    expect(true).toBeTruthy();
+  });
+  describe("GET /", () => {
+    it("returns a 200 (OK) status code", async () => {
+      const response = await request(server).get("/");
+      expect(response.status).toEqual(200);
+    });
+    it('should return {api: "running"} ', async () => {
+      const expectedBody = { api: "running" };
+      const response = await request(server).get("/");
+      expect(response.body).toEqual(expectedBody);
+    });
+    it("should return JSON ", async () => {
+      const response = await request(server).get("/hello");
+      expect(response.type).toEqual("application/json");
+    });
+  });
+  describe("/movies", () => {
+    it("should return a movie", async () => {
+      const response = await request(server)
+        .post("/movies")
+        .send({ film: "Eighth Grade" });
+      const body = [
+        { id: 0, film: "Romy and Michele Highschool Reunion" },
+        { id: 1, film: "Halloween" },
+        { id: 2, film: "Eighth Grade" }
+      ];
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual(body);
+    });
+  });
+  describe("/movies/:id", () => {
+    it("should delete the passed in id", async () => {
+      const response = await request(server).delete("movies/2");
+      const body = [
+        { id: 0, film: "Romy and Michele Highschool Reunion" },
+        { id: 1, film: "Halloween" }
+      ];
+      expect(response.body).toEqual({ movie: body });
+    });
+    it("should return 404 if the id does not exist", async () => {
+      const response = await request(server).delete("/movies/20101");
+      expect(response.status).toBe(404);
+    });
+  });
 });
