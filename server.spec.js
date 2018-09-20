0c9c3cf15623397ab0c65f1688065ed321b22193
adamcglee <adamcglee@gmail.com>
2018-09-20 13:59:06 -0700

    Adam Lee: Server-Testing
    initial project commit and PR with back end db set up with create and delete endpoints.

---
+++ server.spec.js
@@ -0,0 +1,8 @@
+const request = require("supertest");
+const server = require("./server.js");
+
+describe('server.js', () => {
+    it('run the tests', () => {
+        expect(true).toBeTruthy();
+    });
+})
