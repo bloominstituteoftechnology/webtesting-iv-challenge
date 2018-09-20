7e5a6da02664d26b71434a1646bc4ccc281c60f7
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-09-20 16:46:02 -0400

    GET works as test wanted -forgot to add async to tests

--- server.js
+++ server.js
@@ -3,4 +3,21 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+const posts = [
+  {
+    id: 0,
+    text: 'Example Text'
+  },
+];
+
+let id = 1;
+
+const getId = () => {
+  return id++;
+}
+
+server.get('/', (req, res) => {
+  res.status(200).json(posts);
+});
+
 module.exports = server;
