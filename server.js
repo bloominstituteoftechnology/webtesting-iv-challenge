d6e73898a8508ad9ced72bf4961b528424f410df
Grant Reighard <grant.reighard@gmail.com>
2018-09-20 16:27:47 -0400

    Got four tests working for POST method.

--- server.js
+++ server.js
@@ -16,4 +16,14 @@ server.get('/', (req, res) => {
     res.status(200).json(returnBody);
 })
 
+server.post('/', (req, res) => {
+    const {id, name, email} = req.body;
+
+    if (!id || !name || !email) {
+        res.status(500).end();
+    } else {
+        res.status(201).json(req.body)
+    }
+})
+
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -28,4 +28,37 @@ describe('Server', () => {
             expect(response.type).toBe('application/json');
         });
     });
+
+    describe('POST to /', () => {
+        it('should return a status code of 201', async () => {
+            const response = await request(server).post('/').send({id: 4, name: 'Luis', email: 'luis@test.com'});
+            
+            expect(response.status).toEqual(201); 
+        })
+
+        it('should return a status code of 500', async () => {
+            const response = await request(server).post('/').send({id: 4, name: 'Luis',})//no name 
+
+            expect(response.status).toEqual(500);
+        } )
+
+        it('should return posted data in the response body', async () => {
+            const expectedBody = {id: 4, name: 'Luis', email: 'luis@test.com'}
+
+            const response = await request(server).post('/').send(expectedBody)
+
+            expect(response.body).toEqual(expectedBody);
+        })
+        
+        it('should return a JSON object', async () => {
+            const expectedBody = {id: 4, name: 'Luis', email: 'luis@test.com'}
+
+            const response = await request(server).post('/').send(expectedBody);
+
+            expect(response.type).toBe('application/json');
+
+        });
+
+    });
+
 });
