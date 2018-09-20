c6c90e6451f24a6b6451c51793fab4e3b7c985cb
David Pok <david.pok@gmail.com>
2018-09-20 15:40:08 -0700

    finishes mvp

--- server.spec.js
+++ server.spec.js
@@ -11,19 +11,56 @@ describe("server.js", () => {
       //get access to the server
       //user supertest to make a GET to the server
       const response = await request(server).get("/");
+
       expect(response.status).toEqual(200);
     });
 
     it("should return api: running", async () => {
       const expectedBody = { api: "running" };
       const response = await request(server).get("/");
+
       expect(response.body).toEqual(expectedBody);
     });
 
     it("should return JSON", async () => {
       const expectedBody = { api: "running" };
       const response = await request(server).get("/hello");
+
       expect(response.type).toEqual("application/json");
     });
+
+    describe("/greet/:name", () => {
+      it("should greet me", async () => {
+        let name = "david";
+        let last = "pok";
+        //make a post passing last name in the body
+        //and firt name as url parameter
+        const response = await request(server)
+          .post(`/greet/${name}`)
+          .send({ last });
+        //verify that the endpoint returns the right object
+        expect(response.body).toEqual({ hello: "david pok" });
+      });
+    });
   });
+
+  describe("/skills", () => {
+    it("should post my nonexistant skills", async () => {
+      const response = await request(server)
+        .post("/skills")
+        .send({ name: "david", skills: "none" });
+
+      expect(response.body).toEqual({ name: "david", skills: "none" });
+    });
+
+    it("should delete skills", async () => {
+      const response = await request(server)
+        .delete("/skills")
+        .send({ skills: "none" });
+
+      expect(response.body).toEqual({ skills: "none" });
+    });
+
+  });
+  
 });
