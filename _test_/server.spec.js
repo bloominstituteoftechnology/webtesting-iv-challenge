72d0f940ad93315de466e03cf4be11ec6ec33eaa
francistse23 <wtse.ucsd@gmail.com>
2018-09-20 14:00:33 -0700

    Not using public API Using customized data instead. Tests written for POST and DELETE.

--- _test_/server.spec.js
+++ _test_/server.spec.js
@@ -5,22 +5,58 @@ describe('server.js', () => {
     it('runs the tests', () => {
         expect(true).toBeTruthy();
     })
-    describe('GET /posts', () => {
+    describe('GET /food', () => {
         it('should return a 200 status code', async () => {
-            const response = await request(server).get('/posts')
+            const response = await request(server).get('/food')
             expect(response.status).toEqual(200);
         })
+        it('should respond with json', async () => {
+            const response = await request(server).get('/food');
+            expect(response.type).toBe('application/json');
+        });
+        it('should return a list of food', async () => {
+            const expectedBody = [
+                { id: 0, name: 'Pasta'},
+                { id: 1, name: 'Pizza'},
+                { id: 2, name: 'Soup'}
+            ]
+            const response = await request(server).get('/food');
+            expect(response.body).toEqual(expectedBody);
+        });
     })
-    describe(`GET /posts/${id}`, () => {
-        it('should return a 200 status code', async () => {
-            const response = await request(server).get('/posts')
-            expect(response.status).toEqual(200);
-        })
-        it('should return a list of posts', async () => {
-            let id = 1;
-            let response = await request(server).get(`/posts/${id}`)
-            expect(response.data.title).toEqual("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
-        })
-    })
-
-})
+    describe(`POST /food`, () => {
+        it('should respond with json', async () => {
+            const response = await request(server).post('/food');
+            expect(response.type).toBe('application/json');
+        });
+        it('should add new food to the list of food', async () => {
+            const response = await request(server)
+            .post('/food')
+            .send({ name: 'Fries'});
+            const expectedBody = [
+                { id: 0, name: 'Pasta'},
+                { id: 1, name: 'Pizza'},
+                { id: 2, name: 'Soup'},
+                { id: 3, name: 'Fries'},
+            ];
+            expect(response.body).toEqual(expectedBody);
+        });
+    });
+    describe(`DELETE /food`, () => {
+        it('should delete the specified food by ID from the list', async () => {
+            const response = await request(server)
+            .delete('/food/0');
+            const expectedBody = [
+                { id: 1, name: 'Pizza'},
+                { id: 2, name: 'Soup'},
+                { id: 3, name: 'Fries'},
+            ];
+            expect(response.body).toEqual(expectedBody);
+        });
+        it('should return status code 404 if the food ID is not found', async () => {
+            const response = await request(server)
+            .delete('/food/100');
+            expect(response.status).toBe(404);
+        });
+    });
+});
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json());
+
+server.get('/posts', (req, res) => {
+    
+});
 
 server.listen(8000, () => console.log('Server running on port 8000'))
 
