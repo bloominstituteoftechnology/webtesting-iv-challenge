6be0d33dec075d905ef4bc56fc446c84e209cb27
Carey <careycbaldwin@gmail.com>
2018-09-20 15:44:58 -0400

    testing for request body
    fails because there is no body

--- server.spec.js
+++ server.spec.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 
 const server = require('./server.js');
 
+
+//Tests for / endpoint
 describe ('get request for /', () => {
     it('returns a 200 status code', async () => {
         //get access to the server
@@ -9,5 +11,12 @@ describe ('get request for /', () => {
         const response = await request(server).get('/');
 
         expect(response.status).toEqual(200);
-    })
-})
+    });
+    it('returns {api: "running"}', async () => {
+        const expectedBody = { api: 'running'}
+
+        const response = await request(server).get('/');
+
+        expect(response.body).toEqual(expectedBody);
+    });
+});
