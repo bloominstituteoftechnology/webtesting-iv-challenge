aa4a08743ac00a5220e7475bd086e410d6423d78
Ashwin <aswinjeganath@gmail.com>
2018-09-21 03:24:13 +0530

    Write tests for create route

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-const id = 1;
+let id = 1;
 const todos = [];
 
 app.get('/todos', (req, res) => {
   res.status(200).json(todos);
+});
+
+app.post('/todos', (req, res) => {
+  const newTodo = {
+    id: id++,
+    text: req.body.text,
+    complete: false,
+  };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
 });
 
 module.exports = app;
--- server.spec.js
+++ server.spec.js
@@ -19,3 +19,41 @@ describe('GET /todos', () => {
       .catch(done);
   });
 });
+
+describe('POST /todos', () => {
+  it('returns a 201', done => {
+    return request(app)
+      .post('/todos')
+      .send({ text: 'Learn testing' })
+      .expect(201, done);
+  });
+
+  it('creates a new todo object and returns it with id', done => {
+    return request(app)
+      .post('/todos')
+      .send({ text: 'Learn testing' })
+      .then(response => {
+        expect(response.body.hasOwnProperty('id')).toBe(true);
+        expect(response.body.text).toBe('Learn testing');
+        expect(response.body.complete).toBe(false);
+        done(null);
+      })
+      .catch(done);
+  });
+
+  it('adds the new object to the list of todos', done => {
+    let data = null;
+    return request(app)
+      .post('/todos')
+      .send({ text: 'Learn testing' })
+      .then(response => {
+        data = response.body;
+        return request(app).get('/todos');
+      })
+      .then(response => {
+        expect(response.body).toContainEqual(data);
+        return done(null);
+      })
+      .catch(done);
+  });
+});
