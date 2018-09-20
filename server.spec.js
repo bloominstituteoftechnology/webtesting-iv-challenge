7ffa4e5b8d2e7b88d5607af0ef3c8e0962e5fa79
Carey <careycbaldwin@gmail.com>
2018-09-20 15:38:23 -0400

    wrote first test and then corresponding get request

--- server.spec.js
+++ server.spec.js
@@ -2,3 +2,12 @@ const request = require('supertest');
 
 const server = require('./server.js');
 
+describe ('get request for /', () => {
+    it('returns a 200 status code', async () => {
+        //get access to the server
+        //use super test to run a get request to server
+        const response = await request(server).get('/');
+
+        expect(response.status).toEqual(200);
+    })
+})
