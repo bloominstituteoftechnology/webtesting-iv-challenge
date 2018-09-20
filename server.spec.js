8901ec68e73d5344a884cc158e2ddd57813adfa3
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-09-20 17:35:04 -0500

    Still on error
    Cannot figure out the source of what is causing the error, I've tried different things but nothing has worked.

--- server.spec.js
+++ server.spec.js
@@ -5,36 +5,41 @@ describe("server.js", () => {
   it("runs the test", () => {
     expect(true).toBeTruthy();
   });
-  describe("GET /", () => {
+  describe("GET /movies", () => {
     it("returns a 200 (OK) status code", async () => {
-      const response = await request(server).get("/");
+      const response = await request(server).get("/movies");
       expect(response.status).toEqual(200);
     });
-    it('should return {api: "running"} ', async () => {
-      const expectedBody = { api: "running" };
-      const response = await request(server).get("/");
+    it('should return films ', async () => {
+      const response = await request(server).get("/movies");
+      const expectedBody =[
+        { id: 0, film: "Romy and Michele Highschool Reunion" },
+        { id: 1, film: "Halloween" }
+      ];
       expect(response.body).toEqual(expectedBody);
     });
     it("should return JSON ", async () => {
-      const response = await request(server).get("/hello");
+      const response = await request(server).get("/movies");
       expect(response.type).toEqual("application/json");
     });
   });
-  describe("/movies", () => {
+  describe("POST /movies", () => {
     it("should return a movie", async () => {
       const response = await request(server)
         .post("/movies")
-        .send({ film: "Eighth Grade" });
-      const body = [
+        .send({
+            film: "Eighth Grade" 
+        });
+      const expectedBody = [
         { id: 0, film: "Romy and Michele Highschool Reunion" },
         { id: 1, film: "Halloween" },
         { id: 2, film: "Eighth Grade" }
       ];
       expect(response.status).toBe(201);
-      expect(response.body).toEqual(body);
+      expect(response.body).toEqual(expectedBody);
     });
   });
-  describe("/movies/:id", () => {
+  describe("DELETE /movies/:id", () => {
     it("should delete the passed in id", async () => {
       const response = await request(server).delete("movies/2");
       const body = [
