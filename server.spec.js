3bae439173aec52c09bcfcb08c7eebeb6b29dad1
MichaelBoegner <cmikeb@gmail.com>
2018-09-20 18:21:49 -0400

    Endpoints and Tests Built

--- server.spec.js
+++ server.spec.js
@@ -1,17 +1,22 @@
 const request = require('supertest');
 const server = require('./server.js'); 
 
-describe('server.js', () => {
+describe('BASIC SERVER IS RUNNING TEST', () => {
     it('runs the tests', () => {
         expect(true).toBeTruthy();
     })
 
-    it('should return a status code of 200', async () => {
+})
+
+describe('GET', () => {
+    it('should return a status code of 200 after getting users', async () => {
         const response = await request(server).get('/users');
             
         expect(response.status).toEqual(200); 
     })
+})
 
+describe('POST', () => {
     it.skip('should post a user to the table "users"', async () => {
         const response = await request(server)
             .post('/users')
@@ -23,7 +28,29 @@ describe('server.js', () => {
         expect(response.body).toEqual({added: 'Queen Qbert has been added!'}); 
     })
 
-    it('should delete a user from the table "users"', async () => {
+    it.skip('should return a status code of 200 after posting a user', async () => {
+        const response = await request(server)
+            .post('/users')
+            .send({
+                username: 'Kangaroo Qbert',
+                department: 'fruit loops',
+            }) 
+        expect(response.status).toEqual(200); 
+    })
+
+    it('should return a status code of 500 if posting a user fails', async () => {
+        const response = await request(server)
+            .post('/users')
+            .send({
+                username: 'Kangaroo Qbert',
+                department: 'fruit loops',
+            }) 
+        expect(response.status).toEqual(500); 
+    })
+})
+
+describe('DELETE', () => { 
+    it.skip('should delete a user from the table "users"', async () => {
         const id = 6;
 
         const response = await request(server)
@@ -31,4 +58,14 @@ describe('server.js', () => {
             
         expect(response.body).toEqual({deleted: `User with ID of ${id} has been deleted!`}); 
     })
+
+    it.skip('should return a status code of 200 after deleting a user', async () => {
+        const id = 4;
+
+        const response = await request(server)
+            .delete(`/users/${id}`)
+
+        expect(response.status).toEqual(200); 
+    })
+
 })
