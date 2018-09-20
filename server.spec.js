3d01a32ffe72a33151149aeaf0ee2fc1d0b1a903
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-20 14:02:45 -0600

    Added GET request

--- server.spec.js
+++ server.spec.js
@@ -1,9 +1,41 @@
 const server = require('./server'); 
+const request = require('supertest'); 
 
 // GET Request Tests 
 // should return a status code of 200
 // check for the response body? 
 // the type of the response should be JSON
+
+
+describe('Server', () => {
+    describe('GET to /', () => {
+        it('should return a status code of 200', async () => {
+            const response = await request(server).get('/');
+
+            expect(response.status).toEqual(200);
+        });
+
+        it('should return a response body', async () => {
+            const expectedBody = {
+                users: [
+                    {id: 1, name: 'Mike', email: 'mike@test.com'},
+                    {id: 2, name: 'Katia', email: 'katia@test.com'},
+                    {id: 3, name: 'Grant', email: 'grant@test.com'}
+                ]
+            };
+
+            const response = await request(server).get('/');
+            expect(response.body).toEqual(expectedBody);
+        });
+
+        it('should return a JSON object', async () => {
+            const response = await request(server).get('/');
+
+            expect(response.type).toBe('application/json');
+        });
+    });
+    
+});
 
 //POST Request Tests 
 //should return a status code of 201
