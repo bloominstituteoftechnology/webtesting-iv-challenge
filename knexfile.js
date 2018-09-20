e77f012127d0cab6ecbe09e079d184a43cb2d030
johnoro <officialjohnorourke@gmail.com>
2018-09-20 16:10:02 -0400

    Created DB with knex and sqlite3.

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './db/forms.sqlite3'
+    },
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    }
+  }
+};
