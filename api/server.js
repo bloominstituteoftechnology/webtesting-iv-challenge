417f2cd36feeb24b001be3a9684c0e07b1fccf4d
Lucas Beemer <lucasbeemer@gmail.com>
2018-10-25 15:42:09 -0700

    delete route and tests are passing but user is not being deleted

--- api/server.js
+++ api/server.js
@@ -3,47 +3,55 @@ const server = express();
 
 server.use(express.json());
 
-let users = [
-    {   
-        username: 'Lucas',
-        id: 0
-    },
-]
+let users = [];
 
-// ===== SERVER RUNNING
+// ===== GET SERVER UP
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'server up' });
   });
 
+
+
 // ===== GET LIST OF USERS
-server.get('/users', (req, res) => {
+server.get('/api/users', (req, res) => {
     res.json(users);
  });
  let userId = 1;
 
+
+
 // ===== POST A USER (JSON OBJECT)
- server.post('/users', (req, res) => {
+ server.post('/api/users', (req, res) => {
     const {username} = req.body
     const newUser = {username, id: userId}
     if (!username) {
-        return res.send('username is required');
+        return res.send('username is required', res);
     }
     users.push(newUser);
     userId++;
     res.json(users)
   });
 
+
+
 // ===== POST A USER (url)('/users/:username')
-server.post("/users/:username", (req, res) => {
+server.post('/api/users/:username', (req, res) => {
   const { username } = req.params;
   const newUser = {username, id: userId}
   res.status(200).json({ username: `${username}` });
   if (!username) {
-    return res.send('username is required');
+    return res.send('username is required', res);
 }
 users.push(newUser);
 userId++;
 res.json(users)
 });
+
+
+// ===== DELETE A USER
+server.delete("/api/users/:username", (req, res) => {
+    const { username } = req.params;
+    res.status(200).json({ deleted: `${username}` });
+  });
 
 module.exports = server;
