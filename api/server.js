95a53034396b466c51d1512ed6cc839d6b51d4ba
Lucas Beemer <lucasbeemer@gmail.com>
2018-10-25 13:51:19 -0700

    added another test for POST and also added GET request. GET users test passed but have not stored users yet

--- api/server.js
+++ api/server.js
@@ -3,9 +3,18 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'server up' });
+  });
+
 server.post("/users/:username", (req, res) => {
   const { username } = req.params;
   res.status(200).json({ Username: `${username}` });
 });
+
+server.get('/users', (req, res) => {
+    const users = req.params;
+    res.status(200).json({users});
+})
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -1,6 +1,27 @@
 const request = require("supertest");
 const server = require("./api/server.js");
 
+describe('server', () => {
+
+// ===== GET request
+    describe('GET /', () => {
+        it('should return status code 200(OK)', async () => {
+          const response = await request(server).get('/');
+          expect(response.status).toBe(200);
+        });
+    
+        it('should return JSON', async () => {
+          const response = await request(server).get('/');
+          expect(response.type).toBe('application/json');
+        });
+    
+        it('should return { message: "server up" }', async () => {
+          const response = await request(server).get('/');
+          expect(response.body).toEqual({ message: 'server up' });
+        });
+      });
+
+// ===== POST request
 describe("POST /users/:username", () => {
   it("should add the person", async () => {
     const username = "Lucas";
@@ -14,4 +35,21 @@ describe("POST /users/:username", () => {
     const response = await request(server).post(`/users/${username}`);
     expect(response.status).toBe(200);
   });
+
+  it("should return JSON", async () => {
+    const username = "Lucas";
+    const response = await request(server).post(`/users/${username}`);
+    expect(response.type).toBe("application/json");
+  });
+});
+
+// ===== GET (USERS)
+describe('GET /users', () => {
+    it('should return users', async () => {
+      const response = await request(server).get('/users');
+      expect(response.status).toBe(200);
+    });
+  });
+
+
 });
