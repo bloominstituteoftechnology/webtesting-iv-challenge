d444fcfc55ba9e06d2cad2990efb55b53b6af5fa
Carlos Vargas <cornielleandres@gmail.com>
2018-10-25 15:01:28 -0400

    Write tests for GET /api/users

--- api/server.js
+++ api/server.js
@@ -3,13 +3,25 @@ const helmet = require('helmet');
 
 const server = express();
 
+// middleware
 server.use(
 	express.json(),
 	helmet(),
 );
 
+// db
+const userDb = require('../data/models/userDb.js');
 
 server.get('/', (req, res) => {
+	return res.status(200).json('Server is running.');
+});
+
+// get all users in db
+server.get('/api/users/', (req, res) => {
+	return userDb
+		.get()
+		.then()
+		.catch()
 	return res.status(200).json('Server is running.');
 });
 
--- api/server.spec.js
+++ api/server.spec.js
@@ -18,4 +18,21 @@ describe('server.js', () => {
 			expect(response.body).toBe('Server is running.');
 		});
 	});
+
+	describe('GET /api/users', () => {
+		it('should return status 200(OK)', async () => {
+			const response = await request(server).get('/api/users');
+			expect(response.status).toBe(200);
+		});
+
+		it('should return JSON', async () => {
+			const response = await request(server).get('/api/users');
+			expect(response.type).toBe('application/json');
+		});
+
+		it('should return list of all users in the db', async () => {
+			// const response = await request(server).get('/');
+			// expect(response.body).toBe('Server is running.');
+		});
+	});
 });
