b5d9ad5dab6daa3cb30dfd3774af152c7d99cf48
mparedes003 <michelle.g.paredes@gmail.com>
2018-10-25 14:19:04 -0700

    Added 2 tests to POST.

--- index.spec.js
+++ index.spec.js
@@ -5,6 +5,20 @@ const server = require('./api/server.js');
 describe('server', () => {
   // test for POST ENDPOINT
   describe('POST /pets/:petsname', () => {
+    // check for correct http status code
+    it('should return status code 201', async () => {
+      const response = await request(server).post('/pets/:petsname');
+
+      expect(response.status).toBe(201);
+    });
+
+    // check for format of response
+    it('should return JSON', async () => {
+      const response = await request(server).post('/pets/:petsname');
+
+      expect(response.type).toBe('application/json');
+    });
+    
     // check for adding content inside response.body
     it('should add a name of a pet', async () => {
       const petsname = 'Spot';
