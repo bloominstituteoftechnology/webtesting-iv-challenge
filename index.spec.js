2ff92b083783642c8a454dc3680e346a4ec5ec21
Nicocchi <nicocchilover@gmail.com>
2018-10-25 15:38:01 -0400

    GET / test complete

--- index.spec.js
+++ index.spec.js
@@ -5,8 +5,18 @@ const server = require('./api/server.js');
 describe('Server', () => {
     describe('GET /', () => {
         it('returns status code 200(OK)', async () => {
-            const response = await request(server).get('/');
-            expect(response.status).toBe(200);
+            const res = await request(server).get('/');
+            expect(res.status).toBe(200);
+        });
+
+        it('returns JSON', async () => {
+            const res = await request(server).get('/');
+            expect(res.type).toBe('application/json');
+        });
+
+        it('returns {message: "Server is up"', async () => {
+             const res = await request(server).get('/');
+             expect(res.body).toEqual({message: 'Server is up'});
         });
     });
 });
