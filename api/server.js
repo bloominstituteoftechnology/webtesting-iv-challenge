cddc4c2bacb11b641c24bbfed4567c800402cd58
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-10-25 15:35:54 -0700

    server is up with basic tests. Working on creating the api endpoints.

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'server is running' });
+});
+
+server.post('/hello/:name', (req, res) => {
+  const { name } = req.params;
+  const lastName = req.body.lastName || 'Doe';
+
+  res.status(200).json({ hello: `${name} ${lastName}` });
+});
+
+module.exports = server;
