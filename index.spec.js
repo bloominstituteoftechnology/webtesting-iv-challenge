6d1b97cea524b1d726fefcfc148f8ca990604c65
Nicocchi <nicocchilover@gmail.com>
2018-10-25 15:33:33 -0400

    test file created with 1 test

---
+++ index.spec.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+
+const server = require('./api/server.js');
+
+describe('Server', () => {
+    describe('GET /', () => {
+        it('returns status code 200(OK)', async () => {
+            const response = await request(server).get('/');
+            expect(response.status).toBe(200);
+        });
+    });
+});
