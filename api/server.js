7eaec6646246e994a25dc0c4cdbe137d3f8f1af1
Eva <evalee904@gmail.com>
2018-10-25 17:17:22 -0500

    mvp.

--- api/server.js
+++ api/server.js
@@ -11,9 +11,12 @@ server.post("/api/:user", (req, res) => {
   const { user } = req.params;
   const house = req.body.house;
 
-  res
-    .status(200)
-    .json({ Message: `Hello ${user}, you belong to ${house}` });
+  res.status(201).json({ Message: `Hello ${user}, you belong to ${house}.` });
+});
+
+server.delete("/api/:user", (req, res) => {
+  const { user } = req.params;
+  res.status(200).json({ Message: `${user} was removed.` });
 });
 
 module.exports = server;
--- index.spec.js
+++ index.spec.js
@@ -1,15 +1,56 @@
-const request = require('supertest');
-const server = require('./api/server');
+const request = require("supertest");
+const server = require("./api/server");
 
-describe('server', () => {
-  describe('GET /', () => {
-    it('should return status code 200', async () => {
-      const response = await request(server).get('/');
+describe("server", () => {
+  describe("GET /", () => {
+    it("should return status code 200", async () => {
+      const response = await request(server).get("/");
       expect(response.status).toBe(200);
     });
-  });
 
+    it("should return JSON", async () => {
+      const response = await request(server).get("/");
+      expect(response.type).toBe("application/json");
+    });
+  }); // get
 
+  describe("POST /api/:user", () => {
+    it("should return status code 201", async () => {
+      const user = "Harry";
+      const response = await request(server)
+        .post(`/api/${user}`);
+      expect(response.status).toBe(201);
+    });
 
+    it("should return a message", async () => {
+      const user = "Harry";
+      const house = "Gryffindor";
+      const expected = {
+        Message: "Hello Harry, you belong to Gryffindor."
+      };
+      const response = await request(server)
+        .post(`/api/${user}`)
+        .send({ house });
+      expect(response.body).toEqual(expected);
+    });
+  }); // post
 
-});
+  describe('DELETE /api/:user', () => {
+    it("should return status code 200", async () => {
+      const user = "Harry";
+      const response = await request(server)
+        .delete(`/api/${user}`);
+      expect(response.status).toBe(200);
+    });
+
+    it("should return a message", async () => {
+      const user = "Harry";
+      const expected = {
+        Message: "Harry was removed."
+      };
+      const response = await request(server)
+        .delete(`/api/${user}`)
+      expect(response.body).toEqual(expected);
+    });
+  }); // delete
+}); // server
