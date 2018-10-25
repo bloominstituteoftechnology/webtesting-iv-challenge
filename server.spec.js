45103c930b9006a707d77b67a6b11cb11f3e451e
Terrance Danckert <tbarnett2020@gmail.com>
2018-10-25 16:49:35 -0400

    Terrance Danckert FSW13 (Server-Testing)

---
+++ server.spec.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+
+const server = require('./server.js');
+
+describe('server.js', () => {
+    it('we runnning the tests!!', () => {
+        expect(true).toBeTruthy();
+    })
+})
