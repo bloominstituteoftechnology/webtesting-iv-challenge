dea1cc99c56617530f7868c6c27858b0e9795c1c
Nicocchi <nicocchilover@gmail.com>
2018-10-25 17:41:55 -0400

    DELETE tests complete

--- index.spec.js
+++ index.spec.js
@@ -50,6 +50,7 @@ describe('Server', () => {
                 "password": "pass"
             },
         ]
+
         const res = await request(server).post('/users').send({user});
         expect(res.body.users).toEqual(expected);
         });
@@ -67,12 +68,98 @@ describe('Server', () => {
 
         it('returns JSON', async () => {
             const user = {
-                "id": 5,
+                "id": 6,
                  "username": "Max",
                  "password": "pass"
             }
 
             const res = await request(server).post('/users').send({user});
+            expect(res.type).toBe('application/json');
+        });
+    });
+
+    describe('DELETE /users', () => {
+        it('returns list of users after deletion', async () => {
+            const expected = [
+                {
+                    "id": 1,
+                    "username": "John",
+                    "password": "pass"
+                },
+                {
+                    "id": 2,
+                    "username": "Jack",
+                    "password": "pass"
+                },
+                {
+                    "id": 3,
+                    "username": "Joe",
+                    "password": "pass"
+                },
+                {
+                    "id": 5,
+                     "username": "Max",
+                     "password": "pass"
+                },
+                {
+                    "id": 6,
+                     "username": "Max",
+                     "password": "pass"
+                }
+            ]
+
+            const res = await request(server).delete('/users/4');
+            expect(res.body.users).toEqual(expected);
+        });
+
+        it('returns status code 200(OK)', async () => {
+            const expected = [
+                {
+                    "id": 1,
+                    "username": "John",
+                    "password": "pass"
+                },
+                {
+                    "id": 2,
+                    "username": "Jack",
+                    "password": "pass"
+                },
+                {
+                    "id": 3,
+                    "username": "Joe",
+                    "password": "pass"
+                },
+                {
+                    "id": 5,
+                     "username": "Max",
+                     "password": "pass"
+                }
+            ]
+
+            const res = await request(server).delete('/users/6');
+            expect(res.status).toBe(200);
+        });
+
+        it('returns JSON', async () => {
+            const expected = [
+                {
+                    "id": 1,
+                    "username": "John",
+                    "password": "pass"
+                },
+                {
+                    "id": 2,
+                    "username": "Jack",
+                    "password": "pass"
+                },
+                {
+                    "id": 3,
+                    "username": "Joe",
+                    "password": "pass"
+                }
+            ]
+
+            const res = await request(server).delete('/users/5')
             expect(res.type).toBe('application/json');
         });
     });
