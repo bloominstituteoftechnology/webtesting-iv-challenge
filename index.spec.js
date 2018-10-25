90a93b2111c23f406b8aa6152fe6f433650c31ed
lndubose <lndubose13@gmail.com>
2018-10-25 15:47:56 -0500

    Create Post with tests
    Post now works with test. It will post the a new game
    into the gameList array. The test make sure the correct
    status codes are being sent and check that the information
    is getting passed correctly.

--- index.spec.js
+++ index.spec.js
@@ -3,6 +3,43 @@ const request = require('supertest');
 const server = require('./server.js');
 
 describe('server', () => {
+  describe('POST /game ', () => {
+    it('post is creating game 201(Created)', async () => {
+      const reqBody = { name: 'Halo 5', yearRelease: 2017, genre: 'action' };
+      const response = await request(server)
+        .post('/game')
+        .send(reqBody);
+
+      expect(response.status).toBe(201);
+    });
+
+    it('should return {name: "Halo 5, yearRelease: 2017, genre: action"}', async () => {
+      const reqBody = { name: 'Halo 5', yearRelease: 2017, genre: 'action' };
+      const response = await request(server)
+        .post('/game')
+        .send(reqBody);
+
+      expect(response.body).toEqual({
+        name: 'Halo 5',
+        yearRelease: 2017,
+        genre: 'action',
+      });
+    });
+
+    it('send error when missing parts in req.body', async () => {
+      const reqBody = {
+        name: 'Assasin creed',
+        yearRelease: 2016,
+      };
+
+      const response = await request(server)
+        .post('/game')
+        .send(reqBody);
+
+      expect(response.status).toBe(400);
+    });
+  });
+
   describe('Server running', () => {
     it('server running', () => {
       expect(true).toBeTruthy();
--- server.js
+++ server.js
@@ -1,11 +1,55 @@
 const express = require('express');
-
 const server = express();
 
 server.use(express.json());
 
+let gamesList = [
+  {
+    name: 'Spider-Man',
+    yearRelease: 2018,
+    genre: 'action-adventure',
+  },
+  {
+    name: 'Fallout 4',
+    yearRelease: 2017,
+    genre: 'action, role-playing',
+  },
+  {
+    name: "PLAYERUNKOWN'S Battlegrounds",
+    yearRelease: 2017,
+    genre: 'Action',
+  },
+  {
+    name: 'Minecraft',
+    yearRelease: 2009,
+    genre: 'sandBox, survival Game',
+  },
+  {
+    name: 'Grand Theft Auto V',
+    yearRelease: 2013,
+    genre: 'action-adventure',
+  },
+  {
+    name: 'Portal',
+    yearRelease: 2007,
+    genre: 'platform, puzzle',
+  },
+];
+
 server.get('/', (req, res) => {
-  res.send('working');
+  res.send(gamesList);
+});
+
+server.post('/game', (req, res) => {
+  let { name, yearRelease, genre } = req.body;
+  if (!name || !yearRelease || !genre) {
+    res.status(400).send('Missing the name, release year, or genre.');
+  } else {
+    let newGame = { name, yearRelease, genre };
+    gamesList = [...gamesList, newGame];
+
+    res.status(201).send(newGame);
+  }
 });
 
 module.exports = server;
