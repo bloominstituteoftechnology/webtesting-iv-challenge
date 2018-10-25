89416682d1140b69c354c1dadfb419c8156b00d6
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-25 18:36:35 -0400

    added code for knex migrations

--- data/migrations/20181025183230_create_users_table.js
+++ data/migrations/20181025183230_create_users_table.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-
+  return knex.schema.createTable('users', table => {
+    table.increments();
+    table.string('username', 255')
+      .notNullable()
+      .unique();
+    table.integer('age').notNullable();
+    table.string('department', 255')
+      .notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTableIfExists('users');
 };
