df57d6485923794151c36c46277bf4505995324d
Terrance Danckert <tbarnett2020@gmail.com>
2018-10-25 17:19:11 -0400

    POST Test**

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  res.status(200).json({ API: 'We running young padawans!!' });
+});
 
-
+server.post('/celeb', (req, res) => {
+  const name = req.body.celebrity;
+  const profession = req.body.profession;
+  res.status(201).json({ 
+      id: 1,
+      name: `${name}`,
+      profession: `${profession}`
+   });
+  });
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -6,4 +6,33 @@ describe('server.js', () => {
     it('we runnning the tests!!', () => {
         expect(true).toBeTruthy();
     })
+
+    describe('GET /', () => {
+      it('200 status code (OK)??', async () => {
+          const response = await request(server).get('/');
+
+          expect(response.status).toBe(200);
+      });
+  });
+
+  describe('POST /celeb', () => {
+        it('should add a celebrity to celeb', async () => {
+            let celebrity = 'Chris Brown';
+            let profession = 'Singer';
+            let response = await request(server)
+                .post(`/celeb`)
+                .send({ celebrity, profession })
+                // .send({ name: 'Chris Brown', profession: 'Singer' });
+            expect(response.body).toEqual({ id: 1, name: 'Chris Brown', profession: 'Singer' });
+        });
+
+        it('should return JSON', async () => {
+            let celebrity = 'Chris Brown';
+            let profession = 'Singer';
+            const response = await request(server)
+                .post('/celeb')
+                .send({ celebrity, profession })
+            expect(response.type).toEqual('application/json');
+        });
+    });
 })
