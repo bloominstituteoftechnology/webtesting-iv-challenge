c20ce89ef7604fd2217aa31031f68474d13f6866
decagondev <decagondevelopment@gmail.com>
2018-10-25 19:59:25 +0100

    (MVP) : TDD made a failing test for POST /clients/:id should return a json object { message: client added }

--- src/server.spec.js
+++ src/server.spec.js
@@ -8,3 +8,16 @@ describe("spinup server tests", () => {
     expect(response.status).toEqual(200);
   });
 });
+
+// base mvp tests
+
+describe("POST /clients/:id test", () => {
+  it("should return JSON object { message: `Client added` } when a client is successfully added and a status code of 200", async () => {
+    const response = await request(server).post("/clients");
+    // test part 1
+    expect(response.body).toEqual({ message: "Client added" });
+
+    // test part 2
+    expect(response.status).toEqual(201);
+  });
+});
