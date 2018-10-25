c4fb3a893b345c4ae357ee1d2930e8620654ca77
jmbradley <jmradley808@gmail.com>
2018-10-25 19:11:58 -0400

    adding post

--- api/server.js
+++ api/server.js
@@ -5,7 +5,14 @@ const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    res.status(200).send('the line is hot');
+    res.status(200).json('the line is hot');
+});
+
+server.post('/:name', (req, res) => {
+    const { name } = req.params;
+
+    res.status(202).json( { heyo: `${name}`})
 });
 
 module.exports = server;
+
--- index.spec.js
+++ index.spec.js
@@ -23,4 +23,25 @@ describe('server', () => {
     it('can even more run run run tests', () => {
         expect(false).toBeFalsy();
     });
+
+    it('should return {message: "the line is hot"}', async () => {
+        const response = await request(server).get('/');
+
+        expect (response.body).toEqual('the line is hot');
+    });
+
+    describe('POST /:name', () => {
+        
+        it('should return name', async () => {
+            const name = 'Orioles';
+            const expected = { name };
+
+            const response = await request(server)
+            .post(`${name}`)
+            .send(`${name} is in` );
+
+            expect(response.body).toEqual(expected);
+        });
+    });
+    
 });
