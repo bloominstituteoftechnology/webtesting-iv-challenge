05fed901c05a3267fec0644def8a3103c5873301
Casey Baker <caseye.baker@gmail.com>
2018-10-25 13:15:56 -0700

    fixed index.js issue; added tests for POST

--- index.spec.js
+++ index.spec.js
@@ -5,7 +5,7 @@ const server = require('./api/server.js');
 describe('server', () => {
   describe('GET /', () => {
     
-    it('should return status code 200(OK)', async () => {
+    it('should return status code 200(OK) when successful', async () => {
       const response = await request(server).get('/');
   
       expect(response.status).toBe(200);
@@ -23,28 +23,52 @@ describe('server', () => {
     })
   });
   
-  describe('POST /', () => {
+  describe('POST /students', () => {
 
-    it('should return correct status code', async () => {
-      //return correct status code
+    it('should return status code 201(Created) when successful', async () => {
+      const firstName = 'Casey';
+      const lastName = 'Baker';
+      const house = 'Slytherin';
+      
+      const student = { firstName: `${firstName}`, lastName: `${lastName}`, house: `${house}` }
+
+      const response = await request(server)
+        .post('/students')
+        .send(student);
+
+      expect(response.status).toBe(201);
     })
 
-    it('should return JSON', async () => {
-      //return JSON
+    it('should return error if missing a field', async () => {
+      const response = await request(server)
+        .post('students')
+        .send({ firstName: 'Casey' });
+
+        expect(response.status).toBe(400);
+
     })
 
     it('should return the created resource', async () => {
-      //return resource
+      const firstName = 'Casey';
+      const lastName = 'Baker';
+      const house = 'Slytherin';
+      const student = { firstName: `${firstName}`, lastName: `${lastName}`, house: `${house}` }
+
+      const response = await request(server)
+        .post('/students')
+        .send(student);
+
+        expect(response.body).toEqual(student);
     })
   })
 
-  describe('DELETE /', () => {
-    it('should return correct status code', async () => {
+  describe('DELETE /students/:id', () => {
+    it('should return status code 200(OK) when successful', async () => {
       //return correct status code
     })
 
-    it('should return number of deleted items', async () => {
-      //return number
+    it('should return deleted item', async () => {
+      //return deleted item
     })
   })
 });
