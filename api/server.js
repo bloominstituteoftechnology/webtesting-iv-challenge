7941557d0fdd3e848b842a0641c635eec85c3bfe
Clint Kunz <clintkunz@gmail.com>
2018-10-25 13:46:55 -0600

    server post and delete tests

--- api/server.js
+++ api/server.js
@@ -8,4 +8,16 @@ server.get('/', (req, res) => {
   res.status(200).json({message: 'server up'});
 });
 
+server.post('/create', (req, res) => {
+    const name = req.body.name;
+    
+    res.status(201).json({user: name})
+});
+
+server.delete('/delete', (req, res) => {
+    const name = req.body.name; 
+
+    res.status(200).json(true);
+})
+
 module.exports = server; 
---
+++ index.spec.js
@@ -0,0 +1,47 @@
+const server = require('./api/server.js');
+const request = require('supertest');
+
+describe('server POST', () => {
+    it('should return a name', async () => {
+        const name = 'clint';
+        const response = await request(server)
+            .post('/create')
+            .send({ name });
+
+        expect(response.body).toEqual({user: 'clint'});
+    })
+
+    it('should return a status of 201', async () => {
+        const response = await request(server).post('/create')
+
+        expect(response.status).toBe(201);
+    })
+
+    it('should return JSON', async () => {
+        const response = await request(server).post('/create')
+
+        expect(response.type).toBe('application/json');
+    })
+})
+
+describe('server DELETE', () => {
+    it('should return a true', async () => {
+        const name = 'clint';
+        const response = await request(server).delete('/delete').send({ name })
+
+        expect(response.body).toEqual(true);
+    })
+
+    it('should return a status of 200', async () => {
+        const name = 'clint';
+        const response = await request(server).delete('/delete').send({ name })
+
+        expect(response.status).toBe(200);
+    })
+
+    it('should return JSON', async () => {
+        const response = await request(server).delete('/delete')
+
+        expect(response.type).toBe('application/json');
+    })
+})
