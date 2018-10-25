226e28413bb7291274e0799ee138aa7c2d4f72f3
m-j-hacker <mjhacker@gmail.com>
2018-10-25 16:29:18 -0500

    basic tests for DELETE endpoint complete

--- index.spec.js
+++ index.spec.js
@@ -17,6 +17,11 @@ describe('server', () => {
     });
 
     describe('POST /players/:character', () => {
+        it('should return JSON', async () => {
+            const response = await request(server).get('/');
+
+            expect(response.type).toBe('application/json');
+        });
         it('should greet the player by name and job class', async () => {
             const character = 'Ezra';
             const jobClass = 'Knight';
@@ -24,10 +29,35 @@ describe('server', () => {
             const expected = { hello: 'Ezra the Knight' };
 
             const response = await request(server)
-                .post(`/players/${character}`);
+                .post(`/players/${character}`)
+                .send({ jobClass });
 
             expect(response.body).toEqual(expected);
                 
         });
+        it('should greet the player as a "Peasant" if no job class is provided', async () => {
+            const character = 'Ezra';
+
+            const expected = { hello: 'Ezra the Peasant' };
+
+            const response = await request(server)
+                .post(`/players/${character}`);
+
+            expect(response.body).toEqual(expected);
+        });
     });
+
+    describe('DELETE /players/:character', () => {
+        it('should return status code 200(OK) if delete is successful', async () => {
+            const character = 'Ezra';
+            const response = await request(server).delete(`/players/${character}`);
+
+            expect(response.status).toBe(200);
+        });
+        it('should return JSON', async () => {
+            const response = await request(server).get('/');
+
+            expect(response.type).toBe('application/json');
+        })
+    })
 });
