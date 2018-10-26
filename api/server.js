83d51105eedb5810d690bb78b0e97a715e3e7c88
Tsai Huang <tsaihuangsd@gmail.com>
2018-10-26 08:12:33 -0700

    latest update with temporary array memory - work in progress

--- api/server.js
+++ api/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+
+// const knexConfig = require('./knexfile.js');
+// const db = knex(knexConfig.development);
+
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
 
+const helperMethods = require('./helper-methods.js');
+
 server.get('/', (req,res)=>{
-    res.status(200).json({message: 'server up'});
+    helperMethods.getData()
+        .then(data =>{
+            res.status(200).json(data);
+        })
+        .catch(err=>res.status(500).json(err));
 });
 
 server.post('/hello/:name', (req,res)=>{
@@ -17,6 +30,12 @@ server.delete('/hello/:name', (req,res)=>{
     const {name} = req.params;
     // const lastName = req.body.lastName;
     res.status(201).json({deleted: `${name}`});
+})
+
+server.put('/hello/:name', (req,res)=>{
+    const {name} = req.params;
+    const lastName = req.body.lastName;
+    res.status(200).json({updated: `${name} ${lastName}`});
 })
 
 module.exports = server;
--- index.spec.js
+++ index.spec.js
@@ -18,13 +18,24 @@ describe('server', ()=>{
             const response = await request(server).get('/');
             expect(response.type).toBe('application/json');
         })
-
-        it('should return { message: "server up" } ', async () =>{
+        it('should return a default memory data ', async () =>{
             const response = await request(server).get('/');
-            expect(response.body).toEqual({ message: 'server up'});
+            expect(response.body).toEqual([{name: "Evelyn",
+            lastName: "Huang"}]);
         })
     })
     describe('POST /hello/:name',()=>{
+        it('should return status code 201(OK)', async ()=>{
+            const name = 'Tsai';
+            const response = await request(server).post(`/hello/${name}`);
+            expect(response.status).toBe(201);
+        })
+        it('should return JSON', async ()=>{
+            const name = 'Tsai';
+            const response = await request(server).post(`/hello/${name}`);
+            expect(response.type).toBe('application/json');
+        })
+
         it('should greet the person', async ()=>{
             const name = 'Tsai';
             const lastName = 'Huang';
@@ -35,9 +46,10 @@ describe('server', ()=>{
         })
       
     })
-    describe('Delete /hello/:name',()=>{
-        it('should return status code 200(OK)', async ()=>{
-            const response = await request(server).delete('/hello/:name');
+    describe('DELETE /hello/:name',()=>{
+        it('should return status code 201(OK)', async ()=>{
+            const name = 'Tsai';
+            const response = await request(server).delete(`/hello/${name}`);
             expect(response.status).toBe(201);
         })
         it('should return JSON', async ()=>{
@@ -55,7 +67,30 @@ describe('server', ()=>{
             //.send({ lastName});
             expect(response.body).toEqual(expected);
         })
-      
+    })
+    describe('PUT /hello/:name',()=>{
+        it('should return status code 200(OK)', async ()=>{
+            const name = 'Tsai';
+            const lastName = 'Huang';
+            const response = await request(server).put(`/hello/${name}`).send({lastName});
+            expect(response.status).toBe(200);
+        })
+        it('should return JSON', async ()=>{
+            const name = 'Tsai';
+            const lastName = 'Huang';
+            const response = await request(server).put(`/hello/${name}`).send({lastName});
+            expect(response.type).toBe('application/json');
+        })
+
+        it('should confirm if the person has been updated', async ()=>{
+            const name = 'Tsai';
+            const lastName = 'Huang';
+            const expected = { updated: 'Tsai Huang'};
+
+            const response = await request(server).put(`/hello/${name}`)
+            .send({ lastName});
+            expect(response.body).toEqual(expected);
+        })
     })
     
     
