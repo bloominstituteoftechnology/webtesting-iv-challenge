f815723cf9aa54189222ee9be936e04a310605c6
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-29 12:06:30 -0400

    Created server.js file

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'It is alive!' });
+});
+
+module.exports = app;
