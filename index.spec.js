0217c5216a4a86c0df5281e76122dc02e97ec6b2
Lee Len Formento <leebatef@gmail.com>
2018-12-04 17:54:09 -0500

    Had to reconfigure servers. Kept getting EADRINUSE error. Both files trying to access one server

--- index.spec.js
+++ index.spec.js
@@ -1,9 +1,17 @@
 const request = require('supertest');
-
+const server = require('./server.js');
 
 describe('index.js', () => {
     it('runs tests', () => {
         expect(true).toBeTruthy();
     });
+
+    describe('post', () => {
+        it('should post', async () => {
+            const id = 97;
+            const response = await request(server).post(`/test/${id}`).send({ test: Philosophy });
+            expect(response.body).toEqual(id);
+        })
+    })
 
 })
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Wazzahhhhp');
+})
+
+server.get('/test', (req, res) => {
+    db('test')
+      .then(tests => res.status(200).json(tests))
+      .catch(err => res.status(500).json(err));
+  });
+
+server.post('/test', (req, res) => {
+  const { tests } = req.body;
+  db('test')
+    .insert(tests)
+    .returning('id')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error inserting', err });
+    });
+});
+
+server.delete('/test/:id', (req, res) => {
+	const { testid } = req.params;
+	db('test')
+		.where({ testid })
+		.del()
+		.then(count => {
+			if (count) {
+				res.status(200).json({ message: `Success! ${id} deleted` });
+			} else {
+				res.status(404).json({ error: `Sorry! ${id} could not be deleted` });
+			}
+		})
+		.catch(err => res.status(500).json(err));
+});
+
+module.exports = server;
