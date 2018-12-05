cbffe460c05e3f8f7ad2aea5cd4bb1cc5b616e38
Hunter315 <hunter.mckay.smith@gmail.com>
2018-12-05 15:42:04 -0700

    GET test complete

--- index.spec.js
+++ index.spec.js
@@ -1,12 +1,20 @@
 const request = require("supertest");
 const server = require("./api/server.js");
 
-
 describe("Server", () => {
   describe("GET /", () => {
     it("returns status code 200(OK)", async () => {
-      const response = await request(server).get("/");
+      const res = await request(server).get("/");
       expect(response.status).toBe(200);
+      expect(res.status).toBe(200);
+    });
+    it("returns JSON", async () => {
+      const res = await request(server).get("/");
+      expect(res.type).toBe("application/json");
+    });
+    it('returns {message: "Server is up"', async () => {
+      const res = await request(server).get("/");
+      expect(res.body).toEqual({ message: "Server is up" });
     });
   });
 });
