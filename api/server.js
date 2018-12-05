39f87e3b13c7c0a3a4bed75e79521f293cf665c1
Hunter315 <hunter.mckay.smith@gmail.com>
2018-12-05 15:46:56 -0700

    POST tests complete

--- api/server.js
+++ api/server.js
@@ -3,8 +3,33 @@ const server = express();
 
 server.use(express.json());
 
+
+let userDB = [
+    {
+        "id": 1,
+        "username": "Hunter",
+        "password": "1234"
+    },
+    {
+        "id": 2,
+        "username": "Sandwich",
+        "password": "1234"
+    },
+    {
+        "id": 3,
+        "username": "Yogurt",
+        "password": "1234"
+    },
+]
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Server is up" });
 });
+
+
+server.post('/users', (req, res) => {
+    const { user } = req.body;
+    userDB.push(user);
+    res.status(201).json({users: userDB});
+})
 
 module.exports = server;
--- index.spec.js
+++ index.spec.js
@@ -1,20 +1,73 @@
 const request = require("supertest");
 const server = require("./api/server.js");
 
-describe("Server", () => {
-  describe("GET /", () => {
-    it("returns status code 200(OK)", async () => {
-      const res = await request(server).get("/");
-      expect(response.status).toBe(200);
-      expect(res.status).toBe(200);
+describe('Server', () => {
+    describe('GET /', () => {
+        it('returns status code 200(OK)', async () => {
+            const res = await request(server).get('/');
+            expect(res.status).toBe(200);
+        });
+
+        it('returns JSON', async () => {
+            const res = await request(server).get('/');
+            expect(res.type).toBe('application/json');
+        });
+
+        it('returns {message: "Server is up"', async () => {
+            const res = await request(server).get('/');
+            expect(res.body).toEqual({message: 'Server is up'});
+        });
     });
-    it("returns JSON", async () => {
-      const res = await request(server).get("/");
-      expect(res.type).toBe("application/json");
+
+    describe('POST /users', () => {
+        it('posts a user to the database', async () => {
+        const user = {
+            "id": 4,
+                "username": "May",
+                "password": "pass"
+        }
+
+        const expected = [
+            {
+                "id": 1,
+                "username": "Hunter",
+                "password": "1234"
+            },
+            {
+                "id": 2,
+                "username": "Sandwich",
+                "password": "1234"
+            },
+            {
+                "id": 3,
+                "username": "Yogurt",
+                "password": "1234"
+            },
+        ]
+        const res = await request(server).post('/users').send({user});
+        expect(res.body.users).toEqual(expected);
+        });
+
+        it('returns status code 201(CREATED)', async () => {
+            const user = {
+                "id": 4,
+                 "username": "Steve",
+                 "password": "1234"
+            }
+
+            const res = await request(server).post('/users').send({user});
+            expect(res.status).toBe(201);
+        });
+
+        it('returns JSON', async () => {
+            const user = {
+                "id": 4,
+                 "username": "Steve",
+                 "password": "1234"
+            }
+
+            const res = await request(server).post('/users').send({user});
+            expect(res.type).toBe('application/json');
+        });
     });
-    it('returns {message: "Server is up"', async () => {
-      const res = await request(server).get("/");
-      expect(res.body).toEqual({ message: "Server is up" });
-    });
-  });
 });
