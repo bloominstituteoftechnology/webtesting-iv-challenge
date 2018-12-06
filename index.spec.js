7677feef259643e16ddad79e8761b22c1d4ced94
David247365 <ds051128@gmail.com>
2018-12-06 16:12:19 -0600

    making sure testing works

---
+++ index.spec.js
@@ -0,0 +1,9 @@
+describe("index.js", () => {
+  it("should run the tests", () => {
+    expect(true).toBeTruthy();
+  });
+
+  it("shpuld run other tests", () => {
+    expect(true).toBeTruthy();
+  });
+});
