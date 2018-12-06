a51e7f571f848dc864b71aca461ed90fd54c02f5
Yanrong Xu <xuyanrong@icloud.com>
2018-12-06 16:06:45 -0500

    index.spec.js was create andd test the root for staus code 200

---
+++ index.spec.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const server = require('./api/server');
+
+describe("Test the root path", () => {
+    it('should return status code 200', async () => {
+        let response = await request(server).get('/');
+        expect(response.status).toBe(200);
+    })
+})
