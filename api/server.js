123367181df67f414af5fa803a24209ff9ca2ad3
SavannahGreuel <smallidgesavannah@gmail.com>
2018-12-06 15:29:29 -0700

    forgot to commit, finished MVP

--- api/server.js
+++ api/server.js
@@ -1,14 +1,34 @@
 const express = require('express');
 
 const server = express();
+const users = require('../data/usersModel');
 
 server.use(express.json());
 
 // sanity check endpoint
 server.get('/', (req, res) => {
-  res.status(200).json({ api: 'up' });
+  res.status(200).json({ api: 'live' });
 });
 
-const port = process.env.PORT || 9000;
+server.post('/api/users', async (req, res) => {
+    try {
+        let response = await users.insert(req.body)
+        res.status(201).json(response)
+      } catch(err) {
+        console.log(err);
+      } 
+});
+
+server.delete('/api/users/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        let response = await users.remove(id)
+        res.status(200).json(response)
+      } catch(err) {
+        console.log(err);
+      } 
+})
+
+
 
 module.exports = server;
--- index.spec.js
+++ index.spec.js
@@ -1,11 +1,58 @@
 const request = require('supertest');
-
+const db = require('./data/dbConfig.js');
 const server = require('./api/server.js');
+const users = require('./data/usersModel');
 
-describe('/ route', () => {
-    it('should return status code 200', async () => {
-      let response = await request(server).get('/');
+describe('server.js', () => { 
 
-      expect(response.status).toBe(200);
+    describe('/ route', () => {
+        it('should return status code 200', async () => {
+            let response = await request(server).get('/');
+             expect(response.status).toBe(200);
+        });
+
+        it('should return JSON', async () => {
+            let response = await request(server).get('/');
+            expect(response.type).toBe('application/json');
+        });
+
     });
+
+    beforeEach(async () => {
+        await db('users').truncate();
+    });
+
+    describe('/create users', () => {
+        it('should insert the given user', async () => {
+            let rows = await db('users').where({ name: 'Savannah'});
+            expect(rows).toHaveLength(0);
+
+            await users.insert({ name: 'Savannah' });
+            await users.insert({ name: 'Serenna' });
+
+            rows = await db('users').where({ name: 'Savannah'});
+            expect(rows).toHaveLength(1);
+
+            rows = await db('users');
+            expect(rows).toHaveLength(2);   
+        });
+
+        it('should return correct status code after adding user successfully', async () => {
+            let response = await request(server).post('/api/users').send({name: 'Tai'});
+            expect(response.status).toBe(201);
+        });
+    })
+
+    describe('delete user', () => {
+        it('should delete specified user', async () => {
+            let post = await request(server).post('/api/users').send({name: 'Tai'});
+            let response = await request(server).delete('/api/users/1');
+            expect(response.body).toBe(1);
+        });
+        it('should return correct status code', async () => {
+           let response = await request(server).delete('/api/users/1');
+            expect(response.status).toBe(200);
+        });
+    })
+
 });
