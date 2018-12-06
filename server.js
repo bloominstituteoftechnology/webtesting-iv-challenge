0d02e8db6b17c5f355b450f18e1101491a80630e
Steven O'Neill <oneillsteven957@gmail.com>
2018-12-06 17:35:05 -0600

    endpoints

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express');
 
+const names = require('./data/namesModal.js')
+
 const server = express();
 
 server.use(express.json());
 
 // sanity check endpoint
+
+server.post('/api/names', async (req, res) => {
+    try {
+        let response = await names.insert(req.body)
+        res.status(200).json(response)
+      } catch(err) {
+        console.log(err);
+      } 
+})
+
+  server.delete('/api/names/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        let response = await names.remove(id)
+        res.status(200).json(response)
+      } catch(err) {
+        console.log(err);
+      } 
+  });
+
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'alive' });
