8ca41debdd61f7d2e13a0e24091697cd07465c84
Yanrong Xu <xuyanrong@icloud.com>
2018-12-06 17:04:01 -0500

    the get and post test are working

--- index.spec.js
+++ index.spec.js
@@ -1,16 +1,24 @@
 const request = require('supertest');
 const server = require('./api/server');
 
-describe("Test the root path", () => {
-    it('should return status code 200', async () => {
-        let response = await request(server).get('/');
-        expect(response.status).toBe(200);
+describe('server.js', () => {
+    describe("Test the root path", () => {
+        it('should return status code 200', async () => {
+            let response = await request(server).get('/');
+            expect(response.status).toBe(200);
+        })
     })
 
     describe('POST /create', () => {
         it('should return status code 201', async () => {
             let response = await request(server).post('/create')
             expect(response.status).toBe(201);
+        })
+        it('should return with a body {message: "user Tom created"}', async () => {
+            let response = await request(server)
+                .post('/create')
+            .send({name: 'Tom'});
+            expect(response.body).toEqual({message: 'user Tom created'})
         })
     })
 })
