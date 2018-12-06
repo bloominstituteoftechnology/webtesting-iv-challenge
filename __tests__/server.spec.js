19facd58ab3d31be049e9c0e025c6f9fa056f9e5
ryanboris <ryan.boris@gmail.com>
2018-12-06 15:48:34 -0500

    Creates tests for post endpoint then provides post endpoint route

---
+++ __tests__/server.spec.js
@@ -0,0 +1,50 @@
+const request = require('supertest')
+const server = require('../api/server.js')
+
+describe('server', () => {
+  describe("GET '/'", () => {
+    it('should respond with a status code of 200(OK) when requested', async () => {
+      const response = await request(server).get('/')
+      expect(response.status).toBe(200)
+    })
+    it('should return a JSON response', async () => {
+      const response = await request(server).get('/')
+      expect(response.type).toBe('application/json')
+    })
+    it('should return the following object as a response {message: success}', async () => {
+      const response = await request(server).get('/')
+      expect(response.body).toEqual({ message: 'success' })
+    })
+  })
+})
+
+describe('POST /api/library', () => {
+  it('should return a status of 201(Added) if the post is successful', async () => {
+    const artist = 'Janis Joplin'
+    const album = 'Pearl'
+    const description = 'One of the best albums of all time.'
+    const songs = {
+      sideOne : [ 'Move Over', 'Cry Baby', 'A Woman Left Lonely', 'Half Moon', 'Buried Alive in the Blues' ],
+      sideTwo : [ 'My Baby', 'Me and Bobby McGee', 'Mercedes Benz', 'Trust Me', 'Get It While You Can' ],
+    }
+    const entry = { artist: `${artist}`, album: `${album}`, description: `${description}`, songs: `${songs}` }
+    const response = await request(server).post('/api/library').send(entry)
+    expect(response.status).toBe(201)
+  })
+  it('should return error 400 if missing required properties', async () => {
+    const response = await request(server).post('/api/library').send({ artist: 'Janis' })
+    expect(response.status).toBe(400)
+  })
+  it('should return the newly created entry object', async () => {
+    const artist = 'Janis Joplin'
+    const album = 'Pearl'
+    const description = 'One of the best albums of all time.'
+    const songs = {
+      sideOne : [ 'Move Over', 'Cry Baby', 'A Woman Left Lonely', 'Half Moon', 'Buried Alive in the Blues' ],
+      sideTwo : [ 'My Baby', 'Me and Bobby McGee', 'Mercedes Benz', 'Trust Me', 'Get It While You Can' ],
+    }
+    const entry = { artist: `${artist}`, album: `${album}`, description: `${description}`, songs: `${songs}` }
+    const response = await request(server).post('/api/library').send(entry)
+    expect(response.body).toEqual(entry)
+  })
+})
---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+  console.log('success')
+  res.status(200).json({ message: 'success' })
+})
+
+server.post('/api/library', (req, res) => {
+  let { artist, album, description, songs } = req.body
+  if (!artist || !album || !description || !songs) {
+    res.status(400).json({ error: 'Please provide artist, album, description, and songs.' })
+  }
+    res.status(201).json({ artist, album, description, songs })
+})
+
+module.exports = server
