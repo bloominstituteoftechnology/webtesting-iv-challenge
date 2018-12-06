83a514449910a18da07d7b61962938f77d1ff87a
Peter Pham <phamhere@gmail.com>
2018-12-06 13:01:42 -0700

    added endpoints and passed tests

--- server.js
+++ server.js
@@ -6,4 +6,18 @@ server.use(express.json());
 
 server.get("/", (req, res) => res.send("Welcome!"));
 
+server.post("/users", (req, res) => {
+  if (req.body.name) {
+    res
+      .status(201)
+      .json({ message: `Hello ${req.body.name}, welcome to server testing!` });
+  } else {
+    res.status(400).json({ message: "Please provide a name." });
+  }
+});
+
+server.delete("/users", (req, res) => {
+  res.status(200).json({ message: "User deleted." });
+});
+
 module.exports = server;
