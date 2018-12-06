5f544341009e934c46db728a4ca2b358f02b53b6
Riley-Brown <riley@riley.gg>
2018-12-06 18:30:49 -0500

    wrote a delete route and test

--- api/server.js
+++ api/server.js
@@ -9,4 +9,14 @@ server.get("/", (req, res) => {
   res.status(200).json({ api: "up" });
 });
 
+server.post("/hello", (req, res) => {
+  const { firstName, lastName } = req.body;
+
+  res.status(200).json({ hello: `${firstName} ${lastName}` });
+});
+
+server.delete("/hello", (req, res) => {
+  res.status(200).json({ test: "passed" });
+});
+
 module.exports = server;
--- index.spec.js
+++ index.spec.js
@@ -8,5 +8,30 @@ describe("server.js", () => {
       let res = await request(server).get("/");
       expect(res.status).toBe(200);
     });
+
+    it("Should return JSON", async () => {
+      let res = await request(server).get("/");
+
+      expect(res.type).toBe("application/json");
+    });
+  });
+
+  describe("POST /hello endpoint", () => {
+    it("Should return users first and last name", async () => {
+      let res = await request(server)
+        .post("/hello")
+        .send({ firstName: "Riley", lastName: "Brown" });
+
+      expect(res.body).toEqual({ hello: "Riley Brown" });
+    });
+  });
+
+  describe("Delete /hello endpoint", () => {
+    it("Should delete a greeting", async () => {
+      let res = await request(server)
+        .delete("/hello")
+        .send({ test: "pass" });
+      expect(res.body).toEqual({ test: "passed" });
+    });
   });
 });
