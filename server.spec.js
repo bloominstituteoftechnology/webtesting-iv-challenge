8c63005a1078d1f8a9370b892d24349a63885a85
sean-one <basement365@gmail.com>
2018-12-06 14:49:30 -0800

    testing to see server alive

--- server.spec.js
+++ server.spec.js
@@ -1,8 +1,23 @@
 const request = require('supertest');
 const server = require('./api/server.js');
 
-describe('index.js', () => {
-    it('should be on and crackin', () => {
-        expect(true).toBeTruthy();
+describe('server.js', () => {
+    describe('should check to see if server is up', () => {
+        it('check for server response status code of 200', async () => {
+            const response = await request(server).get('/');
+            expect(response.status).toBe(200);
+        });
+        
+        it('returns JSON data', async () => {
+            const response = await request(server).get('/');
+            expect(response.type).toBe('application/json');
+        });
+        
+        it('should check for alive message | body { message: "here we is!" }', async () => {
+            const response = await request(server).get('/');
+            expect(response.body).toEqual({ message: 'here we is!' });
+            
+        });
+
     });
 })
