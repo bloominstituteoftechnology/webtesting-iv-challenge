a893b0558b65c86f48a0d05acf8564df9e7031b2
Matthew Wright <matthewbwright@gmail.com>
2018-12-06 14:26:13 -0700

    sets up migrations and adds more file structure

---
+++ data/migrations/20181206142347_wotcharacters.js
@@ -0,0 +1,10 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('characters', tbl => {
+    tbl.increments();
+    tbl.string('name', 255).notNullable;
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('characters');
+};
