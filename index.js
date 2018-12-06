0897876466de91103116d3e76b6c14e5559d3d8b
samsisle <samko9522@gmail.com>
2018-12-06 15:53:06 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// WEB API ARCHITECTURE
+// ==============================================
+const app = require('express')();
+
+// START THE WEB API
+// ==============================================
+app.listen(port, () => console.log(`\n=== Web API listening on http://localhost:${port} ===\n`));
