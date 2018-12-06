8996cf58b967e9cd41c6f463cbaf6d225e53dd6d
Matthew Wright <matthewbwright@gmail.com>
2018-12-06 14:27:57 -0700

    adds sanity check

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+//sanity check endpoint
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'up' });
+});
