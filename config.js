3bac45324707b33ebe4fdf80eed625d6cb400525
JacobABrennan <jacobabrennan@gmail.com>
2018-12-06 18:09:42 -0500

    Add Tests

---
+++ config.js
@@ -0,0 +1,11 @@
+
+
+//==============================================================================
+
+//------------------------------------------------
+module.exports = {
+    URL_API_RESOURCE: '/resource',
+    MIME_APPLICATION_JSON: 'application/json',
+    FIELD_NAME: 'name',
+    FIELD_DATA: 'data',
+};
--- index.test.js
+++ index.test.js
@@ -5,10 +5,67 @@
 //------------------------------------------------
 const request = require('supertest');
 const server = require('./server.js');
+const resourceManager = require('./resource_manager.js');
+const config = require('./config.js');
+
+
+//==============================================================================
+
+describe('Test API Server', () => {
 
 //------------------------------------------------
-describe('Tests Functioning', () => {
-    test('should return status code 200', () => {
-        expect(server).toBeTruthy();
+    describe('Test Retrieve Resource', () => {
+        //
+        const testUrl = `${config.URL_API_RESOURCE}/1`;
+        //
+        beforeEach(async function () {
+            await resourceManager.clear();
+            await resourceManager.create({
+                [config.FIELD_NAME]: 'Test Resource',
+                [config.FIELD_DATA]: 'Test Data'    ,
+            });
+        });
+        //
+        test('responds with status code 200', async function () {
+            let response = await request(server).get(testUrl);
+            expect(response.status).toBe(200);
+        });
+        test('responds with JSON object', async function () {
+            let response = await request(server).get(testUrl);
+            expect(response.type).toBe('application/json');
+        });
+        test('responds with requested data', async function () {
+            let response = await request(server).get(testUrl);
+            expect(response.body.hasOwnProperty(config.FIELD_NAME)).toBeTruthy();
+            expect(response.body.hasOwnProperty(config.FIELD_DATA)).toBeTruthy();
+        });
+    });
+
+//------------------------------------------------
+    describe('Test Create Resource', () => {
+        //
+        const testUrl = config.URL_API_RESOURCE;
+        const testData = {
+            [config.FIELD_NAME]: 'Test Resource',
+            [config.FIELD_DATA]: 'Test Data'    ,
+        };
+        //
+        beforeEach(async function () {
+            await resourceManager.clear();
+        });
+        //
+        test('responds with status code 201', async function () {
+            let response = await request(server).post(testUrl, testData);
+            expect(response.status).toBe(201);
+        });
+        test('responds with JSON object', async function () {
+            let response = await request(server).post(testUrl, testData);
+            expect(response.type).toBe(config.MIME_APPLICATION_JSON);
+        });
+        test('responds with id of created resource', async function () {
+            let response = await request(server).post(testUrl, testData);
+            expect(response.body.hasOwnProperty(config.FIELD_NAME)).toBeTruthy();
+            expect(response.body.hasOwnProperty(config.FIELD_DATA)).toBeTruthy();
+        });
     });
 });
