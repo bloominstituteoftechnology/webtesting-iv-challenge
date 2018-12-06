4847abf9e2d3539630aac190d29a9a3b905ff7a5
TRIF3X <thewizardofcrypto@gmail.com>
2018-12-06 14:36:07 -0600

    began to write tests before route handlers

--- server.js
+++ server.js
@@ -7,4 +7,10 @@ server.get('/', (req, res) => {
     res.status(200).json({ message: 'im working' })
 })
 
+server.post('/hello/:name', (req, res) => {
+    const { name } = req.params
+    
+    res.status(200).json({ hello: name })
+})
+
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -12,6 +12,41 @@ describe('server.js', () => {
             const response = await request(server).get('/')
             expect(response.type).toBe('application/json')
         })
+        it('should return message: im working ', async () => {
+            const body = { message: 'im working' }
+            const response = await request(server).get('/')
+            expect(response.body).toEqual(body)
+        })
+    })
+
+    describe('POST /hello/:name', () => {
+
+        it('should greet the person', async () => {
+            const expected = { hello: 'Conner Hoessly' }
+            const name = 'Conner Hoessly'
+            const response = await request(server).post(`/hello/${name}`).send({ name })
+
+            expect(response.body).toEqual(expected)
+        })
+
+    })
+
+    describe('POST should create a user', () => {
+
+        it('should return status code 200', async () => {
+            const response = await request(server).get('/user')
+            expect(response.status).toBe(201)
+        })
+
+    })
+
+    describe('DELETE should delete a user', () => {
+
+        it('should return status code 200', async () => {
+            const response = await request(server).get('/:user')
+            expect(response.status).toBe(202)
+        })
+
     })
 
 })
