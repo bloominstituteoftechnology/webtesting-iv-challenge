3b0a1b7cce3006b5e690bd4576f3bb889c7555a1
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-12-06 16:29:40 -0700

    adds default route test

--- index.spec.js
+++ index.spec.js
@@ -3,8 +3,21 @@ const request = require('supertest');
 const server = require('./api/server.js');
 
 
+// describe('server.js', () => {
+//     it('should run the tests', () => {
+//         expect(true).toBeTruthy();
+//     })
+// })
+
 describe('server.js', () => {
-    it('should run the tests', () => {
-        expect(true).toBeTruthy();
+    describe('/ route', () => {
+        it('should run status code 200', async () => {
+            let response = await request(server).get('/');
+            expect(response.status).toBe(200);
+        });
+        it('should return JSON', async => {
+            let response = await request(server).get('/');
+            expect(response.type).toBe('application/json');
+        });
     })
 })
