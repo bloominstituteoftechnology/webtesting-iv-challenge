ff2ac10d4971660ab555c5e800eefe77c59b060e
koennecker <koenneckerb@gmail.com>
2018-12-06 14:50:54 -0700

    Complete tests for DELETE route.

--- index.js
+++ index.js
@@ -22,13 +22,24 @@ server.get("/", (req, res) => {
 server.post("/", (req, res) => {
   const { name } = req.body;
   if (!name) {
-    res.status(401).json({ error: "Please provide a name." });
+    res.status(400).json({ error: "Please provide a name." });
     return;
   }
   const newUser = { name, id: userId };
   users.push(newUser);
   res.json(newUser);
   userId++;
+});
+
+server.delete("/", (req, res) => {
+  const { id } = req.body;
+  const foundUser = users.find(user => user.id === id);
+  if (!foundUser) {
+    res.status(400).json({ error: "Please provide a valid id." });
+    return;
+  }
+  users.filter(user => user !== foundUser);
+  res.json(foundUser);
 });
 
 server.listen(9000, () => {
--- index.spec.js
+++ index.spec.js
@@ -22,12 +22,27 @@ describe("index.js", () => {
         .send({ name: "user3" });
       expect(response.body).toEqual({ name: "user3", id: 3 });
     });
-    it("return an error if no name is provided", async () => {
+    it("should return an error if no name is provided", async () => {
       let response = await request(server)
         .post("/")
         .send({});
-      expect(response.status).toBe(401);
+      expect(response.status).toBe(400);
       expect(response.body).toEqual({ error: "Please provide a name." });
+    });
+  });
+  describe("DELETE", () => {
+    it("should return the delete user", async () => {
+      let response = await request(server)
+        .delete("/")
+        .send({ id: 1 });
+      expect(response.body).toEqual({ name: "user", id: 1 });
+    });
+    it("should return an error if not given a valid id", async () => {
+      let response = await request(server)
+        .delete("/")
+        .send({ id: 11 });
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({ error: "Please provide a valid id." });
     });
   });
 });
