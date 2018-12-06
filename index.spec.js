d8af629fe38fd73d8a8bbe1f5328c623b52f321e
Brian G <brian.gregorius@gmail.com>
2018-12-06 15:03:01 -0500

    tested get status and json response

--- index.spec.js
+++ index.spec.js
@@ -4,6 +4,18 @@ const server = require('./api/server.js');
 
 describe('server', () => {
 
+    it('can run test', () => {
+        expect(true).toBeTruthy();
+    });
 
+    it('can respond with status code 200 (ok)', async () => {
+        const response = await request(server).get('/');
+        expect(response.status).toBe(200);
+    });
+
+    it('can return JSON', async () => {
+        const response = await request(server).get('/');
+        expect(response.body).toEqual({ message: `server is up` });
+    });
 
 });
