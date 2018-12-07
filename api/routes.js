f143d36ea876c5a4c642d989ba1db79d4b2a8001
samsisle <samko9522@gmail.com>
2018-12-07 02:55:07 -0800

    Add GET / route handler

--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,8 @@
-const goftRouter = require('./gameofthrones/routes');
+// WEB API GLOBAL ROUTES
+// ==============================================
+const goftRouter = require('./gameofthrones/goftRoutes');
 
 module.exports = app => {
+  app.get('/', (_, res) => res.status(200).json({ you: 'up?' }));
   app.use('/api', goftRouter);
 };
