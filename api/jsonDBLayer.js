fe28f8bee8e5a733037a4eb43766ce70e0de4e5a
Hargobind Atwal <ethaipath@gmail.com>
2018-12-06 16:09:14 -0800

    Wrote json promise libaray to db

--- api/jsonDBLayer.js
+++ api/jsonDBLayer.js
@@ -1,7 +1,51 @@
+const db = require('./testData')
 module.exports = {
-  insert: '',
-  update: '',
-  delete: '',
-  getAll: '',
-  findById: '',
+  insert: (user) => {
+    const {id, firstName, age} = user;
+    return new Promise((resolve, reject) => {
+      db.push({id, firstName, age})
+      resolve([id])
+    })
+  },
+  update: (user) => {
+  const {id, firstName, age} = user;
+  return new Promise((resolve, reject) => {
+    if(db[id]) {
+    const updatedUser = {
+      id: id !== undefined ? id : db[id].id, 
+      firstName: firstName !== undefined ? firstName : db[id].firstName, 
+      age: age !== undefined ? age : db[id].age
+    }
+      db[id] = updatedUser
+      resolve([id])
+  } else {
+    reject('user does not exist')
+  }
+    })
+  },
+  delete: ({id}) => {
+    return new Promise((resolve, reject) => {
+      if(db[id]) {
+        delete db[id]
+        resolve(id)
+      } else {
+        reject('user does not exist')
+      }
+    });
+
+  },
+  getAll: () => {
+    return new Promise((resolve, reject) => {
+      resolve(db)
+    });
+  },
+  findById: ({id}) => {
+    return new Promise((resolve, reject) => {
+      if(db[id]) {
+        resolve(db[id])
+      } else {
+        reject('user does not exist')
+      }
+    });
+  },
 }
--- api/testData.js
+++ api/testData.js
@@ -1,201 +1,201 @@
 module.exports = [{
   "id": 1,
-  "first_name": "Dinah",
-  "age": 35
+  "firstName": "Yancey",
+  "age": 39
 }, {
   "id": 2,
-  "first_name": "Vance",
-  "age": 19
+  "firstName": "Kai",
+  "age": 24
 }, {
   "id": 3,
-  "first_name": "Grissel",
-  "age": 77
+  "firstName": "Agathe",
+  "age": 23
 }, {
   "id": 4,
-  "first_name": "Christoffer",
-  "age": 5
-}, {
-  "id": 5,
-  "first_name": "Lazarus",
-  "age": 98
-}, {
-  "id": 6,
-  "first_name": "Berenice",
-  "age": 90
-}, {
-  "id": 7,
-  "first_name": "Jeanine",
-  "age": 21
-}, {
-  "id": 8,
-  "first_name": "Osbourne",
-  "age": 63
-}, {
-  "id": 9,
-  "first_name": "Frants",
-  "age": 58
-}, {
-  "id": 10,
-  "first_name": "Drake",
-  "age": 40
-}, {
-  "id": 11,
-  "first_name": "Lorant",
-  "age": 7
-}, {
-  "id": 12,
-  "first_name": "Jorge",
+  "firstName": "Charlean",
   "age": 35
 }, {
-  "id": 13,
-  "first_name": "Brittany",
-  "age": 100
+  "id": 5,
+  "firstName": "Maddi",
+  "age": 65
 }, {
-  "id": 14,
-  "first_name": "Ranee",
-  "age": 13
+  "id": 6,
+  "firstName": "Jeth",
+  "age": 62
 }, {
-  "id": 15,
-  "first_name": "Cozmo",
-  "age": 52
+  "id": 7,
+  "firstName": "Sigvard",
+  "age": 92
 }, {
-  "id": 16,
-  "first_name": "Thaxter",
-  "age": 58
+  "id": 8,
+  "firstName": "Cam",
+  "age": 47
 }, {
-  "id": 17,
-  "first_name": "Emery",
-  "age": 56
+  "id": 9,
+  "firstName": "Daisey",
+  "age": 31
 }, {
-  "id": 18,
-  "first_name": "Ray",
+  "id": 10,
+  "firstName": "Nora",
+  "age": 64
+}, {
+  "id": 11,
+  "firstName": "Liza",
   "age": 18
 }, {
+  "id": 12,
+  "firstName": "Jacquetta",
+  "age": 30
+}, {
+  "id": 13,
+  "firstName": "Adriena",
+  "age": 84
+}, {
+  "id": 14,
+  "firstName": "Alfred",
+  "age": 67
+}, {
+  "id": 15,
+  "firstName": "Tobiah",
+  "age": 72
+}, {
+  "id": 16,
+  "firstName": "Carley",
+  "age": 16
+}, {
+  "id": 17,
+  "firstName": "Joly",
+  "age": 3
+}, {
+  "id": 18,
+  "firstName": "Brigida",
+  "age": 5
+}, {
   "id": 19,
-  "first_name": "Tony",
-  "age": 43
+  "firstName": "Waverley",
+  "age": 4
 }, {
   "id": 20,
-  "first_name": "Brigid",
+  "firstName": "Amitie",
   "age": 39
 }, {
   "id": 21,
-  "first_name": "Clarey",
-  "age": 27
+  "firstName": "Odelinda",
+  "age": 82
 }, {
   "id": 22,
-  "first_name": "Hersch",
-  "age": 48
+  "firstName": "Niki",
+  "age": 32
 }, {
   "id": 23,
-  "first_name": "Major",
-  "age": 17
+  "firstName": "Carlyle",
+  "age": 75
 }, {
   "id": 24,
-  "first_name": "Creigh",
-  "age": 40
+  "firstName": "Tammy",
+  "age": 22
 }, {
   "id": 25,
-  "first_name": "Bartie",
-  "age": 52
-}, {
-  "id": 26,
-  "first_name": "Alyssa",
-  "age": 44
-}, {
-  "id": 27,
-  "first_name": "Gavra",
-  "age": 59
-}, {
-  "id": 28,
-  "first_name": "Wiatt",
-  "age": 26
-}, {
-  "id": 29,
-  "first_name": "Averil",
-  "age": 3
-}, {
-  "id": 30,
-  "first_name": "Arthur",
-  "age": 64
-}, {
-  "id": 31,
-  "first_name": "Rollins",
-  "age": 67
-}, {
-  "id": 32,
-  "first_name": "Curcio",
-  "age": 52
-}, {
-  "id": 33,
-  "first_name": "Noemi",
-  "age": 23
-}, {
-  "id": 34,
-  "first_name": "Kaye",
-  "age": 91
-}, {
-  "id": 35,
-  "first_name": "Quentin",
-  "age": 23
-}, {
-  "id": 36,
-  "first_name": "Arlina",
-  "age": 23
-}, {
-  "id": 37,
-  "first_name": "Debee",
-  "age": 18
-}, {
-  "id": 38,
-  "first_name": "Dinah",
-  "age": 4
-}, {
-  "id": 39,
-  "first_name": "Antonietta",
-  "age": 51
-}, {
-  "id": 40,
-  "first_name": "Stavro",
+  "firstName": "Katerine",
   "age": 53
 }, {
-  "id": 41,
-  "first_name": "Ninnetta",
-  "age": 100
+  "id": 26,
+  "firstName": "Oberon",
+  "age": 31
 }, {
-  "id": 42,
-  "first_name": "Farlie",
+  "id": 27,
+  "firstName": "Mallory",
+  "age": 45
+}, {
+  "id": 28,
+  "firstName": "Bancroft",
+  "age": 3
+}, {
+  "id": 29,
+  "firstName": "Kevyn",
   "age": 67
 }, {
+  "id": 30,
+  "firstName": "Rebeka",
+  "age": 70
+}, {
+  "id": 31,
+  "firstName": "Johann",
+  "age": 77
+}, {
+  "id": 32,
+  "firstName": "Kelcey",
+  "age": 60
+}, {
+  "id": 33,
+  "firstName": "Lillis",
+  "age": 66
+}, {
+  "id": 34,
+  "firstName": "Cathy",
+  "age": 19
+}, {
+  "id": 35,
+  "firstName": "Agata",
+  "age": 96
+}, {
+  "id": 36,
+  "firstName": "Thebault",
+  "age": 97
+}, {
+  "id": 37,
+  "firstName": "Cosetta",
+  "age": 16
+}, {
+  "id": 38,
+  "firstName": "Demetrius",
+  "age": 97
+}, {
+  "id": 39,
+  "firstName": "Elia",
+  "age": 16
+}, {
+  "id": 40,
+  "firstName": "Eric",
+  "age": 71
+}, {
+  "id": 41,
+  "firstName": "Boot",
+  "age": 43
+}, {
+  "id": 42,
+  "firstName": "Fairfax",
+  "age": 56
+}, {
   "id": 43,
-  "first_name": "Theda",
-  "age": 65
-}, {
-  "id": 44,
-  "first_name": "Madelena",
-  "age": 23
-}, {
-  "id": 45,
-  "first_name": "Bronson",
-  "age": 14
-}, {
-  "id": 46,
-  "first_name": "Osbourne",
+  "firstName": "Betty",
   "age": 29
 }, {
+  "id": 44,
+  "firstName": "Catlee",
+  "age": 67
+}, {
+  "id": 45,
+  "firstName": "Bay",
+  "age": 68
+}, {
+  "id": 46,
+  "firstName": "Clovis",
+  "age": 28
+}, {
   "id": 47,
-  "first_name": "Alvie",
-  "age": 36
+  "firstName": "Eleni",
+  "age": 73
 }, {
   "id": 48,
-  "first_name": "Monro",
-  "age": 63
+  "firstName": "Erminie",
+  "age": 73
 }, {
   "id": 49,
-  "first_name": "Massimo",
-  "age": 55
+  "firstName": "Coralyn",
+  "age": 3
 }, {
   "id": 50,
-  "first_name": "Corette",
-  "age": 57
-}]
+  "firstName": "Ashlee",
+  "age": 91
+}];
