c287f52302f4ea5846b06089b5070ce7b948b4f1
DaftBeowulf <ajmboosh@gmail.com>
2018-12-07 09:22:08 -0600

    after reviewing some of the material and seeing a proper implementation of supertest, successfully implemented post and delete functionality to the created db for Scrubs characters

--- api/server.js
+++ api/server.js
@@ -1,28 +1,34 @@
 const express = require("express");
+const db = require("../data/dbConfig");
 const server = express();
 server.use(express.json());
-
-let mockDB = [{ name: "Samwise" }];
 
 // sanity check
 server.get("/", (req, res) => {
   res.status(200).json({ api: "running" });
 });
 
-server.get("/hobbits", (req, res) => {
-  res.status(200).json(mockDB);
+server.post("/scrubs", (req, res) => {
+  const character = req.body;
+  db("characters")
+    .insert(character)
+    .then(id => {
+      res.status(201).json({ id });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-server.post("/hobbits", (req, res) => {
-  const hobbit = req.body;
-  hobbit !== {} ? (mockDB = [...mockDB, hobbit]) : null;
-  res.status(201).json(mockDB.length);
-});
-
-server.delete("/hobbits", (req, res) => {
-  const hobbitName = req.body.name;
-  mockDB = mockDB.filter(hobbit => hobbit.name !== hobbitName);
-  res.status(200).json(mockDB);
+server.delete("/scrubs/:id", (req, res) => {
+  const charId = req.params.id;
+  db("characters")
+    .where({ id: charId })
+    .del()
+    .then(count => {
+      count ? res.status(200).json(count) : res.status(404);
+    })
+    .catch(err => res.status(500));
 });
 
 const port = process.env.PORT || 7000;
--- index.spec.js
+++ index.spec.js
@@ -2,6 +2,13 @@ const request = require("supertest");
 
 const server = require("./api/server");
 
+const db = require("./data/dbConfig");
+
+//clears table and resets id counter before each test
+beforeEach(async () => {
+  await db("characters").truncate();
+});
+
 describe("server.js", () => {
   describe("/", () => {
     it("should keep me sane by returning a 200 code", async () => {
@@ -21,43 +28,51 @@ describe("server.js", () => {
     });
   });
 
-  describe("GET route /hobbits", () => {
-    it("returns the length of the current mockDB", async () => {
-      let response = await request(server).get("/hobbits");
-      expect(response.body.length).toBe(1);
+  describe("/scrubs POST route", () => {
+    it("successfully adds a new scrubs character to the db", async () => {
+      let rows = await db("characters").where({ name: "JD" });
+      // first prove it doesn't have the specified characters
+      expect(rows).toHaveLength(0);
+
+      // insert a record
+      await db("characters").insert({ name: "JD" });
+      rows = await db("characters").where({ name: "JD" });
+      expect(rows).toHaveLength(1);
+
+      await db("characters").insert({ name: "Turk" });
+      rows = await db("characters");
+      expect(rows).toHaveLength(2);
+    });
+
+    it("returns status 201 upon successful creation", async () => {
+      let response = await request(server)
+        .post("/scrubs")
+        .send({ name: "Bob Kelso" });
+      expect(response.status).toBe(201);
+      let rows = await db("characters").where({ name: "Bob Kelso" });
+      expect(rows.length).toBe(1);
     });
   });
 
-  describe("POST route /hobbits", () => {
-    it("adds the new hobbit to the mockDB", async () => {
-      await request(server)
-        .post("/hobbits")
-        .send({ name: "Merry" });
-      let response = await request(server).get("/hobbits");
-      expect(response.body.length).toBe(2);
+  describe("/scrubs/:id DELETE route", () => {
+    it("successfully deletes a specified scrubs character from the db", async () => {
+      //since we're truncating all tables before tests run, first insert a new character
+
+      await db("characters").insert({ name: "Turkleton" });
+      let rows = await db("characters");
+      expect(rows).toHaveLength(1);
+      await db("characters")
+        .where({ id: 1 })
+        .del();
+      rows = await db("characters");
+      expect(rows).toHaveLength(0);
     });
 
-    it("returns a status code 201 if successful", async () => {
-      let response = await request(server)
-        .post("/hobbits")
-        .send({ name: "Frodo" });
-      expect(response.status).toEqual(201);
-    });
-  });
-
-  describe("DELETE route /hobbits", () => {
-    it("deletes the passed hobbit", async () => {
-      expect((await request(server).get("/hobbits")).body).toHaveLength(3);
-      let response = await request(server)
-        .delete("/hobbits")
-        .send({ name: "Samwise" });
-      expect(response.body).toHaveLength(2);
-    });
-
-    it("returns a status code 200", async () => {
-      let response = await request(server)
-        .delete("/hobbits")
-        .send({ name: "Samwise" });
+    it("returns 200 status upon successful deletion", async () => {
+      await db("characters").insert({ name: "Turkleton" });
+      let rows = await db("characters");
+      expect(rows).toHaveLength(1);
+      let response = await request(server).delete("/scrubs/1");
       expect(response.status).toBe(200);
     });
   });
