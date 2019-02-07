af8a88374d9b9da55cde2005f359d8574777d7c8
Joseph Thompson <joe.thompson838@gmail.com>
2019-02-07 11:32:26 -0500

    wrote tests for a get to /

---
+++ api/server.test.js
@@ -0,0 +1,25 @@
+const request = require("supertest");
+
+const server = require("./server");
+
+describe("ROUTE HANDLERS", () => {
+  describe("get /", () => {
+    it("recieves a 200 status code", async () => {
+      const response = await request(server).get("/");
+
+      expect(response.status).toBe(200);
+    });
+
+    it("recieves a 400 status code", async () => {
+      const response = await request(server).get("/");
+      response.status(400);
+      expect(response.status).toBe(400);
+    });
+
+    it("recieves json format", async () => {
+      const response = await request(server).get("/");
+
+      expect(response.type).toMatch(/json/i);
+    });
+  });
+});
