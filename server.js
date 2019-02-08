5fca92d0ad3a3e7b5c9af8b170f3060ee8266cbe
jbrock1981 <jbrock1981@gmail.com>
2019-02-08 07:30:23 -0600

    finished bioler plating and file creations

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const users = require('./users');
 const server = express();
 
 const PORT = process.env.PORT || 42;
 
 server.use(express.json());
 
-
+server.get('/', async (req, res) => {
+    await
+    res
+        .status(200)
+        .json({ API: 'working' });
+})
 
 module.exports = {
     server
