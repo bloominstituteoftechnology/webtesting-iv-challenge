a73f3be6676f5c901b420980426e88b12c75a6c7
Bruce Cabanayan <bcabanayan@Unknown.domain>
2019-02-07 20:51:11 -0700

    Wrote tests for post endpoint

--- api/server.spec.js
+++ api/server.spec.js
@@ -5,20 +5,46 @@ const server = require('./server.js');
 const db = require('../data/dbConfig.js');
 
 describe('The route handlers', () => {
-    describe('get /', () => {
+    describe('get /users', () => {
         it('responds with status code 200', async () => {
             const response = await request(server).get('/users');
+            
             expect(response.status).toBe(200);
         });
 
         it('responds with json', async () => {
             const response = await request(server).get('/users');
+            
             expect(response.type).toMatch(/json/i);
         });
 
         it('sends the correct response object', async () => {
             const response = await request(server).get('/users');
+            
             expect(response.body).toEqual({});
         });
     });
+
+    describe('post /users', () => {
+        it('responds with status code 201', async () => {
+            const body = { username: 'bruce', password: 'password'};
+            const response = await request(server).post('/users').send(body);
+
+            expect(response.status).toBe(201);
+        });
+
+        it('responds with 400 when body is missing data', async () => {
+            const body = { username: 'samuel' };
+            const response = await request(server).post('/users').send(body);
+
+            expect(response.status).toBe(400);
+        });
+
+        it('responds with an array containing a new id', async () => {
+            const body = { name: 'bilbo' };
+            const response = await(server).post('/users').send(body);
+
+            expect(response.body.length.toBe(1));
+        });
+    })
 })
