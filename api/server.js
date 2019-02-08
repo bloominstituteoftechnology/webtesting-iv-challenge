e57d5ae10a815dba6f4d422ef73dbb199a46cbbd
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-07 23:25:11 -0500

    added post route

--- api/server.js
+++ api/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 
+const characterDB = require('../data/helpers/characterDb');
+
 const server = express();
 
 server.use(express.json());
 
 server.get('/', (req, res) => {
     res.status(200).json({ hero: 'The Gunslinger' });
+});
+
+server.post('/character', async(req, res) => {
+    const character = req.body;
+
+    if(character.name) {
+        const posted = await characterDB.add(character);
+
+        res.status(201).json(posted);
+    } else {
+        res.status(400).json({ message: 'New character needs a name' })
+    }
 });
 
 module.exports = server;
