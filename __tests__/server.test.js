e61d14079c8ac61c9cd0925e3cef57a76b8f1a0a
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-07 22:50:06 -0500

    added my first test for route handler /

---
+++ __tests__/server.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const server = require('../api/server');
+
+describe('The route handlers', () => {
+    describe('get /', () => {
+        it('responds with 200', async () => {
+            const response = await request(server).get('/');
+
+            expect(response.status).toBe(200);
+        });
+    });
+});
