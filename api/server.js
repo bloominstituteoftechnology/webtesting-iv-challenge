f31863f46832b4ee825885e7ea4cdbda8dbd9ad0
cferrell2 <cferrell12182@gmail.com>
2019-02-07 22:38:31 -0500

    create tests for GET and POST route handlers.

--- api/server.js
+++ api/server.js
@@ -13,8 +13,17 @@ server.get('/',(req, res)=>{
   res.status(200).json(carsData);
 })
 
+//GET Route Handler
 server.get('/cars', (req, res)=>{
   res.status(200).json({carsData})
+})
+
+//POST Route Handler
+server.post('/cars', (req, res) =>{
+  const make = req.body.make;
+  const model = req.body.model;
+  carsData.push(req.body);
+  res.status(201).json({name: `${make} ${model}`});
 })
 
 //export the server
--- index.spec.js
+++ index.spec.js
@@ -10,17 +10,70 @@ const carsData = require(filename);
 
 //GET Tests
 describe('GET /cars', ()=>{
+  it('should return status code 200', async ()=>{
+    const response = await request(server).get('/cars');
 
+    expect(response.status).toBe(200);
+  })
+
+  it('should return type JSON', async ()=>{
+    const response = await request(server).get('/cars')
+
+    expect(response.type).toBe('application/json')
+  })
+
+  it('should return object containing all cars', async ()=>{
+    const response = await request(server).get('/cars');
+    const expected = [
+      {
+        "id": 0,
+        "make": "porshe", 
+        "model": "911 carerra",
+        "year": "2013"
+      },
+      {
+        "id": 1,
+        "make": "chevrolet", 
+        "model": "corvette",
+        "year": "2017"
+      }
+    ]
+
+    expect(response.body.carsData).toEqual(expected);
+  })
 })
 
 //POST Tests
 describe('POST /cars', ()=>{
+  it('should return the name of the newly created car', async ()=>{
+    const response = await request(server).post('/cars')
+          .send(  {
+            "id": 2,
+            "make": "Hyundai", 
+            "model": "Elantra",
+            "year": "2012"
+          });
+    const expected = {name: "Hyundai Elantra"};
+    expect(response.body).toEqual(expected);
+  });
+
+  it('should return status code of 200', async ()=>{
+    const response = await request(server).post('/cars')
+    .send(  {
+      "id": 3,
+      "make": "Subaru", 
+      "model": "Forester",
+      "year": "2015"
+    });
+
+    expect(response.status).toBe(201);
+  })
 
 })
 
 //DELETE Tests
 describe('DELETE /cars/:id', ()=>{
-  
+
 })
 
 
