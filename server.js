9527c652dd70458aaa6c39362b5ee7758679f840
shannonhealy <sahealy525@gmail.com>
2019-02-07 23:23:01 -0500

    create route implementation

--- server.js
+++ server.js
@@ -4,3 +4,21 @@ const server = express();
 server.use(express.json());
 
 module.exports = server;
+
+let nextId = 2;
+let users = [
+    {
+        id: 1,
+        username: "sample user"
+    }
+];
+
+server.post("/api/users", (req, res) => {
+    if (req.body && req.body.username && typeof req.body.username === "string") {
+        users.push({ id: nextId, username: req.body.username });
+        nextId++;
+        res.status(201).json([nextId-1]);
+    } else {
+        res.status(400).json({ error: "incorrectly formatted user" });
+    }
+});
