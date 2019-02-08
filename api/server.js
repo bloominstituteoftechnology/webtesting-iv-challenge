8daac488e42f1c686e67ca8a3260cdb922115c68
Emmanium <pradoe.ep@gmail.com>
2019-02-07 20:42:20 -0700

    initial commit setup boilerplate for project

---
+++ api/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const musicians = require('../musicians/musicians.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+  res.status(200).json({ API: 'Working'});
+});
+
+server.get('/musicians', async (req, res) => {
+  const rows = await hobbits.getAll();
+
+  res.status(200).json(rows);
+});
+
+server.post('/musicians', async (req, res) => {
+  const musicianData = req.body;
+
+  if (musicianData.name) {
+    const ids = await hobbits.insert(musicianData);
+    res.status(201).json(ids);
+  } else {
+    res.status(400).json({ error: 'missing name' });
+  }
+});
+
+server.delete('/musicians', async (req, res) => {
+  
+})
+
+module.exports = server;
