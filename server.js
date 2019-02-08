fd6101919c5646d76b8bd72661d318659c738379
shannonhealy <sahealy525@gmail.com>
2019-02-07 23:39:07 -0500

    delete implementation

--- server.js
+++ server.js
@@ -22,3 +22,21 @@ server.post("/api/users", (req, res) => {
         res.status(400).json({ error: "incorrectly formatted user" });
     }
 });
+
+server.delete("/api/users/:id", (req, res) => {
+    let userExists = false;
+    let userIndex = undefined;
+    users.forEach((user, index) => {
+        if (user.id === parseInt(req.params.id)) {
+            userIndex = index;
+            userExists = true;
+        }
+    });
+
+    if (userExists) {
+        users.splice(userIndex, 1);
+        res.status(200).json(1);
+    } else {
+        res.status(404).json({ error: "user not found" });
+    }
+});
