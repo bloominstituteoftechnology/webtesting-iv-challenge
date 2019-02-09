bb717acfd11dc109dde5ae2adba4c243f7dbcbb9
paulapivat <paul.apivat@gmail.com>
2019-02-09 23:48:04 +0700

    add two test for GET and POST route handler

--- data/api/server.js
+++ data/api/server.js
@@ -14,4 +14,14 @@ server.get('/hobbits', async (req, res) => {
     res.status(200).json(rows);
 });
 
+server.post('/hobbits', async (req, res) => {
+    const hobbitData = req.body;
+    if (hobbitData.name){
+        const ids = await hobbits.insert(hobbitData);
+        res.status(201).json(ids);
+    } else {
+        res.status(400).json({error: "missing name in body"});
+    }
+})
+
 module.exports = server;
